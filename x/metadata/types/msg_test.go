9eb7a3551d13da4ab1b22d0d3a4224190c07472c
Daniel Wedul <dwedul@figure.com>
2021-02-22 17:00:15 -0700

    [89]: fix types/msg_test tests.

--- x/metadata/types/msg_test.go
+++ x/metadata/types/msg_test.go
@@ -9,11 +9,19 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func ownerPartyList(addresses ...string) []Party {
+	retval := make([]Party, len(addresses))
+	for i, addr := range addresses {
+		retval[i] = Party{Address: addr, Role: PartyType_PARTY_TYPE_OWNER}
+	}
+	return retval
+}
+
 func TestAddScopeRoute(t *testing.T) {
 	var scope = NewScope(
 		ScopeMetadataAddress(uuid.MustParse("8d80b25a-c089-4446-956e-5d08cfe3e1a5")),
 		ScopeSpecMetadataAddress(uuid.MustParse("22fc17a6-40dd-4d68-a95b-ec94e7572a09")),
-		[]string{"data_owner"},
+		ownerPartyList("data_owner"),
 		[]string{"data_accessor"},
 		"value_owner",
 	)
@@ -24,22 +32,23 @@ func TestAddScopeRoute(t *testing.T) {
 	yaml := `scope:
   scope_id: scope1qzxcpvj6czy5g354dews3nlruxjsahhnsp
   specification_id: scopespec1qs30c9axgrw5669ft0kffe6h9gysfe58v3
-  owneraddress:
-  - data_owner
-  dataaccess:
+  parties:
+  - address: data_owner
+    role: 5
+  data_access:
   - data_accessor
-  valueowneraddress: value_owner
+  value_owner_address: value_owner
 signers: []
 `
 	require.Equal(t, yaml, msg.String())
-	require.Equal(t, "{\"type\":\"provenance/metadata/AddScopeRequest\",\"value\":{\"scope\":{\"data_access\":[\"data_accessor\"],\"owner_address\":[\"data_owner\"],\"scope_id\":\"scope1qzxcpvj6czy5g354dews3nlruxjsahhnsp\",\"specification_id\":\"scopespec1qs30c9axgrw5669ft0kffe6h9gysfe58v3\",\"value_owner_address\":\"value_owner\"}}}", string(msg.GetSignBytes()))
+	require.Equal(t, "{\"type\":\"provenance/metadata/AddScopeRequest\",\"value\":{\"scope\":{\"data_access\":[\"data_accessor\"],\"parties\":[{\"address\":\"data_owner\",\"role\":5}],\"scope_id\":\"scope1qzxcpvj6czy5g354dews3nlruxjsahhnsp\",\"specification_id\":\"scopespec1qs30c9axgrw5669ft0kffe6h9gysfe58v3\",\"value_owner_address\":\"value_owner\"}}}", string(msg.GetSignBytes()))
 }
 
 func TestAddScopeValidation(t *testing.T) {
 	var scope = NewScope(
 		ScopeMetadataAddress(uuid.MustParse("8d80b25a-c089-4446-956e-5d08cfe3e1a5")),
 		ScopeSpecMetadataAddress(uuid.MustParse("22fc17a6-40dd-4d68-a95b-ec94e7572a09")),
-		[]string{"data_owner"},
+		ownerPartyList("data_owner"),
 		[]string{"data_accessor"},
 		"value_owner",
 	)
@@ -47,23 +56,23 @@ func TestAddScopeValidation(t *testing.T) {
 	err := msg.ValidateBasic()
 	require.Panics(t, func() { msg.GetSigners() }, "panics due to invalid addresses")
 	require.Error(t, err, "invalid addresses")
-	require.Equal(t, "invalid owner on scope: decoding bech32 failed: invalid index of 1", err.Error())
+	require.Equal(t, "invalid party on scope: decoding bech32 failed: invalid index of 1", err.Error())
 
 	msg.Scope = *NewScope(
 		ScopeMetadataAddress(uuid.MustParse("8d80b25a-c089-4446-956e-5d08cfe3e1a5")),
 		ScopeSpecMetadataAddress(uuid.MustParse("22fc17a6-40dd-4d68-a95b-ec94e7572a09")),
-		[]string{},
+		[]Party{},
 		[]string{},
 		"",
 	)
 	err = msg.ValidateBasic()
 	require.Error(t, err, "no owners")
-	require.Equal(t, "scope must have at least one owner", err.Error())
+	require.Equal(t, "scope must have at least one party", err.Error())
 
 	msg.Scope = *NewScope(
 		ScopeMetadataAddress(uuid.MustParse("8d80b25a-c089-4446-956e-5d08cfe3e1a5")),
 		ScopeSpecMetadataAddress(uuid.MustParse("22fc17a6-40dd-4d68-a95b-ec94e7572a09")),
-		[]string{"cosmos1sh49f6ze3vn7cdl2amh2gnc70z5mten3y08xck"},
+		ownerPartyList("cosmos1sh49f6ze3vn7cdl2amh2gnc70z5mten3y08xck"),
 		[]string{},
 		"",
 	)
