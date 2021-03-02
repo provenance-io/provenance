b9e07bd63f972dd07256e29e577e7cf223d25239
Ira Miller <ira@elementalrain.com>
2021-03-02 15:22:14 -0700

    lint

--- x/marker/keeper/msg_server.go
+++ x/marker/keeper/msg_server.go
@@ -42,12 +42,10 @@ func (k msgServer) AddMarker(goCtx context.Context, msg *types.MsgAddMarkerReque
 
 	// Add marker requests must pass extra validation for denom (in addition to regular coin validation expression)
 	valExp := k.GetParams(ctx).UnrestrictedDenomRegex
-	if v, err := regexp.Compile(valExp); err != nil {
-		return nil, err
-	} else {
-		if !v.MatchString(msg.Amount.Denom) {
-			return nil, fmt.Errorf("invalid denom (fails unrestriced marker denom validation %s): %s", valExp, msg.Amount.Denom)
-		}
+	if v, expErr := regexp.Compile(valExp); err != nil {
+		return nil, expErr
+	} else if !v.MatchString(msg.Amount.Denom) {
+		return nil, fmt.Errorf("invalid denom (fails unrestricted marker denom validation %s): %s", valExp, msg.Amount.Denom)
 	}
 
 	addr := types.MustGetMarkerAddress(msg.Amount.Denom)
