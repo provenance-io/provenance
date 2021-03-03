582051952d9363ae1bc429ec0cbf63fd12964439
Phil Story <mtps@users.noreply.github.com>
2021-03-03 14:42:43 -0700

    Add proper upgrade versions

--- app/upgrades/upgrades.go
+++ app/upgrades/upgrades.go
@@ -22,7 +22,9 @@ type appUpgrade struct {
 }
 
 var handlers = map[string]appUpgrade{
-	"v0.1.5": {Added: []string{"metadata"}},
+	"v0.1.5": {},
+	"v0.1.6": {Added: []string{"metadata"}},
+
 
 	// TODO - Add new upgrade definitions here.
 }
