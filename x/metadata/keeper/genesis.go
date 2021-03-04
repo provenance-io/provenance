1fef0413b5f6b19b443489c2d8c396d05a0ac48d
Daniel Wedul <dwedul@figure.com>
2021-03-03 17:20:51 -0700

    [59]: Add spec stuff to the genesis keeper.

--- x/metadata/keeper/genesis.go
+++ x/metadata/keeper/genesis.go
@@ -36,7 +36,11 @@ func (k Keeper) InitGenesis(ctx sdk.Context, data *types.GenesisState) {
 			k.SetContractSpecification(ctx, s)
 		}
 	}
-	// TODO: data.RecordSpecifications
+	if data.RecordSpecifications != nil {
+		for _, s := range data.RecordSpecifications {
+			k.SetRecordSpecification(ctx, s)
+		}
+	}
 }
 
 // ExportGenesis exports the current keeper state of the metadata module.ExportGenesis
@@ -45,6 +49,9 @@ func (k Keeper) ExportGenesis(ctx sdk.Context) (data *types.GenesisState) {
 	scopes := make([]types.Scope, 0)
 	groups := make([]types.RecordGroup, 0)
 	records := make([]types.Record, 0)
+	scopeSpecs := make([]types.ScopeSpecification, 0)
+	contractSpecs := make([]types.ContractSpecification, 0)
+	recordSpecs := make([]types.RecordSpecification, 0)
 
 	appendToScopes := func(scope types.Scope) bool {
 		scopes = append(scopes, scope)
@@ -61,6 +68,21 @@ func (k Keeper) ExportGenesis(ctx sdk.Context) (data *types.GenesisState) {
 		return false
 	}
 
+	appendToScopeSpecs := func(scopeSpec types.ScopeSpecification) bool {
+		scopeSpecs = append(scopeSpecs, scopeSpec)
+		return false
+	}
+
+	appendToContractSpecs := func(contractSpec types.ContractSpecification) bool {
+		contractSpecs = append(contractSpecs, contractSpec)
+		return false
+	}
+
+	appendToRecordSpecs := func(recordSpec types.RecordSpecification) bool {
+		recordSpecs = append(recordSpecs, recordSpec)
+		return false
+	}
+
 	if err := k.IterateScopes(ctx, appendToScopes); err != nil {
 		panic(err)
 	}
@@ -70,7 +92,15 @@ func (k Keeper) ExportGenesis(ctx sdk.Context) (data *types.GenesisState) {
 	if err := k.IterateRecords(ctx, types.MetadataAddress{}, appendToRecords); err != nil {
 		panic(err)
 	}
-	// TODO iterate over existing scope, group specifications and collect here for export
+	if err := k.IterateScopeSpecs(ctx, appendToScopeSpecs); err != nil {
+		panic(err)
+	}
+	if err := k.IterateContractSpecs(ctx, appendToContractSpecs); err != nil {
+		panic(err)
+	}
+	if err := k.IterateRecordSpecs(ctx, appendToRecordSpecs); err != nil {
+		panic(err)
+	}
 
-	return types.NewGenesisState(params, scopes, groups, records, []types.ScopeSpecification{}, []types.ContractSpecification{}, []types.RecordSpecification{})
+	return types.NewGenesisState(params, scopes, groups, records, scopeSpecs, contractSpecs, recordSpecs)
 }
