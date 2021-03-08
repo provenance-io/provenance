940308167b2ee91555415fb0ccbc9b813bec41b1
Daniel Wedul <dwedul@figure.com>
2021-03-08 12:20:46 -0700

    [142]: Being work on cli command for getting a metadata entry by address (id).

--- x/metadata/client/cli/query.go
+++ x/metadata/client/cli/query.go
@@ -30,6 +30,58 @@ func GetQueryCmd() *cobra.Command {
 	return queryCmd
 }
 
+// GetMetadataEntryCmd returns the command handler for querying metadata for anything from an id.
+func GetMetadataEntryCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "get [id]",
+		Short: "Query the current metadata by id",
+		Args:  cobra.ExactArgs(1),
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query the current metadata module by id:
+Example:
+$ %s query metadata get scope1qzxcpvj6czy5g354dews3nlruxjsahhnsp
+
+Any metadata address type is allowed and the appropriate entry will be returned.
+`,
+				version.AppName,
+			)),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			id, err := types.MetadataAddressFromBech32(strings.ToLower(strings.TrimSpace(args[0])))
+			if err != nil {
+				return err
+			}
+			addressType, err := id.Prefix()
+			if err != nil {
+				return err
+			}
+
+			switch addressType {
+			case types.PrefixScope:
+				scopeUUID, err := id.ScopeUUID()
+				if err != nil {
+					return err
+				}
+				return scopeByUUID(cmd, scopeUUID.String())
+			case types.PrefixSession:
+				// TODO: Session lookup
+			case types.PrefixRecord:
+				// TODO: Record lookup
+			case types.PrefixScopeSpecification:
+				// TODO: Scope spec lookup
+			case types.PrefixContractSpecification:
+				// TODO: Contract spec lookup
+			case types.PrefixRecordSpecification:
+				// TODO: Record spec lookup
+			}
+			return fmt.Errorf("unexpected address type %s", addressType)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
 // GetMetadataParamsCmd returns the command handler for metadata parameter querying.
 func GetMetadataParamsCmd() *cobra.Command {
 	cmd := &cobra.Command{
@@ -78,24 +130,26 @@ $ %s query metadata scope 123e4567-e89b-12d3-a456-426614174000
 				version.AppName,
 			)),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx, err := client.GetClientQueryContext(cmd)
-			if err != nil {
-				return err
-			}
-
-			scopeUUID := strings.ToLower(strings.TrimSpace(args[0]))
-
-			queryClient := types.NewQueryClient(clientCtx)
-			res, err := queryClient.Scope(context.Background(), &types.ScopeRequest{ScopeUuid: scopeUUID})
-			if err != nil {
-				return err
-			}
-
-			return clientCtx.PrintProto(res.Scope)
+			return scopeByUUID(cmd, strings.ToLower(strings.TrimSpace(args[0])))
 		},
 	}
 
 	flags.AddQueryFlagsToCmd(cmd)
 
 	return cmd
+}
+
+func scopeByUUID(cmd *cobra.Command, scopeUUID string) error {
+	clientCtx, err := client.GetClientQueryContext(cmd)
+	if err != nil {
+		return err
+	}
+
+	queryClient := types.NewQueryClient(clientCtx)
+	res, err := queryClient.Scope(context.Background(), &types.ScopeRequest{ScopeUuid: scopeUUID})
+	if err != nil {
+		return err
+	}
+
+	return clientCtx.PrintProto(res.Scope)
 }
