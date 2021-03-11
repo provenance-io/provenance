e15df5d2fe9d34f125141e42e01ce15466999e72
Phil Story <mtps@users.noreply.github.com>
2021-03-11 12:44:21 -0700

    Enhance upgrade handlers and add 0.2.1 upgrade handler. (#160)
    * Add app to params passed to upgrader plans
    * Add default param for new denom restrictions #84
    * Move upgrades into app package to avoid import cycles
    
    Co-authored-by: Ira Miller <ira@elementalrain.com>

--- app/upgrades.go
+++ app/upgrades.go
@@ -1,12 +1,12 @@
-package upgrades
+package app
 
 import (
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	upgradekeeper "github.com/cosmos/cosmos-sdk/x/upgrade/keeper"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
-	"github.com/tendermint/tendermint/libs/log"
+
+	markertypes "github.com/provenance-io/provenance/x/marker/types"
 )
 
 var (
@@ -15,20 +15,30 @@ var (
 	}
 )
 
+type appUpgradeHandler = func(*App, sdk.Context, upgradetypes.Plan)
+
 type appUpgrade struct {
 	Added   []string
 	Deleted []string
 	Renamed []storetypes.StoreRename
-	Handler upgradetypes.UpgradeHandler
+	Handler appUpgradeHandler
 }
 
 var handlers = map[string]appUpgrade{
 	"v0.2.0": {},
+	"v0.2.1": {
+		Handler: func(app *App, ctx sdk.Context, plan upgradetypes.Plan) {
+			existing := app.MarkerKeeper.GetParams(ctx)
+			existing.UnrestrictedDenomRegex = markertypes.DefaultUnrestrictedDenomRegex
+			app.MarkerKeeper.SetParams(ctx, existing)
+		},
+	},
 
 	// TODO - Add new upgrade definitions here.
 }
 
-func CustomUpgradeStoreLoader(logger log.Logger, keeper upgradekeeper.Keeper, info storetypes.UpgradeInfo) baseapp.StoreLoader {
+// CustomUpgradeStoreLoader provides upgrade handlers for store and application module upgrades at specified versions
+func CustomUpgradeStoreLoader(app *App, info storetypes.UpgradeInfo) baseapp.StoreLoader {
 	// Register all explicit appUpgrades
 	for name, upgrade := range handlers {
 		// If the handler has been defined, add it here, otherwise, use no-op.
@@ -36,21 +46,24 @@ func CustomUpgradeStoreLoader(logger log.Logger, keeper upgradekeeper.Keeper, in
 		if upgrade.Handler == nil {
 			handler = noopHandler
 		} else {
-			handler = upgrade.Handler
+			ref := upgrade
+			handler = func(ctx sdk.Context, plan upgradetypes.Plan) {
+				ref.Handler(app, ctx, plan)
+			}
 		}
-		keeper.SetUpgradeHandler(name, handler)
+		app.UpgradeKeeper.SetUpgradeHandler(name, handler)
 	}
 
 	for name, upgrade := range handlers {
 		// If the plan is executing this block, set the store locator to create any
 		// missing modules, delete unused modules, or rename any keys required in the plan.
-		if info.Name == name && !keeper.IsSkipHeight(info.Height) {
+		if info.Name == name && !app.UpgradeKeeper.IsSkipHeight(info.Height) {
 			storeUpgrades := storetypes.StoreUpgrades{
 				Added:   upgrade.Added,
 				Renamed: upgrade.Renamed,
 				Deleted: upgrade.Deleted,
 			}
-			logger.Info("Store upgrades", "plan", name, "height", info.Height, "upgrade", upgrade)
+			app.Logger().Info("Store upgrades", "plan", name, "height", info.Height, "upgrade", upgrade)
 			return upgradetypes.UpgradeStoreLoader(info.Height, &storeUpgrades)
 		}
 	}
