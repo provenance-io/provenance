a332f484e7d99fedff36a62918b9db64907c6a67
Phil Story <mtps@users.noreply.github.com>
2021-03-11 09:49:20 -0700

    Add app to params passed to upgrader plans

--- app/upgrades/upgrades.go
+++ app/upgrades/upgrades.go
@@ -4,9 +4,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	upgradekeeper "github.com/cosmos/cosmos-sdk/x/upgrade/keeper"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
-	"github.com/tendermint/tendermint/libs/log"
+	"github.com/provenance-io/provenance/app"
 )
 
 var (
@@ -15,20 +14,26 @@ var (
 	}
 )
 
+type appUpgradeHandler = func(*app.App, sdk.Context, upgradetypes.Plan)
+
 type appUpgrade struct {
 	Added   []string
 	Deleted []string
 	Renamed []storetypes.StoreRename
-	Handler upgradetypes.UpgradeHandler
+	Handler appUpgradeHandler
 }
 
 var handlers = map[string]appUpgrade{
 	"v0.2.0": {},
+	"v0.2.1": {
+		Handler: func(app *app.App, ctx sdk.Context, plan upgradetypes.Plan){
+		},
+	},
 
 	// TODO - Add new upgrade definitions here.
 }
 
-func CustomUpgradeStoreLoader(logger log.Logger, keeper upgradekeeper.Keeper, info storetypes.UpgradeInfo) baseapp.StoreLoader {
+func CustomUpgradeStoreLoader(app *app.App, info storetypes.UpgradeInfo) baseapp.StoreLoader {
 	// Register all explicit appUpgrades
 	for name, upgrade := range handlers {
 		// If the handler has been defined, add it here, otherwise, use no-op.
@@ -36,21 +41,26 @@ func CustomUpgradeStoreLoader(logger log.Logger, keeper upgradekeeper.Keeper, in
 		if upgrade.Handler == nil {
 			handler = noopHandler
 		} else {
-			handler = upgrade.Handler
+			handler = func(ctx sdk.Context, plan upgradetypes.Plan) {
+
+				upgrade.Handler(app, ctx, plan)
+			}
 		}
-		keeper.SetUpgradeHandler(name, handler)
+		app.UpgradeKeeper.SetUpgradeHandler(name, handler)
 	}
+
+
 
 	for name, upgrade := range handlers {
 		// If the plan is executing this block, set the store locator to create any
 		// missing modules, delete unused modules, or rename any keys required in the plan.
-		if info.Name == name && !keeper.IsSkipHeight(info.Height) {
+		if info.Name == name && !app.UpgradeKeeper.IsSkipHeight(info.Height) {
 			storeUpgrades := storetypes.StoreUpgrades{
 				Added:   upgrade.Added,
 				Renamed: upgrade.Renamed,
 				Deleted: upgrade.Deleted,
 			}
-			logger.Info("Store upgrades", "plan", name, "height", info.Height, "upgrade", upgrade)
+			app.Logger().Info("Store upgrades", "plan", name, "height", info.Height, "upgrade", upgrade)
 			return upgradetypes.UpgradeStoreLoader(info.Height, &storeUpgrades)
 		}
 	}
