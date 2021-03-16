d713e362b2c0b30cbc8a62528eca6c596e09dac0
Carlton Hanna <channa@figure.com>
2021-03-16 10:52:42 -0600

    Move p8e specific functions to p8e file

---
+++ x/metadata/types/p8e.go
@@ -0,0 +1,100 @@
+package types
+
+import (
+	"encoding/base64"
+	fmt "fmt"
+
+	"github.com/google/uuid"
+	p8e "github.com/provenance-io/provenance/x/metadata/types/p8e"
+)
+
+func ConvertContractSpec(old *p8e.ContractSpec, owners []string) (
+	newSpec ContractSpecification,
+	newRecords []RecordSpecification,
+	err error,
+) {
+	raw, err := base64.StdEncoding.DecodeString(old.Definition.ResourceLocation.Ref.Hash)
+	if err != nil {
+		return newSpec, nil, err
+	}
+	specUUID, err := uuid.FromBytes(raw[0:16])
+	if err != nil {
+		return newSpec, nil, err
+	}
+	id := ContractSpecMetadataAddress(specUUID)
+
+	parties := make([]PartyType, len(old.PartiesInvolved))
+	for i := range old.PartiesInvolved {
+		parties[i] = PartyType(int32(old.PartiesInvolved[i]))
+	}
+
+	newSpec = ContractSpecification{
+		SpecificationId: id,
+		Description: &Description{
+			Name:        old.Definition.Name,
+			Description: old.Definition.ResourceLocation.Classname,
+		},
+		PartiesInvolved: parties,
+		OwnerAddresses:  owners,
+		Source: &ContractSpecification_Hash{
+			Hash: old.Definition.ResourceLocation.Ref.Hash,
+		},
+		ClassName: old.Definition.ResourceLocation.Classname,
+	}
+
+	newRecords = make([]RecordSpecification, len(old.ConsiderationSpecs))
+	for i := range old.ConsiderationSpecs {
+		recordInputs, err := convertInputSpecs(old.ConsiderationSpecs[i].InputSpecs)
+		if err != nil {
+			return newSpec, nil, err
+		}
+		specUUID, err := newSpec.SpecificationId.ContractSpecUUID()
+		if err != nil {
+			return newSpec, nil, err
+		}
+		recordSpecID := RecordSpecMetadataAddress(specUUID, old.ConsiderationSpecs[i].OutputSpec.Spec.Name)
+		newRecords[i] = RecordSpecification{
+			SpecificationId:    recordSpecID,
+			Name:               old.ConsiderationSpecs[i].OutputSpec.Spec.Name,
+			TypeName:           old.ConsiderationSpecs[i].OutputSpec.Spec.ResourceLocation.Classname,
+			ResultType:         DefinitionType(old.ConsiderationSpecs[i].OutputSpec.Spec.Type),
+			Inputs:             recordInputs,
+			ResponsibleParties: []PartyType{PartyType(old.ConsiderationSpecs[i].ResponsibleParty)},
+		}
+	}
+
+	return newSpec, newRecords, nil
+}
+
+// converts a v39 DefinitionSpec used for inputs into a v40 input specification.
+func convertInputSpecs(old []*p8e.DefinitionSpec) (inputs []*InputSpecification, err error) {
+	inputs = make([]*InputSpecification, len(old))
+	for i, oldInput := range old {
+		if oldInput.ResourceLocation.Ref.ScopeUuid != nil &&
+			len(oldInput.ResourceLocation.Ref.ScopeUuid.Value) > 0 {
+			scopeUUID, err := uuid.Parse(oldInput.ResourceLocation.Ref.ScopeUuid.Value)
+			if err != nil {
+				return nil, err
+			}
+			if len(oldInput.ResourceLocation.Ref.Name) < 1 {
+				return nil, fmt.Errorf("must have a value for record name")
+			}
+			inputs[i] = &InputSpecification{
+				Name:     oldInput.Name,
+				TypeName: oldInput.ResourceLocation.Classname,
+				Source: &InputSpecification_RecordId{
+					RecordId: RecordMetadataAddress(scopeUUID, oldInput.ResourceLocation.Ref.Name),
+				},
+			}
+		} else {
+			inputs[i] = &InputSpecification{
+				Name:     oldInput.Name,
+				TypeName: oldInput.ResourceLocation.Classname,
+				Source: &InputSpecification_Hash{
+					Hash: oldInput.ResourceLocation.Ref.Hash,
+				},
+			}
+		}
+	}
+	return
+}
