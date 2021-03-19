fb1bc37784194148e581f653077e97a5b982a124
Ira Miller <ira@elementalrain.com>
2021-03-19 11:40:30 -0600

    Add name module params testing

--- x/name/types/params.go
+++ x/name/types/params.go
@@ -8,9 +8,9 @@ import (
 
 // Default parameter namespace
 const (
-	DefaultMinSegmentLength       = 2
-	DefaultMaxSegmentLength       = 32
-	DefaultMaxSegments            = 16
+	DefaultMinSegmentLength       = uint32(2)
+	DefaultMaxSegmentLength       = uint32(32)
+	DefaultMaxSegments            = uint32(16)
 	DefaultAllowUnrestrictedNames = true
 )
 
@@ -64,6 +64,42 @@ func DefaultParams() Params {
 		DefaultMaxSegments,
 		DefaultAllowUnrestrictedNames,
 	)
+}
+
+// Equal returns true if the given value is equivalent to the current instance of params
+func (p *Params) Equal(that interface{}) bool {
+	if that == nil {
+		return p == nil
+	}
+
+	that1, ok := that.(*Params)
+	if !ok {
+		that2, ok := that.(Params)
+		if ok {
+			that1 = &that2
+		} else {
+			return false
+		}
+	}
+	if that1 == nil {
+		return p == nil
+	} else if p == nil {
+		return false
+	}
+	if p.AllowUnrestrictedNames != that1.AllowUnrestrictedNames {
+		return false
+	}
+	if p.MaxNameLevels != that1.MaxNameLevels {
+		return false
+	}
+	if p.MaxSegmentLength != that1.MaxSegmentLength {
+		return false
+	}
+	if p.MinSegmentLength != that1.MinSegmentLength {
+		return false
+	}
+
+	return true
 }
 
 func validateIntParam(i interface{}) error {
