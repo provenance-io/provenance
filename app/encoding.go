98440158eb9ccdf48415454c7203920f638ab817
Ira Miller <ira@elementalrain.com>
2021-03-23 13:17:52 -0600

    fix encoding config wiring

--- app/encoding.go
+++ app/encoding.go
@@ -6,9 +6,12 @@ import (
 	"github.com/provenance-io/provenance/app/params"
 )
 
-// MakeEncodingConfig creates an EncodingConfig for testing
+// MakeTestEncodingConfig creates an EncodingConfig for testing. This function
+// should be used only in tests or when creating a new app instance (NewApp*()).
+// App user shouldn't create new codecs - use the app.AppCodec instead.
+// [DEPRECATED]
 func MakeEncodingConfig() params.EncodingConfig {
-	encodingConfig := params.MakeEncodingConfig()
+	encodingConfig := params.MakeTestEncodingConfig()
 	std.RegisterLegacyAminoCodec(encodingConfig.Amino)
 	std.RegisterInterfaces(encodingConfig.InterfaceRegistry)
 	ModuleBasics.RegisterLegacyAminoCodec(encodingConfig.Amino)
