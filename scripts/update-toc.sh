011a5ccbe886698a8747a2f14a37c8f181e0a406
Daniel Wedul <dwedul@figure.com>
2021-04-13 14:17:06 -0600

    [224] Split out the generate-toc piece into its own script and have update-toc just use that.

--- scripts/update-toc.sh
+++ scripts/update-toc.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# This file can either be executed or sourced.
+# If sourced, the update_toc function will be added to the environment.
+# If executed, it will be run using the provided parameters (provide --help for usage).
+# This depends on the generate-toc.sh file being in the same directory.
+
+# Determine if this script was invoked by being executed or sourced.
+( [[ -n "$ZSH_EVAL_CONTEXT" && "$ZSH_EVAL_CONTEXT" =~ :file$ ]] \
+  || [[ -n "$KSH_VERSION" && $(cd "$(dirname -- "$0")" && printf '%s' "${PWD%/}/")$(basename -- "$0") != "${.sh.file}" ]] \
+  || [[ -n "$BASH_VERSION" ]] && (return 0 2>/dev/null) \
+) && update_toc_sourced='YES' || update_toc_sourced='NO'
 
 __update_toc_usage () {
   cat << EOF
@@ -46,10 +56,10 @@ TOC comment line to index all headings:
 EOF
 }
 
-TOC_LOC_REGEX="^[[:space:]]*<\!--[[:space:]]*TOC.*-->"
-
-# Usage: __update_toc "<filename>"
-__update_toc () {
+# Updates (or adds) a TOC in a markdown file.
+# Usage: update_toc "<filename>"
+# See __update_toc_usage for details.
+update_toc () {
   local filename
   filename="$1"
   shift
@@ -59,31 +69,32 @@ __update_toc () {
     return 1
   fi
 
-  local tempfile has_toc_loc line toc_params toc_included in_old_toc
+  local toc_loc_regex tempfile has_toc_loc line toc_params toc_included in_old_toc
+  toc_loc_regex="^[[:space:]]*<\!--[[:space:]]*TOC.*-->"
   tempfile="$( mktemp -t "$( sed 's/\//-/g' <<< 'x/metadata/spec/03_messages.md' )" )"
-  has_toc_loc="$( grep -q "$TOC_LOC_REGEX" "$filename" && printf 'YES' )"
+  has_toc_loc="$( grep -q "$toc_loc_regex" "$filename" && printf 'YES' )"
 
   while IFS= read -r line; do
     if [[ -n "$in_old_toc" ]] && ! grep -q "^[[:space:]]*- \[" <<< "$line"; then
       in_old_toc=''
     fi
-    if [[ -n "$has_toc_loc" ]] && grep -q "$TOC_LOC_REGEX" <<< "$line"; then
+    if [[ -n "$has_toc_loc" ]] && grep -q "$toc_loc_regex" <<< "$line"; then
       # Get just the TOC comment piece,
       # replace all non-digits with spaces, trim leading and trailing spaces.
       # Expected result examples: "", "3", "1 3"
       toc_params="$(
-        grep -o "$TOC_LOC_REGEX" <<< "$line" \
+        grep -o "$toc_loc_regex" <<< "$line" \
         | sed -E 's/[^[:digit:]]+/ /g; s/^[[:space:]]+//; s/[[:space:]]+$//;'
       )"
 
       printf '%s\n' "$line" >> "$tempfile"
       # Note: $toc_params is specifically not in quotes so that if it's zero or two numbers, it becomes zero or two args.
-      __generate_toc "$filename" $toc_params >> "$tempfile"
+      generate_toc "$filename" $toc_params >> "$tempfile"
       toc_included='YES'
       in_old_toc='YES'
     elif [[ -z "$toc_included" && -z "$has_toc_loc" ]] && grep -q '^##' <<< "$line"; then
       printf '<!-- TOC -->\n' >> "$tempfile"
-      __generate_toc "$filename" >> "$tempfile"
+      generate_toc "$filename" >> "$tempfile"
       printf '\n\n\n' >> "$tempfile"
       printf '%s\n' "$line" >> "$tempfile"
       toc_included='YES'
@@ -94,7 +105,7 @@ __update_toc () {
 
   if [[ -z "$toc_included" ]]; then
       printf '<!-- TOC -->\n' >> "$tempfile"
-      __generate_toc "$filename" >> "$tempfile"
+      generate_toc "$filename" >> "$tempfile"
       printf '\n' >> "$tempfile"
   fi
 
@@ -103,79 +114,43 @@ __update_toc () {
   rm "$tempfile"
 }
 
-# Creates the actual table of contents text.
-# Usage: __generate_toc "<filename>" [<max level>|<min level> <max level>]
-# <filename> is the file to make the table of contents for
-# The min and max levels are optional.
-# If exactly one is provided, it is treated as the max level.
-# If two are provided, the smallest is the min and the largest is the max.
-# Default min level is 2.
-# Default max level is 3.
-# Both have a minimum value of 1 and a maximum value of 5.
-# These represent the number of pound signs in the heading markdown.
-__generate_toc () {
-  local filename level
-  filename="$1"
-  shift
-  if [[ "$#" -ge '2' ]]; then
-      level1=$1
-      level2=$2
-      shift
-      shift
-  elif [[ "$#" -ge '1' ]]; then
-      level2=$1
-      shift
-      if [[ "$level2" -lt '2' ]]; then
-        level1=$level2
-      else
-        level1=2
-      fi
-  else
-    level1=2
-    level2=3
+__source_generate_toc () {
+  local source_file
+  source_file="$( cd "$( dirname "${BASH_SOURCE:-$0}" )"; pwd -P )/generate-toc.sh"
+  if [[ ! -f "$source_file" ]]; then
+    printf 'File not found: %s\n' "$source_file" >&2
+    return 1
   fi
-  if [[ "$level1" -lt '1' ]]; then
-    level1=1
-  elif [[ "$level1" -gt '5' ]]; then
-    level1=5
-  fi
-  if [[ "$level2" -lt '1' ]]; then
-    level2=1
-  elif [[ "$level2" -gt '5' ]]; then
-    level2=5
-  fi
-  if [[ "$level1" -gt "$level2" ]]; then
-    local temp
-    temp=$level1
-    level1=$level2
-    level2=$temp
-  fi
-  # Use Grep to get all heading lines up to the desired level.
-  # Use sed to remove any pound below the min level.
-  # Use sed to delimit the markdown from the heading, and a duplicate of the heading
-  # Use awk to do some conversion on each piece and output each final entry line.
-  grep -E "^#{${level1},${level2}} " "$filename" \
-  | sed "s/^#\{${level1}\}/#/" \
-  | sed -E 's/(#+) (.+)/\1~\2~\2/g' \
-  | awk -F "~" \
-      '{
-          gsub(/#/,"  ",$1);
-          gsub(/[^[:alnum:]_]/,"-",$3);
-          print $1 "- [" $2 "](#" tolower($3) ")";
-        }'
+  source "$source_file"
 }
 
-if [[ "$#" -eq 0 ]]; then
-  __update_toc_usage
-  exit 0
+if [[ "$update_toc_sourced" == 'YES' ]]; then
+  __source_generate_toc || return $?
+else
+  __source_generate_toc || exit $?
 fi
 
-for a in "$@"; do
-  if [[ "$a" == '-h' || "$a" == '--help' || "$a" == "help" ]]; then
+# If not sourced, do the stuff!
+if [[ "$update_toc_sourced" != 'YES' ]]; then
+  if [[ "$#" -eq 0 ]]; then
     __update_toc_usage
     exit 0
   fi
-done
 
-__update_toc "$@"
-exit $?
+  for a in "$@"; do
+    if [[ "$a" == '-h' || "$a" == '--help' || "$a" == "help" ]]; then
+      __update_toc_usage
+      exit 0
+    fi
+  done
+
+  update_toc "$@"
+  exit $?
+fi
+
+# It was sourced, clean up some environment stuff.
+unset update_toc_sourced
+unset -f __update_toc_usage
+unset -f __source_generate_toc
+
+return 0
