04aa11819c7eefe6917e5b1e20a95a7a5079415b
Ira Miller <72319+iramiller@users.noreply.github.com>
2021-04-30 10:29:16 -0600

    Update module event handler scope (#289)
    * Update module event handler scope
    
    * changelog
    
    * Fix invalid tests to look in response for events

--- x/attribute/handler.go
+++ x/attribute/handler.go
@@ -13,6 +13,8 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 	msgServer := keeper.NewMsgServerImpl(k)
 
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
+		ctx = ctx.WithEventManager(sdk.NewEventManager())
+
 		switch msg := msg.(type) {
 		case *types.MsgAddAttributeRequest:
 			res, err := msgServer.AddAttribute(sdk.WrapSDKContext(ctx), msg)
