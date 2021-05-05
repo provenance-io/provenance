652cdafda57a0843b7603e79e415f3dc97070fe0
Ira Miller <72319+iramiller@users.noreply.github.com>
2021-05-04 19:30:52 -0600

    Update tracing_meter.go
    Remove extra space from suggested change.

--- internal/antewrapper/tracing_meter.go
+++ internal/antewrapper/tracing_meter.go
@@ -31,7 +31,7 @@ func NewTracingMeterWrapper(logger log.Logger, baseMeter sdkgas.GasMeter) sdkgas
 	}
 }
 
-var _  sdkgas.GasMeter = &tracingGasMeter{}
+var _ sdkgas.GasMeter = &tracingGasMeter{}
 
 // GasConsumed reports the amount of gas consumed at Log.Info level
 func (g *tracingGasMeter) GasConsumed() sdkgas.Gas {
