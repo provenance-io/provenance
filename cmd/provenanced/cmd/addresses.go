2be759ac788ab10f9f8a00d923f96653ba0bdf0a
Daniel Wedul <dwedul@figure.com>
2021-05-20 12:34:31 -0600

    [321]: Refactor AddMetaAddressEncoder. Add support for record specs and add some extra help stuff. Also fix the argument count issue.

--- cmd/provenanced/cmd/addresses.go
+++ cmd/provenanced/cmd/addresses.go
@@ -2,12 +2,19 @@ package cmd
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/version"
 	"github.com/google/uuid"
 	"github.com/spf13/cobra"
 
 	"github.com/provenance-io/provenance/x/metadata/types"
+)
+
+var (
+	cmdStart = fmt.Sprintf("%s metaaddress", version.AppName)
 )
 
 // GetQueryCmd is the top-level command for name CLI queries.
@@ -93,55 +100,75 @@ Excess (hex): %s
 // AddMetaAddressEncoder returns metadata address encoder cobra Command.
 func AddMetaAddressEncoder() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "encode [type] [uuid] [uuid|name]",
+		Use:   "encode type uuid [uuid|name]",
 		Short: "Encodes metadata uuids to bech32 address for specific type",
-		Long: `Encodes metadata uuids to bech32 address for specific type. 
-		Types: scope,session,record,contract-specification,scope-specification`,
-		Args: cobra.RangeArgs(3, 4),
+		Long: fmt.Sprintf(`Encodes metadata uuids to bech32 address for specific type.
+
+%[1]s encode type uuid [uuid|name]
+
+Types: scope session record scope-specification contract-specification record-specification
+
+The type and first uuid argument are required.
+The third [uuid|name] argument is either required or forbidden based on the type.
+
+These types forbid a third argument: scope scope-specification contract-specification
+These types require a third argument: session record record-specification
+This type requires the third argument to be a UUID: session`, cmdStart),
+		Example: fmt.Sprintf(`%[1]s encode scope 91978ba2-5f35-459a-86a7-feca1b0512e0
+%[1]s encode session 91978ba2-5f35-459a-86a7-feca1b0512e0 5803f8bc-6067-4eb5-951f-2121671c2ec0
+%[1]s encode record 91978ba2-5f35-459a-86a7-feca1b0512e0 recordname
+%[1]s encode scope-specification dc83ea70-eacd-40fe-9adf-1cf6148bf8a2
+%[1]s encode contract-specification def6bc0a-c9dd-4874-948f-5206e6060a84
+%[1]s encode record-specification def6bc0a-c9dd-4874-948f-5206e6060a84 recordname`, cmdStart),
+		Args: cobra.RangeArgs(2, 3),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			primaryUUID, err := uuid.Parse(args[2])
+			addrType := strings.ToLower(regexp.MustCompile("[^[:alpha:]]+").ReplaceAllString(args[0], ""))
+			primaryUUID, err := uuid.Parse(args[1])
 			if err != nil {
 				return err
 			}
-			switch addrType := args[1]; addrType {
+			uuidOrNameArg := args[2]
+			var addr types.MetadataAddress
+			switch addrType {
 			case "scope":
-				if len(args) != 3 {
+				if len(args) != 2 {
 					return fmt.Errorf("too many arguments for %s address encoder", addrType)
 				}
-				scopeAddr := types.ScopeMetadataAddress(primaryUUID)
-				fmt.Fprint(cmd.OutOrStdout(), scopeAddr.String())
+				addr = types.ScopeMetadataAddress(primaryUUID)
 			case "session":
-				if len(args) != 4 {
-					return fmt.Errorf("missing secondary uuid for type session")
+				if len(args) != 3 {
+					return fmt.Errorf("not enough arguments for %s address encoder", addrType)
 				}
-				secondaryUUID, err := uuid.Parse(args[3])
+				secondaryUUID, err := uuid.Parse(uuidOrNameArg)
 				if err != nil {
 					return err
 				}
-				sessionAddr := types.SessionMetadataAddress(primaryUUID, secondaryUUID)
-				fmt.Fprint(cmd.OutOrStdout(), sessionAddr.String())
+				addr = types.SessionMetadataAddress(primaryUUID, secondaryUUID)
 			case "record":
-				if len(args) != 4 {
-					return fmt.Errorf("missing name for type record")
-				}
-				recordAddr := types.RecordMetadataAddress(primaryUUID, args[3])
-				fmt.Fprint(cmd.OutOrStdout(), recordAddr.String())
-			case "contract-specification":
 				if len(args) != 3 {
-					return fmt.Errorf("too many arguments for %s address encoder", addrType)
+					return fmt.Errorf("not enough arguments for %s address encoder", addrType)
 				}
-				contractSpecAddr := types.ContractSpecMetadataAddress(primaryUUID)
-				fmt.Fprint(cmd.OutOrStdout(), contractSpecAddr.String())
-			case "scope-specification":
+				addr = types.RecordMetadataAddress(primaryUUID, uuidOrNameArg)
+			case "scopespecification", "scopespec":
+				if len(args) != 2 {
+					return fmt.Errorf("too many arguments for %s address encoder", "scope-specification")
+				}
+				addr = types.ScopeSpecMetadataAddress(primaryUUID)
+			case "contractspecification", "contractspec", "cspec":
+				if len(args) != 2 {
+					return fmt.Errorf("too many arguments for %s address encoder", "contract-specification")
+				}
+				addr = types.ContractSpecMetadataAddress(primaryUUID)
+			case "recordspecification", "recordspec", "recspec":
 				if len(args) != 3 {
-					return fmt.Errorf("too many arguments for %s address encoder", addrType)
+					return fmt.Errorf("not enough arguments for %s address encoder", "record-specification")
 				}
-				scopeSpecAddr := types.ScopeSpecMetadataAddress(primaryUUID)
-				fmt.Fprint(cmd.OutOrStdout(), scopeSpecAddr.String())
+				addr = types.RecordMetadataAddress(primaryUUID, uuidOrNameArg)
 			default:
-				return fmt.Errorf("unknown type: %s, Supported types: scope, session, record, contract-specification, scope-specification", addrType)
+				return fmt.Errorf("unknown type: %s, Supported types: scope session record scope-specification contract-specification record-specification", addrType)
 			}
-			return nil
+			_, cmdErr := fmt.Fprintf(cmd.OutOrStdout(), "%s\n", addr)
+			return cmdErr
 		},
 	}
 	return cmd
