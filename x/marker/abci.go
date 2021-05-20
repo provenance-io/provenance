3e2e38814999b8efc8a382bfe1c85f69ae675ee4
Ira Miller <ira@elementalrain.com>
2021-05-20 10:20:19 -0600

    adjust logging levels in mint/burn add telemetry to abci in marker add error log to abci fixed supply check

--- x/marker/abci.go
+++ x/marker/abci.go
@@ -1,6 +1,10 @@
 package marker
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/cosmos/cosmos-sdk/telemetry"
 	abci "github.com/tendermint/tendermint/abci/types"
 
 	"github.com/provenance-io/provenance/x/marker/keeper"
@@ -12,6 +16,7 @@ import (
 
 // BeginBlocker returns the begin blocker for the marker module.
 func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k keeper.Keeper, bk bankkeeper.Keeper) {
+	defer telemetry.ModuleMeasureSince(types.ModuleName, time.Now(), telemetry.MetricKeyBeginBlocker)
 	// Iterate through all marker accounts and check for supply above or below expected targets.
 	var err error
 	k.IterateMarkers(ctx, func(record types.MarkerAccountI) bool {
@@ -22,6 +27,9 @@ func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k keeper.Keeper, 
 
 			// If the current amount of marker coin in circulation doesn't match configured supply, make adjustments
 			if !requiredSupply.IsEqual(currentSupply) {
+				ctx.Logger().Error(
+					fmt.Sprintf("Current %s supply is NOT at the required amount, adjusting %s to required supply level",
+						record.GetDenom(), currentSupply))
 				err = k.AdjustCirculation(ctx, record, requiredSupply)
 			}
 			// else supply is equal, nothing to do here.
