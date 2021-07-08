0184a363c08177f4480c2bbec4cc5d4df79159f4
Carlton Hanna <channa@figure.com>
2021-07-08 10:14:28 -0600

    Wire in migrations for metadata

--- x/metadata/keeper/migrations.go
+++ x/metadata/keeper/migrations.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	v042 "github.com/provenance-io/provenance/x/metadata/legacy/v042"
 )
 
 // Migrator is a struct for handling in-place store migrations.
@@ -16,5 +17,5 @@ func NewMigrator(keeper Keeper) Migrator {
 
 // Migrate1to2 migrates from version 1 to 2 to convert attribute keys from 20 to 32 length
 func (m *Migrator) Migrate1to2(ctx sdk.Context) error {
-	return nil
+	return v042.MigrateAddresses(ctx, m.keeper.storeKey, m.keeper.cdc)
 }
