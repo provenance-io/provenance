1efdc12f2538c476a55aa8237e902d20e1be825d
fkneeland <fkneeland@figure.com>
2021-08-16 10:26:47 -0600

    trying to figure things out with new user

--- client/rosetta/configuration/send_funds.sh
+++ client/rosetta/configuration/send_funds.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 set -e
-addr=$(provenanced -t keys show fd -a --keyring-backend=test)
+addr=$(provenanced -t keys show testUser -a --keyring-backend=test)
 echo "12345678" | provenanced -t tx bank send "$addr" "$1" 100nhash --chain-id="testing" --node tcp://provenance:26657 --yes --keyring-backend=test
