69bd06af017627525abf78661f577581655e740b
Daniel Wedul <dwedul@figure.com>
2021-08-16 15:30:28 -0600

    [403]: Handle some special cases and finish up with the rest of the types.

--- cmd/provenanced/cmd/config.go
+++ cmd/provenanced/cmd/config.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"path/filepath"
@@ -8,6 +9,7 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 
 	provconfig "github.com/provenance-io/provenance/cmd/provenanced/config"
 	"github.com/spf13/cobra"
@@ -18,6 +20,8 @@ import (
 
 	tmconfig "github.com/tendermint/tendermint/config"
 )
+
+const entryNotFound = -1
 
 // Cmd returns a CLI command to interactively create an application CLI
 // config file.
@@ -68,6 +72,7 @@ func runConfigGetCmd(cmd *cobra.Command, args []string) error {
 	if len(args) == 0 {
 		args = append(args, "all")
 	}
+	toOutput := map[string]reflect.Value{}
 	for _, key := range args {
 		switch key {
 		case "all":
@@ -93,19 +98,18 @@ func runConfigGetCmd(cmd *cobra.Command, args []string) error {
 			cmd.Println("------------------")
 			cmd.Println(getFieldMapString(clientFields))
 		default:
-			v, ok := appFields[key]
-			if !ok {
-				v, ok = tmFields[key]
-			}
-			if !ok {
-				v, ok = clientFields[key]
-			}
-			if ok {
-				cmd.Println(getKVString(key, v))
-			} else {
+			entries := findEntries(key, appFields, tmFields, clientFields)
+			if len(entries) == 0 {
 				cmd.Printf("Configuration key %s does not exist.\n", key)
+			} else {
+				for k, v := range entries {
+					toOutput[k] = v
+				}
 			}
 		}
+	}
+	if len(toOutput) > 0 {
+		cmd.Println(getFieldMapString(toOutput))
 	}
 	return nil
 }
@@ -139,29 +143,43 @@ func runConfigSetCmd(cmd *cobra.Command, args []string) error {
 	}
 	issueFound := false
 	appUpdated, tmUpdated, clientUpdated := false, false, false
+	configPath := filepath.Join(client.GetClientContextFromCmd(cmd).HomeDir, "config")
 	for i, key := range keys {
-		v, ok := appFields[key]
-		if ok {
-			appUpdated = true
-		} else {
-			v, ok = tmFields[key]
-			if ok {
-				tmUpdated = true
-			} else {
-				v, ok = clientFields[key]
-				if ok {
-					clientUpdated = true
-				}
-			}
+		// Bug: As of Cosmos 0.43 (and 2021-08-16), the app config's index-events configuration value isn't properly marshalled into the config.
+		// For example,
+		//   appConfig.IndexEvents = []string{"a", "b"}
+		//   serverconfig.WriteConfigFile(filename, appConfig)
+		// works without error but the configuration file will have
+		//   index-events = [a b]
+		// instead of what is needed:
+		//   index-events = ["a", "b"]
+		// This results in that config file being invalid and no longer loadable:
+		//   failed to merge configuration: While parsing config: (61, 17): no value can start with a
+		// So for now, if someone requests the setting of that field, return an error with some helpful info.
+		if key == "index-events" {
+			cmd.Printf("The index-events list cannot be set with this command. It can be manually updated in %s\n",
+				filepath.Join(configPath, "app.toml"))
+			issueFound = true
+			continue
 		}
-		if !ok {
+		v, foundIn := findEntry(key, appFields, tmFields, clientFields)
+		if foundIn == entryNotFound {
 			cmd.Printf("Configuration key %s does not exist.\n", key)
 			issueFound = true
 		} else {
 			err := setValueFromString(key, v, vals[i])
 			if err != nil {
-				cmd.Printf("Error setting key %s: %v", key, err)
+				cmd.Printf("Error setting key %s: %v\n", key, err)
 				issueFound = true
+			} else {
+				switch foundIn {
+				case 0:
+					appUpdated = true
+				case 1:
+					tmUpdated = true
+				case 2:
+					clientUpdated = true
+				}
 			}
 		}
 	}
@@ -188,7 +206,6 @@ func runConfigSetCmd(cmd *cobra.Command, args []string) error {
 	if issueFound {
 		return errors.New("one or more issues encountered; no configuration values have been updated")
 	}
-	configPath := filepath.Join(client.GetClientContextFromCmd(cmd).HomeDir, "config")
 	if appUpdated {
 		configFilename := filepath.Join(configPath, "app.toml")
 		serverconfig.WriteConfigFile(configFilename, appConfig)
@@ -215,10 +232,6 @@ func getAppConfigAndMap(cmd *cobra.Command) (*serverconfig.Config, map[string]re
 		return nil, nil, err
 	}
 	fields := provconfig.GetFieldValueMap(conf, true)
-	// TODO add special handling for some cosmos config variables.
-	// These fields need some special handling.
-	// app: global-labels - [][]string (each sub-slice has 2 elements)
-	// app: index-events - []string
 
 	return conf, fields, nil
 }
@@ -231,11 +244,19 @@ func getTmConfigAndMap(cmd *cobra.Command) (*tmconfig.Config, map[string]reflect
 		return nil, nil, err
 	}
 	fields := provconfig.GetFieldValueMap(conf, true)
-	// TODO add special handling for some tendermint config variables.
-	// These fields need some special handling.
-	// tm: rpc.cors_allowed_headers - []string
-	// tm: rpc.cors_allowed_methods - []string
-	// tm: rpc.cors_allowed_origins - []string
+	// There are several fields in this map that don't correspond to entries in the config files.
+	// This info is accurate in Cosmos SDK 0.43 (on 2021-08-16).
+	// None of the "home" keys have entries in the config files:
+	// "home", "consensus.home", "mempool.home", "p2p.home", "rpc.home"
+	// There are several "p2p.test_" fields that should be ignored too.
+	// "p2p.test_dial_fail", "p2p.test_fuzz",
+	// "p2p.test_fuzz_config.*" ("maxdelay", "mode", "probdropconn", "probdroprw", "probsleep")
+	delete(fields, "home")
+	for k := range fields {
+		if (len(k) > 5 && k[len(k)-5:] == ".home") || (len(k) > 9 && k[:9] == "p2p.test_") {
+			delete(fields, k)
+		}
+	}
 
 	return conf, fields, nil
 }
@@ -248,6 +269,50 @@ func getClientConfigAndMap(cmd *cobra.Command) (*provconfig.ClientConfig, map[st
 	}
 	fields := provconfig.GetFieldValueMap(conf, true)
 	return conf, fields, nil
+}
+
+// findEntry gets the entry with the given key in one of the provided maps.
+// Maps are searched in the order provided and the first match is returned.
+// The second return value is the index of the provided map that the entry was found in (starting with 0).
+// If it's equal to entryNotFound, the entry wasn't found.
+func findEntry(key string, maps ...map[string]reflect.Value) (reflect.Value, int) {
+	for i, m := range maps {
+		if v, ok := m[key]; ok {
+			return v, i
+		}
+	}
+	return reflect.Value{}, entryNotFound
+}
+
+// findEntries gets entries that match a given key from the provided maps.
+// If the key doesn't end in a period, findEntry is used first to find an exact match.
+// If an exact match isn't found, a period is appended to the key (unless already there) and sub-entry matches are looked for.
+// E.g. Providing "filter_peers" will get just the "filter_peers" entry.
+// Providing "consensus." will bypass the exact key lookup, and return all fields that start with "consensus.".
+// Providing "consensus" will look first for a field specifically called "consensus",
+// then, if/when not found, will return all fields that start with "consensus.".
+func findEntries(key string, maps ...map[string]reflect.Value) map[string]reflect.Value {
+	rv := map[string]reflect.Value{}
+	baseKey := key
+	if len(key) == 0 {
+		return rv
+	}
+	if key[len(key)-1:] != "." {
+		if v, i := findEntry(key, maps...); i != entryNotFound {
+			rv[key] = v
+			return rv
+		}
+		baseKey+="."
+	}
+	baseKeyLen := len(baseKey)
+	for _, m := range maps {
+		for k, v := range m {
+			if len(k) > baseKeyLen && k[:baseKeyLen] == baseKey {
+				rv[k] = v
+			}
+		}
+	}
+	return rv
 }
 
 // getFieldMapString gets a multi-line string with all the keys and values in the provided map.
@@ -301,6 +366,11 @@ func getStringFromValue(v reflect.Value) string {
 		return sb.String()
 	case reflect.String:
 		return fmt.Sprintf("\"%v\"", v)
+	case reflect.Int64:
+		if v.Type().String() == "time.Duration" {
+			return fmt.Sprintf("\"%v\"", v)
+		}
+		return fmt.Sprintf("%v", v)
 	default:
 		return fmt.Sprintf("%v", v)
 	}
@@ -311,7 +381,6 @@ func getStringFromValue(v reflect.Value) string {
 // Assuming the value came from GetFieldValueMap, this will actually be updating the
 // value in the config object provided to that function.
 func setValueFromString(fieldName string, fieldVal reflect.Value, strVal string) error {
-	// TODO: Check for one of the special fields and handle them appropriately.
 	switch fieldVal.Kind() {
 	case reflect.String:
 		fieldVal.SetString(strVal)
@@ -323,8 +392,128 @@ func setValueFromString(fieldName string, fieldVal reflect.Value, strVal string)
 		}
 		fieldVal.SetBool(b)
 		return nil
-	default:
-		return fmt.Errorf("field %s cannot be set because setting values of type %s has not yet been set up", fieldName, fieldVal.Kind())
+	case reflect.Int:
+		i, err := strconv.Atoi(strVal)
+		if err != nil {
+			return err
+		}
+		fieldVal.SetInt(int64(i))
+		return nil
+	case reflect.Int64:
+		if fieldVal.Type().String() == "time.Duration" {
+			i, err := time.ParseDuration(strVal)
+			if err != nil {
+				return err
+			}
+			fieldVal.SetInt(int64(i))
+			return nil
+		}
+		i, err := strconv.ParseInt(strVal, 10, 64)
+		if err != nil {
+			return err
+		}
+		fieldVal.SetInt(i)
+		return nil
+	case reflect.Int32:
+		i, err := strconv.ParseInt(strVal, 10, 32)
+		if err != nil {
+			return err
+		}
+		fieldVal.SetInt(i)
+		return nil
+	case reflect.Int16:
+		i, err := strconv.ParseInt(strVal, 10, 16)
+		if err != nil {
+			return err
+		}
+		fieldVal.SetInt(i)
+		return nil
+	case reflect.Int8:
+		i, err := strconv.ParseInt(strVal, 10, 8)
+		if err != nil {
+			return err
+		}
+		fieldVal.SetInt(i)
+		return nil
+	case reflect.Uint, reflect.Uint64:
+		ui, err := strconv.ParseUint(strVal, 10, 64)
+		if err != nil {
+			return err
+		}
+		fieldVal.SetUint(ui)
+		return nil
+	case reflect.Uint32:
+		ui, err := strconv.ParseUint(strVal, 10, 32)
+		if err != nil {
+			return err
+		}
+		fieldVal.SetUint(ui)
+		return nil
+	case reflect.Uint16:
+		ui, err := strconv.ParseUint(strVal, 10, 16)
+		if err != nil {
+			return err
+		}
+		fieldVal.SetUint(ui)
+		return nil
+	case reflect.Uint8:
+		ui, err := strconv.ParseUint(strVal, 10, 8)
+		if err != nil {
+			return err
+		}
+		fieldVal.SetUint(ui)
+		return nil
+	case reflect.Float64:
+		f, err := strconv.ParseFloat(strVal, 64)
+		if err != nil {
+			return err
+		}
+		fieldVal.SetFloat(f)
+		return nil
+	case reflect.Float32:
+		f, err := strconv.ParseFloat(strVal, 32)
+		if err != nil {
+			return err
+		}
+		fieldVal.SetFloat(f)
+		return nil
+	case reflect.Slice:
+		switch fieldVal.Type().Elem().Kind() {
+		case reflect.String:
+			val := []string{}
+			if len(strVal) > 0 {
+				err := json.Unmarshal([]byte(strVal), &val)
+				if err != nil {
+					return err
+				}
+			}
+			fieldVal.Set(reflect.ValueOf(val))
+			return nil
+		case reflect.Slice:
+			if fieldVal.Type().Elem().Elem().Kind() == reflect.String {
+				val := [][]string{}
+				if len(strVal) > 0 {
+					err := json.Unmarshal([]byte(strVal), &val)
+					if err != nil {
+						return err
+					}
+				}
+				if fieldName == "telemetry.global-labels" {
+					// The Cosmos config ValidateBasic doesn't do this checking (as of Cosmos 0.43, 2021-08-16).
+					// If the length of a sub-slice is 0 or 1, you get a panic:
+					//   panic: template: appConfigFileTemplate:95:26: executing "appConfigFileTemplate" at <index $v 1>: error calling index: reflect: slice index out of range
+					// If the length of a sub-slice is greater than 2, everything after the first two ends up getting chopped off.
+					// e.g. trying to set it to '[["a","b","c"]]' will actually end up just setting it to '[["a","b"]]'.
+					for i, s := range val {
+						if len(s) != 2 {
+							return fmt.Errorf("invalid %s: sub-arrays must have length 2, but the sub-array at index %d has %d", fieldName, i, len(s))
+						}
+					}
+				}
+				fieldVal.Set(reflect.ValueOf(val))
+				return nil
+			}
+		}
 	}
-	// TODO: Figure out what all kinds need to be handled here, and add them.
+	return fmt.Errorf("field %s cannot be set because setting values of type %s has not yet been set up", fieldName, fieldVal.Type())
 }
