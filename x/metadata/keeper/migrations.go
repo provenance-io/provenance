edd967db97abb7be681b48c307fb8d7869cb5789
Daniel Wedul <dwedul@figure.com>
2021-10-27 16:43:14 -0600

    [541]: Create metadata migration function for fixing the indexes.

--- x/metadata/keeper/migrations.go
+++ x/metadata/keeper/migrations.go
@@ -1,9 +1,13 @@
 package keeper
 
 import (
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	v042 "github.com/provenance-io/provenance/x/metadata/legacy/v042"
+	"github.com/provenance-io/provenance/x/metadata/types"
 )
 
 // Migrator is a struct for handling in-place store migrations.
@@ -22,4 +26,94 @@ func (m *Migrator) Migrate1to2(ctx sdk.Context) error {
 	err := v042.MigrateAddresses(ctx, m.keeper.storeKey)
 	ctx.Logger().Info("Finished Migrating Metadata Module from Version 1 to 2")
 	return err
+}
+
+// Migrate2to3 migrates from version 2 to 3 to fix the indexes.
+func (m *Migrator) Migrate2to3(ctx sdk.Context) error {
+	ctx.Logger().Info("Migrating Metadata Module from Version 2 to 3")
+	steps := []struct {
+		name string
+		runner func() error
+	}{
+		{
+			name:   "Deleting indexes",
+			runner: func() error { return deleteAllIndexes(ctx, m.keeper.storeKey) },
+		},
+		{
+			name:   "Reindexing scopes",
+			runner: func() error { return reindexScopes(ctx, m.keeper) },
+		},
+		{
+			name:   "Reindexing scope specs",
+			runner: func() error { return reindexScopeSpecs(ctx, m.keeper) },
+		},
+		{
+			name:   "Reindexing contract specs",
+			runner: func() error { return reindexContractSpecs(ctx, m.keeper) },
+		},
+	}
+
+	for i, step := range steps {
+		logHeader := fmt.Sprintf("Metadata step %d of %d: %s", i+1, len(steps), step.name)
+		ctx.Logger().Info(logHeader)
+		if err := step.runner(); err != nil {
+			ctx.Logger().Error(logHeader, "error", err)
+			return err
+		}
+	}
+
+	ctx.Logger().Info("Finished Migrating Metadata Module from Version 2 to 3")
+	return nil
+}
+
+// deleteAllIndexes deletes all the indexes on Metadata entries.
+func deleteAllIndexes(ctx sdk.Context, storeKey sdk.StoreKey) error {
+	prefixes := [][]byte{
+		types.AddressScopeCacheKeyPrefix, types.ScopeSpecScopeCacheKeyPrefix,
+		types.ValueOwnerScopeCacheKeyPrefix, types.AddressScopeSpecCacheKeyPrefix,
+		types.ContractSpecScopeSpecCacheKeyPrefix, types.AddressContractSpecCacheKeyPrefix,
+	}
+
+	store := ctx.KVStore(storeKey)
+	for _, pf := range prefixes {
+		if err := deleteIndex(store, pf); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// deleteIndex deletes all the entries with a given prefix.
+func deleteIndex(store sdk.KVStore, pf []byte) error {
+	pfStore := prefix.NewStore(store, pf)
+	iter := pfStore.Iterator(nil, nil)
+	defer iter.Close()
+	for ; iter.Valid(); iter.Next() {
+		pfStore.Delete(iter.Key())
+	}
+	return nil
+}
+
+// reindexScopes indexes all scopes.
+func reindexScopes(ctx sdk.Context, mdKeeper Keeper) error {
+	return mdKeeper.IterateScopes(ctx, func(scope types.Scope) (stop bool) {
+		mdKeeper.indexScope(ctx, scope)
+		return false
+	})
+}
+
+// reindexScopeSpecs indexes all scope specifications.
+func reindexScopeSpecs(ctx sdk.Context, mdKeeper Keeper) error {
+	return mdKeeper.IterateScopeSpecs(ctx, func(spec types.ScopeSpecification) (stop bool) {
+		mdKeeper.indexScopeSpecification(ctx, spec)
+		return false
+	})
+}
+
+// reindexContractSpecs indexes all contract specifications.
+func reindexContractSpecs(ctx sdk.Context, mdKeeper Keeper) error {
+	return mdKeeper.IterateContractSpecs(ctx, func(spec types.ContractSpecification) (stop bool) {
+		mdKeeper.indexContractSpecification(ctx, spec)
+		return false
+	})
 }
