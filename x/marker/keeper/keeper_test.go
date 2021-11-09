bc963f4fe75959c858b938f61195c39bc7b2c0e6
Ira Miller <ira@elementalrain.com>
2021-11-09 09:56:47 -0700

    A bit more unit test coverage of changes.

--- x/marker/keeper/keeper_test.go
+++ x/marker/keeper/keeper_test.go
@@ -229,8 +229,12 @@ func TestAccountKeeperManageAccess(t *testing.T) {
 	require.NoError(t, err)
 
 	// Manager can make changes to grants for finalized markers
+	require.NoError(t, app.MarkerKeeper.RemoveAccess(ctx, user1, "testcoin", user1))
 	require.NoError(t, app.MarkerKeeper.AddAccess(ctx, user1, "testcoin",
-		types.NewAccessGrant(user2, []types.Access{types.Access_Mint})))
+		types.NewAccessGrant(user1, []types.Access{types.Access_Burn})))
+
+	// Unauthorized user can not manipulate finalized marker grants
+	require.Error(t, app.MarkerKeeper.RemoveAccess(ctx, user2, "testcoin", user1))
 
 	// Admin can make changes to grants for finalized markers
 	require.NoError(t, app.MarkerKeeper.AddAccess(ctx, admin, "testcoin",
