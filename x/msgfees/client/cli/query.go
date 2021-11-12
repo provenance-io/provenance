d41598f808d50cb79ed07d50d07392b06ab752c4
Carlton Hanna <channa@figure.com>
2021-11-12 10:01:02 -0700

    Add query all msg based fees command

--- x/msgfees/client/cli/query.go
+++ x/msgfees/client/cli/query.go
@@ -1,7 +1,14 @@
 package cli
 
 import (
+	"context"
+	"encoding/base64"
+	"fmt"
+
+	flag "github.com/spf13/pflag"
+
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/provenance-io/provenance/x/msgfees/types"
 	"github.com/spf13/cobra"
 )
@@ -15,6 +22,56 @@ func GetQueryCmd() *cobra.Command {
 		SuggestionsMinimumDistance: 2,
 		RunE:                       client.ValidateCmd,
 	}
-	queryCmd.AddCommand()
+	queryCmd.AddCommand(AllMsgBasedFeesCmd())
 	return queryCmd
+}
+
+// AllMsgBasedFeesCmd is the CLI command for listing all msg based fees.
+func AllMsgBasedFeesCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "list",
+		Aliases: []string{"ls", "l"},
+		Short:   "List all the msg based fees on the Provenance Blockchain",
+		Args:    cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+
+			pageReq, err := client.ReadPageRequest(withPageKeyDecoded(cmd.Flags()))
+			if err != nil {
+				return err
+			}
+
+			var response *types.QueryAllMsgBasedFeesResponse
+			if response, err = queryClient.QueryAllMsgBasedFees(
+				context.Background(),
+				&types.QueryAllMsgBasedFeesRequest{Pagination: pageReq},
+			); err != nil {
+				fmt.Printf("failed to query msg based fees: %s\n", err.Error())
+				return nil
+			}
+			return clientCtx.PrintProto(response)
+		},
+	}
+
+	flags.AddPaginationFlagsToCmd(cmd, "markers")
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// sdk ReadPageRequest expects binary but we encoded to base64 in our marshaller
+func withPageKeyDecoded(flagSet *flag.FlagSet) *flag.FlagSet {
+	encoded, err := flagSet.GetString(flags.FlagPageKey)
+	if err != nil {
+		panic(err.Error())
+	}
+	raw, err := base64.StdEncoding.DecodeString(encoded)
+	if err != nil {
+		panic(err.Error())
+	}
+	_ = flagSet.Set(flags.FlagPageKey, string(raw))
+	return flagSet
 }
