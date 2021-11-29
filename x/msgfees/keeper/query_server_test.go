df66efc9ad547339c88f96b718afaf5c6c85bf50
Carlton Hanna <channa@figure.com>
2021-11-29 11:31:24 -0700

    WIP: Adding query server test, currently failing

--- x/msgfees/keeper/query_server_test.go
+++ x/msgfees/keeper/query_server_test.go
@@ -6,8 +6,14 @@ import (
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	"github.com/cosmos/cosmos-sdk/testutil/network"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/tx/signing"
+	authsign "github.com/cosmos/cosmos-sdk/x/auth/signing"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	simapp "github.com/provenance-io/provenance/app"
+	"github.com/provenance-io/provenance/testutil"
 	"github.com/provenance-io/provenance/x/msgfees/types"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
@@ -19,31 +25,44 @@ type QueryServerTestSuite struct {
 
 	app         *simapp.App
 	ctx         sdk.Context
+	cfg         network.Config
 	queryClient types.QueryClient
 
+	privkey1  cryptotypes.PrivKey
 	pubkey1   cryptotypes.PubKey
 	user1     string
 	user1Addr sdk.AccAddress
+	acct1     authtypes.AccountI
 
+	privkey2  cryptotypes.PrivKey
 	pubkey2   cryptotypes.PubKey
 	user2     string
 	user2Addr sdk.AccAddress
+	acct2     authtypes.AccountI
 }
 
 func (s *QueryServerTestSuite) SetupTest() {
 	s.app = simapp.Setup(false)
 	s.ctx = s.app.BaseApp.NewContext(false, tmproto.Header{})
+	s.cfg = testutil.DefaultTestNetworkConfig()
 	queryHelper := baseapp.NewQueryServerTestHelper(s.ctx, s.app.InterfaceRegistry())
 	types.RegisterQueryServer(queryHelper, s.app.MsgBasedFeeKeeper)
 	s.queryClient = types.NewQueryClient(queryHelper)
 
-	s.pubkey1 = secp256k1.GenPrivKey().PubKey()
+	s.privkey1 = secp256k1.GenPrivKey()
+	s.pubkey1 = s.privkey1.PubKey()
 	s.user1Addr = sdk.AccAddress(s.pubkey1.Address())
 	s.user1 = s.user1Addr.String()
 
-	s.pubkey2 = secp256k1.GenPrivKey().PubKey()
+	s.privkey2 = secp256k1.GenPrivKey()
+	s.pubkey2 = s.privkey1.PubKey()
 	s.user2Addr = sdk.AccAddress(s.pubkey2.Address())
 	s.user2 = s.user2Addr.String()
+
+	authParams := authtypes.DefaultParams()
+	s.app.AccountKeeper.SetParams(s.ctx, authParams)
+	s.acct1 = s.app.AccountKeeper.NewAccountWithAddress(s.ctx, s.user1Addr)
+	s.acct2 = s.app.AccountKeeper.NewAccountWithAddress(s.ctx, s.user2Addr)
 
 }
 
@@ -58,5 +77,47 @@ func (s *QueryServerTestSuite) TestCalculateTxFees() {
 	}
 	response, err := queryClient.CalculateTxFees(s.ctx.Context(), &simulate1)
 	s.Assert().Error(err)
+	s.Assert().Nil(response)
+	bankSend := banktypes.NewMsgSend(s.user1Addr, s.user1Addr, sdk.NewCoins(sdk.NewCoin("stake", sdk.NewInt(100))))
+	theTx := s.cfg.TxConfig.NewTxBuilder()
+	theTx.SetMsgs(bankSend)
+
+	account1Sig := signing.SignatureV2{
+		PubKey: s.pubkey1,
+		Data: &signing.SingleSignatureData{
+			SignMode: s.cfg.TxConfig.SignModeHandler().DefaultMode(),
+		},
+		Sequence: s.acct1.GetSequence(),
+	}
+	signerData := authsign.SignerData{
+		ChainID:       s.cfg.ChainID,
+		AccountNumber: s.acct1.GetAccountNumber(),
+		Sequence:      s.acct1.GetSequence(),
+	}
+	signBytes, err := s.cfg.TxConfig.SignModeHandler().GetSignBytes(s.cfg.TxConfig.SignModeHandler().DefaultMode(), signerData, theTx.GetTx())
+	if err != nil {
+		panic(err)
+	}
+	sig, err := s.privkey1.Sign(signBytes)
+	if err != nil {
+		panic(err)
+	}
+	account1Sig.Data.(*signing.SingleSignatureData).Signature = sig
+	err = theTx.SetSignatures(account1Sig)
+	if err != nil {
+		panic(err)
+	}
+
+	theTx.SetMemo("memo")
+	theTx.SetFeeAmount(sdk.NewCoins(sdk.NewCoin("stake", sdk.NewInt(1))))
+	theTx.SetGasLimit(uint64(1000000000))
+
+	txBytes, err := s.cfg.TxConfig.TxEncoder()(theTx.(sdk.Tx))
+	s.Require().NoError(err)
+	simulate1 = types.CalculateTxFeesRequest{
+		TxBytes: txBytes,
+	}
+	response, err = queryClient.CalculateTxFees(s.ctx.Context(), &simulate1)
+	s.Assert().NoError(err)
 	s.Assert().Nil(response)
 }
