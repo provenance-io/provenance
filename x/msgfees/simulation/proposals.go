e18614b262bd9bd9c5da0ba104453109ca8c38c5
arnabmitra <arnabmitra.mitra@gmail.com>
2021-12-08 17:51:37 -0700

    adding simultion test for msg fee module.

--- x/msgfees/simulation/proposals.go
+++ x/msgfees/simulation/proposals.go
@@ -1,62 +1,60 @@
 package simulation
 
 import (
+	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+	"github.com/cosmos/cosmos-sdk/x/simulation"
+	simappparams "github.com/provenance-io/provenance/app/params"
+	markertypes "github.com/provenance-io/provenance/x/marker/types"
 	"github.com/provenance-io/provenance/x/msgfees/keeper"
+	"github.com/provenance-io/provenance/x/msgfees/types"
+	"math/rand"
 )
 
-// const (
-// 	// OpWeightAddMsgBasedFeesProposal add msg based fees proposal
-// 	OpWeightAddMsgBasedFeesProposal = "op_weight_add_msg_based_fees_proposal"
-// 	// OpWeightUpdateMsgBasedFeesProposal update msg based fees proposal
-// 	OpWeightUpdateMsgBasedFeesProposal = "op_weight_add_msg_based_fees_proposal"
-// 	// OpWeightRemoveMsgBasedFeesProposal remove msg based fees proposal
-// 	OpWeightRemoveMsgBasedFeesProposal = "op_weight_add_msg_based_fees_proposal"
-// )
+const (
+	// OpWeightAddMsgBasedFeesProposal add msg based fees proposal
+	OpWeightAddMsgBasedFeesProposal = "op_weight_add_msg_based_fees_proposal"
+	// OpWeightUpdateMsgBasedFeesProposal update msg based fees proposal
+	OpWeightUpdateMsgBasedFeesProposal = "op_weight_add_msg_based_fees_proposal"
+	// OpWeightRemoveMsgBasedFeesProposal remove msg based fees proposal
+	OpWeightRemoveMsgBasedFeesProposal = "op_weight_add_msg_based_fees_proposal"
+)
 
-// // ProposalContents defines the module weighted proposals' contents
-// func ProposalContents(k keeper.Keeper) []simtypes.WeightedProposalContent {
-// 	return []simtypes.WeightedProposalContent{
-// 		simulation.NewWeightedProposalContent(
-// 			OpWeightAddMsgBasedFeesProposal,
-// 			simappparams.DefaultWeightAddMarkerProposalContent,
-// 			SimulateCreateAddMsgBasedFeesProposal(k),
-// 		),
-// 		simulation.NewWeightedProposalContent(
-// 			OpWeightUpdateMsgBasedFeesProposal,
-// 			simappparams.DefaultWeightAddMarkerProposalContent,
-// 			SimulateCreateAddMarkerProposalContent(k),
-// 		),
-// 		simulation.NewWeightedProposalContent(
-// 			OpWeightRemoveMsgBasedFeesProposal,
-// 			simappparams.DefaultWeightAddMarkerProposalContent,
-// 			SimulateCreateAddMarkerProposalContent(k),
-// 		),
-// 	}
-// }
-
-// // SimulateCreateSupplyIncreaseProposalContent generates random increase marker supply proposal content
-// func SimulateCreateAddMsgBasedFeesProposal(k keeper.Keeper) simtypes.ContentSimulatorFn {
-// 	return func(r *rand.Rand, ctx sdk.Context, accs []simtypes.Account) simtypes.Content {
-// 		dest := ""
-// 		if r.Intn(100) < 40 {
-// 			acc, _ := simtypes.RandomAcc(r, accs)
-// 			dest = acc.Address.String()
-// 		}
-// 		// m := randomMarker(r, ctx, k)
-// 		// if m == nil || !m.HasGovernanceEnabled() || m.GetStatus() > types.StatusActive {
-// 		// 	return nil
-// 		// }
-// 		return types.NewAddMsgBasedFeesProposal(
-// 			simtypes.RandStringOfLength(r, 10),
-// 			simtypes.RandStringOfLength(r, 100),
-// 			sdk.NewCoin(m.GetDenom(), sdk.NewIntFromUint64(randomUint64(r, k.GetMaxTotalSupply(ctx)-k.CurrentCirculation(ctx, m).Uint64()))),
-// 			dest,
-// 		)
-// 	}
-// }
-
-// ProposalContents defines the module weighted proposals' contents (none for attribute)
+// ProposalContents defines the module weighted proposals' contents
 func ProposalContents(k keeper.Keeper) []simtypes.WeightedProposalContent {
-	return []simtypes.WeightedProposalContent{}
+	return []simtypes.WeightedProposalContent{
+		simulation.NewWeightedProposalContent(
+			OpWeightAddMsgBasedFeesProposal,
+			simappparams.DefaultWeightAddMarkerProposalContent,
+			SimulateCreateAddMsgBasedFeesProposal(k),
+		),
+		//simulation.NewWeightedProposalContent(
+		//	OpWeightUpdateMsgBasedFeesProposal,
+		//	simappparams.DefaultWeightAddMarkerProposalContent,
+		//	SimulateCreateAddMarkerProposalContent(k),
+		//),
+		//simulation.NewWeightedProposalContent(
+		//	OpWeightRemoveMsgBasedFeesProposal,
+		//	simappparams.DefaultWeightAddMarkerProposalContent,
+		//	SimulateCreateAddMarkerProposalContent(k),
+		//),
+	}
+}
+
+// SimulateCreateAddMsgBasedFeesProposal generates random additional fee for AddMsgBasedFeesProposal
+func SimulateCreateAddMsgBasedFeesProposal(k keeper.Keeper) simtypes.ContentSimulatorFn {
+	return func(r *rand.Rand, ctx sdk.Context, accs []simtypes.Account) simtypes.Content {
+
+		addMarkerRequest, err := cdctypes.NewAnyWithValue(&markertypes.MsgAddMarkerRequest{})
+		if err != nil {
+			panic(err)
+		}
+		return types.NewAddMsgBasedFeeProposal(
+			simtypes.RandStringOfLength(r, 10),
+			simtypes.RandStringOfLength(r, 100),
+			addMarkerRequest,
+			sdk.NewCoin("hotdog", sdk.NewInt(r.Int63n(100000000))),
+		)
+	}
 }
