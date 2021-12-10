2efe6fe8ca75e6bf859e0c1c7fb115e8580fe9e5
Ergels Gaxhaj <egaxhaj@figure.com>
2021-12-09 19:06:01 -0600

    disable rust plugin for maven publish

--- protoBindings/bindings/rust/build.gradle.kts
+++ protoBindings/bindings/rust/build.gradle.kts
@@ -2,3 +2,5 @@
 // Register task
 tasks.register<rust.ProtobufRustGrpcTask>("generateRustGrpc")
 
+// Remove Maven publishing
+tasks.withType<PublishToMavenRepository>().configureEach { setProperty("enabled", false) }
