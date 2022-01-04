107c562d5ff79b27bcad94ab16b8f2cd4a4b3a43
Carlton Hanna <channa@figure.com>
2022-01-04 12:50:00 -0700

    Remove unused gov enabled param

--- x/msgfees/keeper/params.go
+++ x/msgfees/keeper/params.go
@@ -9,18 +9,8 @@ import (
 // GetParams returns the total set of distribution parameters.
 func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
 	return types.Params{
-		EnableGovernance: k.GetEnableGovernance(ctx),
-		FloorGasPrice:    k.GetFloorGasPrice(ctx),
+		FloorGasPrice: k.GetFloorGasPrice(ctx),
 	}
-}
-
-// GetEnableGovernance returns the current parameter value for enabling governance control (or default if unset)
-func (k Keeper) GetEnableGovernance(ctx sdk.Context) (enabled bool) {
-	enabled = types.DefaultEnableGovernance
-	if k.paramSpace.Has(ctx, types.ParamStoreKeyEnableGovernance) {
-		k.paramSpace.Get(ctx, types.ParamStoreKeyEnableGovernance, &enabled)
-	}
-	return
 }
 
 // SetParams sets the account parameters to the param space.
