52d0f9c00130201f4dc1ae1724d54ad10dc457f6
arnabmitra <arnabmitra.mitra@gmail.com>
2022-01-11 12:32:19 -0700

    THIS BUILD WILL FAIL because of ibc v2 requied but shows how to integrate with new wasmd changes for ibcv2 and MessageRouter interface.

--- app/app.go
+++ app/app.go
@@ -430,7 +430,6 @@ func New(
 	)
 
 	// Init CosmWasm module
-	var wasmRouter = bApp.Router()
 	wasmDir := filepath.Join(homePath, "data", "wasm")
 
 	wasmWrap := WasmWrapper{Wasm: wasm.DefaultWasmConfig()}
@@ -456,6 +455,10 @@ func New(
 	// Add the staking feature and indicate that provwasm contracts can be run on this chain.
 	supportedFeatures := "staking,provenance,stargate"
 
+	wasmMessageRouter := MessageRouterFunc(func(msg sdk.Msg) baseapp.MsgServiceHandler {
+		return pioMsgFeesRouter.Handler(msg)
+	})
+
 	// The last arguments contain custom message handlers, and custom query handlers,
 	// to allow smart contracts to use provenance modules.
 	app.WasmKeeper = wasm.NewKeeper(
@@ -470,8 +473,7 @@ func New(
 		&app.IBCKeeper.PortKeeper,
 		scopedWasmKeeper,
 		app.TransferKeeper,
-		wasmRouter,
-		app.MsgServiceRouter(),
+		wasmMessageRouter,
 		app.GRPCQueryRouter(),
 		wasmDir,
 		wasmConfig,
@@ -887,3 +889,4 @@ func initParamsKeeper(appCodec codec.BinaryCodec, legacyAmino *codec.LegacyAmino
 
 	return paramsKeeper
 }
+
--- cmd/provenanced/main.go
+++ cmd/provenanced/main.go
@@ -5,10 +5,14 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/server"
 
+	"github.com/pkg/profile"
 	"github.com/provenance-io/provenance/cmd/provenanced/cmd"
 )
 
 func main() {
+
+	//defer profile.Start(profile.ProfilePath(".")).Stop()
+	defer profile.Start(profile.MemProfileHeap,profile.ProfilePath(".")).Stop()
 	rootCmd, _ := cmd.NewRootCmd()
 	if err := cmd.Execute(rootCmd); err != nil {
 		switch e := err.(type) {
