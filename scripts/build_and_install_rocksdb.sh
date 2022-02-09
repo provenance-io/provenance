54e4a5472224f1e5cb81cf56ae4ac4f5b138e736
Daniel Wedul <dwedul@figure.com>
2022-02-08 18:12:33 -0700

    Update the build_and_install_rocksdb script to clean up after itself and also not use sudo on macs.

--- scripts/build_and_install_rocksdb.sh
+++ scripts/build_and_install_rocksdb.sh
@@ -1,34 +1,52 @@
 #!/bin/bash
 
-# This script was written for use within github actions.
-# It downloads and builds a provided version of rocksdb.
-# A tar will be downloaded and unpacked in your current working directory.
+# This script will download, compile, and install rocksdb.
+# It downloads and unpacks a tar in the current working directory, then clean them up when done.
 # Usage: build_rocksdb.sh <version>
 # As of writing this (Feb 7, 2022), the current version is 6.28.2
 
-if [[ -z "$1" || "$1" == '-h' || "$1" == '--help' || "$1" == 'help' ]]; then
+if [[ "$1" == '-h' || "$1" == '--help' || "$1" == 'help' ]]; then
     echo "Usage: $( basename $0 ) <version> [<jobs>]"
     echo 'See https://github.com/facebook/rocksdb/releases for version info.'
-    echo 'Default <jobs> comes from the nproc command.'
+    echo '<jobs> is the number of parallel jobs for make to use. The default comes from the nproc command.'
+    exit 0
+fi
+
+if [[ -n "$1" && "$1" =~ ^v ]]; then
+    echo "Illegal version: [$1]. Must not start with 'v'." >&2
     exit 1
 fi
 
-# The github action runners have sudo, and it's required when installing rocksdb.
-# But when building for docker, there is no sudo command.
-# So, check if sudo is available, and use that for the installation if we can.
-# The build purposefully does NOT use sudo due to security concerns.
-SUDO_MAKE='make'
-if command -v sudo > /dev/null 2>&1; then
-    SUDO_MAKE="sudo ${SUDO_MAKE}"
+if [[ -n "$2" && "$2" =~ [^[:digit:]] ]]; then
+    echo "Illegal jobs count: [$2]. Must only contain digits. Must be at least 1." >&2
+    exit 1
+fi
+
+# In order to install the compiled libraries:
+#  * For linux (at least in the github action runners), sudo is required.
+#  * From inside a docker container, sudo isn't needed (and isn't even available), so we cannot use sudo there.
+#  * On a mac, it ends up using brew, which complains if you use sudo (security concerns).
+# So basically, if sudo is available and brew is not, use sudo for the install.
+SUDO=''
+if command -v sudo > /dev/null 2>&1 && ! command -v brew > /dev/null 2>&1; then
+    SUDO="sudo"
 fi
 
 set -ex
 
-ROCKS_DB_VERSION="$1"
+ROCKS_DB_VERSION="${1:-6.28.2}"
 JOBS="${2:-$( nproc )}"
-wget "https://github.com/facebook/rocksdb/archive/refs/tags/v${ROCKS_DB_VERSION}.tar.gz"
-tar zxf "v${ROCKS_DB_VERSION}.tar.gz"
-cd "rocksdb-${ROCKS_DB_VERSION}"
+TAR_FILE="v${ROCKS_DB_VERSION}.tar.gz"
+
+[[ ! -e "$TAR_FILE" ]] || rm "$TAR_FILE"
+wget "https://github.com/facebook/rocksdb/archive/refs/tags/$TAR_FILE"
+tar zxf "$TAR_FILE"
+ROCKS_DB_DIR="$( tar --exclude='./*/*/*' -tf "$TAR_FILE" | head -n 1 )"
+cd "$ROCKS_DB_DIR"
 export DEBUG_LEVEL=0
 make -j${JOBS} shared_lib
-${SUDO_MAKE} install-shared
+$SUDO make install-shared
+
+cd ..
+rm "$TAR_FILE"
+rm -rf "$ROCKS_DB_DIR"
