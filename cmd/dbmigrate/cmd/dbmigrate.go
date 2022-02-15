7e7f5b00cca1a84cfb3da0ae3de8bd92150a877b
Daniel Wedul <dwedul@figure.com>
2022-02-15 13:28:01 -0700

    Refactor the migrator a bit to work off an object instead of passing all those args around all the time.

--- cmd/dbmigrate/cmd/dbmigrate.go
+++ cmd/dbmigrate/cmd/dbmigrate.go
@@ -3,15 +3,11 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"strings"
-	"time"
-
-	"github.com/otiai10/copy"
+	"github.com/provenance-io/provenance/cmd/dbmigrate/utils"
 	"github.com/rs/zerolog"
 	"github.com/spf13/cobra"
+	"os"
+	"strings"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
@@ -19,21 +15,18 @@ import (
 
 	tmcfg "github.com/tendermint/tendermint/config"
 	tmcli "github.com/tendermint/tendermint/libs/cli"
-	tmlog "github.com/tendermint/tendermint/libs/log"
 	tmdb "github.com/tendermint/tm-db"
 
 	"github.com/provenance-io/provenance/app"
 	"github.com/provenance-io/provenance/cmd/provenanced/config"
 )
 
+const FlagBackupDir = "backup-dir"
+
 var PossibleDBTypes = []string{
 	string(tmdb.RocksDBBackend), string(tmdb.BadgerDBBackend),
 	string(tmdb.GoLevelDBBackend), string(tmdb.CLevelDBBackend),
 }
-
-// TODO: Create a converter params struct for all the args and pass that around instead of all the args.
-// TODO: Add flag for defining a new backup directory.
-// TODO: Add flag for defining the staging directory?
 
 // NewDBMigrateCmd creates a command for migrating the provenanced database from one underlying type to another.
 func NewDBMigrateCmd() *cobra.Command {
@@ -92,7 +85,9 @@ Default Backup directory: {PIO_HOME}/data-{timestamp}-{old type}`, strings.Join(
 				logger.Info(fmt.Sprintf("Database already has type %q. Nothing to do.", targetDBType))
 				return nil
 			}
-			err = convertDB(command, server.GetServerContextFromCmd(command).Logger, tmConfig.RootDir, sourceDBType, targetDBType)
+			// If this is just an empty string, the default ends up being used.
+			backupDir, _ := command.Flags().GetString(FlagBackupDir)
+			err = DoMigrateCmd(command, tmConfig.RootDir, sourceDBType, targetDBType, backupDir)
 			if err != nil {
 				logger.Error(err.Error())
 				// If this returns an error, the help is printed. But that isn't wanted here.
@@ -103,6 +98,7 @@ Default Backup directory: {PIO_HOME}/data-{timestamp}-{old type}`, strings.Join(
 			return nil
 		},
 	}
+	rv.Flags().String(FlagBackupDir, "", "directory to back up the current data directory to (default is {home}/data-{timestamp}-{dbtype})")
 	return rv
 }
 
@@ -119,6 +115,7 @@ func Execute(command *cobra.Command) error {
 	return command.ExecuteContext(ctx)
 }
 
+// IsPossibleDBType checks if the given dbType string is one that this migrator can handle.
 func IsPossibleDBType(dbType string) bool {
 	for _, p := range PossibleDBTypes {
 		if dbType == p {
@@ -128,166 +125,23 @@ func IsPossibleDBType(dbType string) bool {
 	return false
 }
 
-// convertDB converts all database dirs in the given homePath from the source underlying type to the target type.
-func convertDB(command *cobra.Command, logger tmlog.Logger, homePath, sourceDBType, targetDBType string) error {
-	logger.Info("Setting up database migration.", "home", homePath, "source type", sourceDBType, "target type", targetDBType)
-
-	sourceDataDir := filepath.Join(homePath, "data")
-	dbDirs, nonDBEntries, err := getDataDirContents(sourceDataDir)
+// DoMigrateCmd does all the work associated with the dbmigrate command (assuming that inputs have been validated).
+func DoMigrateCmd(command *cobra.Command, homePath, sourceDBType, targetDBType, backupDir string) error {
+	logger := server.GetServerContextFromCmd(command).Logger
+	logger.Info("Setting up database migrations.", "home", homePath, "source type", sourceDBType, "target type", targetDBType)
+	migrator, err := utils.SetUpMigrator(homePath, sourceDBType, targetDBType, backupDir)
 	if err != nil {
-		return fmt.Errorf("error reading database at %q: %w", sourceDataDir, err)
+		return err
 	}
-	if len(dbDirs) == 0 {
-		return fmt.Errorf("no database directories found in %q", sourceDataDir)
-	}
-	targetDataDir, err := ioutil.TempDir(homePath, "data-dbmigrate-tmp-*")
+	logger.Info("Starting migrations.")
+	err = migrator.Migrate(logger)
 	if err != nil {
-		return fmt.Errorf("error creating temporariy target data directory: %w", err)
+		return err
 	}
-
-	logger.Info(fmt.Sprintf("Converting %d database directories from %q %s to %q %s",
-		len(dbDirs), sourceDBType, sourceDataDir, targetDBType, targetDataDir))
-	for i, dbDir := range dbDirs {
-		err = convertDBDir(logger.With("db dir", fmt.Sprintf("%d/%d: %s", i+1, len(dbDirs), dbDir)),
-			sourceDataDir, targetDataDir, dbDir, sourceDBType, targetDBType)
-		if err != nil {
-			return fmt.Errorf("could not convert %q from %q to %q: %w", dbDir, sourceDBType, targetDBType, err)
-		}
-	}
-
-	logger.Info(fmt.Sprintf("Copying %d items from %s to %s", len(nonDBEntries), sourceDataDir, targetDataDir))
-	for i, entry := range nonDBEntries {
-		logger.Info(fmt.Sprintf("%d/%d: Copying %s", i+1, len(nonDBEntries), entry))
-		if err = copy.Copy(filepath.Join(sourceDataDir, entry), filepath.Join(targetDataDir, entry)); err != nil {
-			return fmt.Errorf("could not copy %s: %w", entry, err)
-		}
-	}
-
-	backupDataDir := filepath.Join(homePath, fmt.Sprintf("data-%s-%s", time.Now().Format("2006-01-02-15-04"), sourceDBType))
-	logger.Info("Moving existing data directory to backup location.", "from", sourceDataDir, "to", backupDataDir)
-	if err = os.Rename(sourceDataDir, backupDataDir); err != nil {
-		return fmt.Errorf("could not back up existing db: %w", err)
-	}
-
-	logger.Info("Moving new data directory into place.", "from", targetDataDir, "to", sourceDataDir)
-	if err = os.Rename(targetDataDir, sourceDataDir); err != nil {
-		return fmt.Errorf("could not move new db into place: %w", err)
-	}
-
-	// TODO: Do superficial check of permissions/ownership and issue warning if different.
-
-	logger.Info("Updating config.", "key", "db_backend", "from", sourceDBType, "to", targetDBType)
-	tmConfig, err := config.ExtractTmConfig(command)
+	err = migrator.UpdateConfig(logger, command)
 	if err != nil {
-		return fmt.Errorf("could not extract Tendermint config: %w", err)
+		return err
 	}
-	tmConfig.DBBackend = targetDBType
-	config.SaveConfigs(command, nil, tmConfig, nil, false)
-	logger.Info(fmt.Sprintf("%s Was: %s, Is Now: %s", "db_backend", sourceDBType, targetDBType))
-
-	logger.Info("Done migrating database.")
+	logger.Info("Done migrating databases.")
 	return nil
-}
-
-// getDataDirContents gets the contents of a directory split into database directories and non-database entries.
-// The first return value will contain an entry for each database directory (including if they in sub-directories).
-// The second return value will contain all entries (files or directories) under dataDirPath that are not part of a database directory.
-// That is, it's all entries that will not be migrated, and should copied.
-func getDataDirContents(dataDirPath string) ([]string, []string, error) {
-	contents, err := ioutil.ReadDir(dataDirPath)
-	if err != nil {
-		return nil, nil, err
-	}
-	dbs := make([]string, 0)
-	nonDBs := make([]string, 0)
-	for _, entry := range contents {
-		if entry.IsDir() {
-			if filepath.Ext(entry.Name()) == ".db" {
-				dbs = append(dbs, entry.Name())
-			} else {
-				subDBs, subNonDBs, err := getDataDirContents(filepath.Join(dataDirPath, entry.Name()))
-				if err != nil {
-					return nil, nil, err
-				}
-				for _, dbDir := range subDBs {
-					dbs = append(dbs, filepath.Join(entry.Name(), dbDir))
-				}
-				if len(subDBs) > 0 {
-					for _, nonDBDir := range subNonDBs {
-						nonDBs = append(nonDBs, filepath.Join(entry.Name(), nonDBDir))
-					}
-				} else {
-					nonDBs = append(nonDBs, entry.Name())
-				}
-			}
-		} else {
-			nonDBs = append(nonDBs, entry.Name())
-		}
-		if filepath.Ext(entry.Name()) == ".db" && entry.IsDir() {
-			dbs = append(dbs, entry.Name())
-		}
-	}
-	return dbs, nonDBs, nil
-}
-
-// convertDBDir converts a single db directory from one underlying type to another.
-func convertDBDir(logger tmlog.Logger, sourceDataDir, targetDataDir, dbDir, sourceDBType, targetDBType string) error {
-	sourceDir, dbName := splitDBPath(filepath.Join(sourceDataDir, dbDir))
-	targetDir, _ := splitDBPath(filepath.Join(targetDataDir, dbDir))
-	targetDataDirInfo, err := os.Stat(targetDataDir)
-	if err != nil {
-		return fmt.Errorf("could not stat target data dir: %w", err)
-	}
-	targetDataDirMode := targetDataDirInfo.Mode()
-
-	logger.Info("Setting up migration of directory.", "from", sourceDir, "to", targetDir)
-
-	sourceDB, err := tmdb.NewDB(dbName, tmdb.BackendType(sourceDBType), sourceDir)
-	if err != nil {
-		return fmt.Errorf("could not open %q source db: %w", dbName, err)
-	}
-	defer sourceDB.Close()
-
-	if targetDir != targetDataDir {
-		err = os.MkdirAll(targetDir, targetDataDirMode)
-		if err != nil {
-			return fmt.Errorf("could not create target sub-directory: %w", err)
-		}
-	}
-	targetDB, err := tmdb.NewDB(dbName, tmdb.BackendType(targetDBType), targetDir)
-	if err != nil {
-		return fmt.Errorf("could not open %q target db: %w", dbName, err)
-	}
-	defer targetDB.Close()
-
-	iter, err := sourceDB.Iterator(nil, nil)
-	if err != nil {
-		return fmt.Errorf("could not create %q source iterator: %w", dbName, err)
-	}
-	defer iter.Close()
-
-	batch := targetDB.NewBatch()
-	defer batch.Close()
-
-	logger.Info("Starting migration")
-	for ; iter.Valid(); iter.Next() {
-		if err = batch.Set(iter.Key(), iter.Value()); err != nil {
-			return fmt.Errorf("could not set %q key/value: %w", dbName, err)
-		}
-	}
-
-	logger.Info("Writing batch")
-	if err = batch.Write(); err != nil {
-		return fmt.Errorf("could not write %q batch: %w", dbName, err)
-	}
-
-	logger.Info("Done")
-	return nil
-}
-
-// splitDBPath breaks down a path to a db directory into the path to the directory containing that and the db name.
-// For example: "/foo/bar/baz.db" will return "/foo/bar" and "baz".
-func splitDBPath(path string) (string, string) {
-	base, name := filepath.Split(path)
-	return filepath.Clean(base), strings.TrimSuffix(name, ".db")
 }
---
+++ cmd/dbmigrate/utils/migrator.go
@@ -0,0 +1,207 @@
+package utils
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	copier "github.com/otiai10/copy"
+	"github.com/spf13/cobra"
+
+	tmlog "github.com/tendermint/tendermint/libs/log"
+	tmdb "github.com/tendermint/tm-db"
+
+	"github.com/provenance-io/provenance/cmd/provenanced/config"
+)
+
+// Migrator is an object to help guide a migration.
+type Migrator struct {
+	HomePath      string
+	SourceDBType  string
+	TargetDBType  string
+	SourceDataDir string
+	TargetDataDir string
+	BackupDataDir string
+
+	ToConvert []string
+	ToCopy    []string
+}
+
+// SetUpMigrator creates a new Migrator with the given info and sets it up so that it's ready for a migration.
+func SetUpMigrator(homePath, sourceDBType, targetDBType, backupDir string) (*Migrator, error) {
+	var err error
+	m := Migrator{
+		HomePath:      homePath,
+		SourceDBType:  sourceDBType,
+		TargetDBType:  targetDBType,
+		SourceDataDir: filepath.Join(homePath, "data"),
+		BackupDataDir: backupDir,
+	}
+	if len(m.BackupDataDir) == 0 {
+		m.BackupDataDir = filepath.Join(homePath, fmt.Sprintf("data-%s-%s", time.Now().Format("2006-01-02-15-04"), sourceDBType))
+	}
+	m.ToConvert, m.ToCopy, err = GetDataDirContents(m.SourceDataDir)
+	if err != nil {
+		return nil, fmt.Errorf("error reading %q: %w", m.SourceDataDir, err)
+	}
+	if len(m.ToConvert) == 0 {
+		return nil, fmt.Errorf("no database directories found in %q", m.SourceDataDir)
+	}
+	m.TargetDataDir, err = os.MkdirTemp(homePath, "data-dbmigrate-tmp-*")
+	if err != nil {
+		return nil, fmt.Errorf("error creating temporariy target data directory: %w", err)
+	}
+	return &m, nil
+}
+
+// Migrate converts all database dirs in the given homePath from the source underlying type to the target type.
+// It then copies everything else in the data dir and swaps out the existing data dir for the newly created one.
+func (m Migrator) Migrate(logger tmlog.Logger) error {
+	logger.Info(fmt.Sprintf("Converting %d database directories from %q %s to %q %s",
+		len(m.ToConvert), m.SourceDBType, m.SourceDataDir, m.TargetDBType, m.TargetDataDir))
+	for i, dbDir := range m.ToConvert {
+		err := m.MigrateDBDir(logger.With("db dir", fmt.Sprintf("%d/%d: %s", i+1, len(m.ToConvert), dbDir)), dbDir)
+		if err != nil {
+			return fmt.Errorf("could not convert %q from %q to %q: %w", dbDir, m.SourceDBType, m.TargetDBType, err)
+		}
+	}
+
+	logger.Info(fmt.Sprintf("Copying %d items from %s to %s", len(m.ToCopy), m.SourceDataDir, m.TargetDataDir))
+	for i, entry := range m.ToCopy {
+		logger.Info(fmt.Sprintf("%d/%d: Copying %s", i+1, len(m.ToCopy), entry))
+		if err := copier.Copy(filepath.Join(m.SourceDataDir, entry), filepath.Join(m.TargetDataDir, entry)); err != nil {
+			return fmt.Errorf("could not copy %s: %w", entry, err)
+		}
+	}
+
+	logger.Info("Moving existing data directory to backup location.", "from", m.SourceDataDir, "to", m.BackupDataDir)
+	if err := os.Rename(m.SourceDataDir, m.BackupDataDir); err != nil {
+		return fmt.Errorf("could not back up existing data directory: %w", err)
+	}
+
+	logger.Info("Moving new data directory into place.", "from", m.TargetDataDir, "to", m.SourceDataDir)
+	if err := os.Rename(m.TargetDataDir, m.SourceDataDir); err != nil {
+		return fmt.Errorf("could not move new data directory into place: %w", err)
+	}
+
+	return nil
+}
+
+// MigrateDBDir creates a copy of the given db directory, converting it from one underlying type to another.
+func (m Migrator) MigrateDBDir(logger tmlog.Logger, dbDir string) error {
+	sourceDir, dbName := splitDBPath(filepath.Join(m.SourceDataDir, dbDir))
+	targetDir, _ := splitDBPath(filepath.Join(m.TargetDataDir, dbDir))
+	targetDataDirInfo, err := os.Stat(m.TargetDataDir)
+	if err != nil {
+		return fmt.Errorf("could not stat target data dir: %w", err)
+	}
+	targetDataDirMode := targetDataDirInfo.Mode()
+
+	logger.Info("Setting up migration of directory.", "from", sourceDir, "to", targetDir)
+
+	sourceDB, err := tmdb.NewDB(dbName, tmdb.BackendType(m.SourceDBType), sourceDir)
+	if err != nil {
+		return fmt.Errorf("could not open %q source db: %w", dbName, err)
+	}
+	defer sourceDB.Close()
+
+	if targetDir != m.TargetDataDir {
+		err = os.MkdirAll(targetDir, targetDataDirMode)
+		if err != nil {
+			return fmt.Errorf("could not create target sub-directory: %w", err)
+		}
+	}
+	targetDB, err := tmdb.NewDB(dbName, tmdb.BackendType(m.TargetDBType), targetDir)
+	if err != nil {
+		return fmt.Errorf("could not open %q target db: %w", dbName, err)
+	}
+	defer targetDB.Close()
+
+	iter, err := sourceDB.Iterator(nil, nil)
+	if err != nil {
+		return fmt.Errorf("could not create %q source iterator: %w", dbName, err)
+	}
+	defer iter.Close()
+
+	batch := targetDB.NewBatch()
+	defer batch.Close()
+
+	logger.Info("Starting migration")
+	for ; iter.Valid(); iter.Next() {
+		if err = batch.Set(iter.Key(), iter.Value()); err != nil {
+			return fmt.Errorf("could not set %q key/value: %w", dbName, err)
+		}
+	}
+
+	logger.Info("Writing batch")
+	if err = batch.Write(); err != nil {
+		return fmt.Errorf("could not write %q batch: %w", dbName, err)
+	}
+
+	logger.Info("Done")
+	return nil
+}
+
+// UpdateConfig updates the config file to reflect the new database type.
+func (m Migrator) UpdateConfig(logger tmlog.Logger, command *cobra.Command) error {
+	logger.Info("Updating config.", "key", "db_backend", "from", m.SourceDBType, "to", m.TargetDBType)
+	tmConfig, err := config.ExtractTmConfig(command)
+	if err != nil {
+		return fmt.Errorf("could not extract Tendermint config: %w", err)
+	}
+	tmConfig.DBBackend = m.TargetDBType
+	config.SaveConfigs(command, nil, tmConfig, nil, false)
+	logger.Info(fmt.Sprintf("%s Was: %s, Is Now: %s", "db_backend", m.SourceDBType, m.TargetDBType))
+	return nil
+}
+
+// splitDBPath breaks down a path to a db directory into the path to the directory containing that and the db name.
+// For example: "/foo/bar/baz.db" will return "/foo/bar" and "baz".
+func splitDBPath(path string) (string, string) {
+	base, name := filepath.Split(path)
+	return filepath.Clean(base), strings.TrimSuffix(name, ".db")
+}
+
+// GetDataDirContents gets the contents of a directory separated into database directories and non-database entries.
+// The first return value will contain an entry for each database directory (including if they are in sub-directories).
+// The second return value will contain all entries (files or directories) under dataDirPath that are not part of a database directory.
+// That is, it's all entries that will not be migrated, and should copied.
+func GetDataDirContents(dataDirPath string) ([]string, []string, error) {
+	contents, err := ioutil.ReadDir(dataDirPath)
+	if err != nil {
+		return nil, nil, err
+	}
+	dbs := make([]string, 0)
+	nonDBs := make([]string, 0)
+	for _, entry := range contents {
+		if entry.IsDir() {
+			if filepath.Ext(entry.Name()) == ".db" {
+				dbs = append(dbs, entry.Name())
+			} else {
+				subDBs, subNonDBs, err := GetDataDirContents(filepath.Join(dataDirPath, entry.Name()))
+				if err != nil {
+					return nil, nil, err
+				}
+				for _, dbDir := range subDBs {
+					dbs = append(dbs, filepath.Join(entry.Name(), dbDir))
+				}
+				if len(subDBs) > 0 {
+					for _, nonDBDir := range subNonDBs {
+						nonDBs = append(nonDBs, filepath.Join(entry.Name(), nonDBDir))
+					}
+				} else {
+					nonDBs = append(nonDBs, entry.Name())
+				}
+			}
+		} else {
+			nonDBs = append(nonDBs, entry.Name())
+		}
+		if filepath.Ext(entry.Name()) == ".db" && entry.IsDir() {
+			dbs = append(dbs, entry.Name())
+		}
+	}
+	return dbs, nonDBs, nil
+}
