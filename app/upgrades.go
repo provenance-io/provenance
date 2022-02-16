913d6f350fd42dfea8ffe101339b0dc67a0fecfb
Arnab Mitra <arnabmitra.mitra@gmail.com>
2022-02-16 10:00:02 -0700

    adding upgrade handler indigo. (#665)
    * adding upgrade handler indigo.

--- app/upgrades.go
+++ app/upgrades.go
@@ -77,7 +77,8 @@ var handlers = map[string]appUpgrade{
 		},
 		Added: []string{msgfeestypes.ModuleName},
 	},
-	"hazel": {},
+	"hazel":  {},
+	"indigo": {}, // upgrade for pio-testnet-1 from v1.8.0-rc7 to v1.8.0-rc8
 	// TODO - Add new upgrade definitions here.
 }
 
