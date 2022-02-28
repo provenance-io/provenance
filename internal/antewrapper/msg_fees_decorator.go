ee43954a16b60be81283624b141c9465a22722e0
arnabmitra <arnabmitra.mitra@gmail.com>
2022-02-28 10:42:52 -0700

    fixing comment.

--- internal/antewrapper/msg_fees_decorator.go
+++ internal/antewrapper/msg_fees_decorator.go
@@ -119,7 +119,7 @@ func (afd MsgFeesDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool
 
 //   The chain-id check is exclusively for not breaking all existing sim tests which freak out when denom is anything other than stake.
 //   and some network tests won't work without a chain id being set(but they also setup everything with stake denom) so `simapp-unit-testing` chain id is skipped also.
-//	 This only needs to work to pio-testnet and pio-mainnet, so this is safe.
+//   This only needs to work to pio-testnet and pio-mainnet, so this is safe.
 func chainIdsToIgnoreForFloorGasPriceCheck(ctx sdk.Context) bool {
 	return len(ctx.ChainID()) != 0 && ctx.ChainID() != SimAppChainID
 }
