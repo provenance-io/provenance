5c742caf80b1b110b010d58d6238153fbf1892e9
Carlton Hanna <channa@figure.com>
2022-03-07 14:47:29 -0700

    Add interface methods to query server

--- x/epoch/keeper/query_server.go
+++ x/epoch/keeper/query_server.go
@@ -1,7 +1,17 @@
 package keeper
 
 import (
+	"context"
+
 	"github.com/provenance-io/provenance/x/epoch/types"
 )
 
 var _ types.QueryServer = Keeper{}
+
+func (k Keeper) CurrentEpoch(goCtx context.Context, request *types.QueryCurrentEpochRequest) (*types.QueryCurrentEpochResponse, error) {
+	return nil, nil
+}
+
+func (k Keeper) EpochInfos(goCtx context.Context, request *types.QueryEpochsInfoRequest) (*types.QueryEpochsInfoResponse, error) {
+	return nil, nil
+}
