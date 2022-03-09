299b7abd91fd0376d78a53f78ff199ce33be6da3
Ergels Gaxhaj <egaxhaj@figure.com>
2022-03-09 16:56:26 -0600

    continue with testing when docker-compose returns error

--- sims-state-listening.mk
+++ sims-state-listening.mk
@@ -21,14 +21,14 @@ test-sim-nondeterminism-state-listening-trace:
 test-sim-nondeterminism-state-listening-kafka:
 	@echo "Running non-determinism-state-listening-kafka test..."
 	@echo "Starting Kafka..."
-	docker-compose -f networks/local/kafka/docker-compose.yml up -d
+	@-docker-compose -f networks/local/kafka/docker-compose.yml up -d
 
 	@-go test -mod=readonly $(SIMAPP) -run TestAppStateDeterminismWithStateListening -Enabled=true \
 		-NumBlocks=50 -BlockSize=100 -Commit=true -Period=0 -v -timeout 24h \
 		-StateListeningPlugin=kafka -HaltAppOnDeliveryError=true
 
 	@echo "Stopping Kafka..."
-	docker-compose -f networks/local/kafka/docker-compose.yml down
+	@-docker-compose -f networks/local/kafka/docker-compose.yml down
 
 test-sim-nondeterminism-state-listening-all: \
 	test-sim-nondeterminism-state-listening-file \
