3c30fa33ae53929ac2855dd0a097b66bcdbf09cf
Carlton Hanna <channa@figure.com>
2022-03-14 17:26:09 -0600

    Added more reward proto defs

--- x/reward/types/tx.pb.go
+++ x/reward/types/tx.pb.go
@@ -104,33 +104,109 @@ func (m *MsgWriteCriteraResponse) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_MsgWriteCriteraResponse proto.InternalMessageInfo
 
+type MsgAddToCriteraRequest struct {
+}
+
+func (m *MsgAddToCriteraRequest) Reset()         { *m = MsgAddToCriteraRequest{} }
+func (m *MsgAddToCriteraRequest) String() string { return proto.CompactTextString(m) }
+func (*MsgAddToCriteraRequest) ProtoMessage()    {}
+func (*MsgAddToCriteraRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_6a1c90eb8246d229, []int{2}
+}
+func (m *MsgAddToCriteraRequest) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *MsgAddToCriteraRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_MsgAddToCriteraRequest.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *MsgAddToCriteraRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_MsgAddToCriteraRequest.Merge(m, src)
+}
+func (m *MsgAddToCriteraRequest) XXX_Size() int {
+	return m.Size()
+}
+func (m *MsgAddToCriteraRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_MsgAddToCriteraRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_MsgAddToCriteraRequest proto.InternalMessageInfo
+
+type MsgAddToCriteraResponse struct {
+}
+
+func (m *MsgAddToCriteraResponse) Reset()         { *m = MsgAddToCriteraResponse{} }
+func (m *MsgAddToCriteraResponse) String() string { return proto.CompactTextString(m) }
+func (*MsgAddToCriteraResponse) ProtoMessage()    {}
+func (*MsgAddToCriteraResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_6a1c90eb8246d229, []int{3}
+}
+func (m *MsgAddToCriteraResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *MsgAddToCriteraResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_MsgAddToCriteraResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *MsgAddToCriteraResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_MsgAddToCriteraResponse.Merge(m, src)
+}
+func (m *MsgAddToCriteraResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *MsgAddToCriteraResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_MsgAddToCriteraResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_MsgAddToCriteraResponse proto.InternalMessageInfo
+
 func init() {
 	proto.RegisterType((*MsgWriteCriteraRequest)(nil), "provenance.reward.v1.MsgWriteCriteraRequest")
 	proto.RegisterType((*MsgWriteCriteraResponse)(nil), "provenance.reward.v1.MsgWriteCriteraResponse")
+	proto.RegisterType((*MsgAddToCriteraRequest)(nil), "provenance.reward.v1.MsgAddToCriteraRequest")
+	proto.RegisterType((*MsgAddToCriteraResponse)(nil), "provenance.reward.v1.MsgAddToCriteraResponse")
 }
 
 func init() { proto.RegisterFile("provenance/reward/v1/tx.proto", fileDescriptor_6a1c90eb8246d229) }
 
 var fileDescriptor_6a1c90eb8246d229 = []byte{
-	// 282 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x91, 0xc1, 0x4a, 0xc3, 0x30,
-	0x18, 0xc7, 0x57, 0x04, 0x0f, 0xc1, 0x53, 0x19, 0x4e, 0x0b, 0x46, 0xdc, 0xd9, 0x25, 0x74, 0xbe,
-	0x81, 0x9e, 0x07, 0xe2, 0x45, 0xf0, 0x96, 0x76, 0x9f, 0x59, 0xd0, 0xf6, 0x8b, 0xf9, 0xd2, 0x3a,
-	0xdf, 0xc2, 0xc7, 0xf2, 0xb8, 0xa3, 0x47, 0x69, 0x5f, 0x44, 0xda, 0x5a, 0x56, 0xb4, 0x07, 0x2f,
-	0x21, 0x1f, 0xbf, 0x1f, 0xff, 0xfc, 0x93, 0xb0, 0x33, 0xeb, 0xb0, 0x84, 0x5c, 0xe5, 0x29, 0x48,
-	0x07, 0xaf, 0xca, 0xad, 0x65, 0x19, 0x4b, 0xbf, 0x15, 0xd6, 0xa1, 0xc7, 0x70, 0xba, 0xc7, 0xa2,
-	0xc3, 0xa2, 0x8c, 0xa3, 0xa9, 0x46, 0x8d, 0xad, 0x20, 0x9b, 0x5d, 0xe7, 0x46, 0x5c, 0x23, 0xea,
-	0x67, 0x90, 0xed, 0x94, 0x14, 0x8f, 0x72, 0x5d, 0x38, 0xe5, 0x0d, 0xe6, 0x3f, 0xfc, 0xfc, 0x37,
-	0xf7, 0x26, 0x03, 0xf2, 0x2a, 0xb3, 0x7d, 0x40, 0x8a, 0x94, 0x21, 0xc9, 0x44, 0x11, 0xc8, 0x32,
-	0x4e, 0xc0, 0xab, 0x58, 0xa6, 0x68, 0xfa, 0x80, 0x8b, 0x41, 0x57, 0xb0, 0x98, 0x6e, 0x9a, 0xaa,
-	0x1a, 0x72, 0x20, 0x43, 0x9d, 0x32, 0x3f, 0x61, 0xc7, 0x2b, 0xd2, 0xf7, 0xce, 0x78, 0xb8, 0x69,
-	0x16, 0xa7, 0xee, 0xe0, 0xa5, 0x00, 0xf2, 0xf3, 0x53, 0x36, 0xfb, 0x43, 0xc8, 0x62, 0x4e, 0xb0,
-	0x74, 0xec, 0x60, 0x45, 0x3a, 0x7c, 0x62, 0x47, 0x43, 0x1c, 0x5e, 0x8a, 0xb1, 0xcb, 0x8b, 0xf1,
-	0xfc, 0x68, 0xf1, 0x4f, 0xbb, 0x3b, 0xf3, 0x5a, 0x7f, 0x54, 0x3c, 0xd8, 0x55, 0x3c, 0xf8, 0xaa,
-	0x78, 0xf0, 0x5e, 0xf3, 0xc9, 0xae, 0xe6, 0x93, 0xcf, 0x9a, 0x4f, 0xd8, 0xcc, 0xe0, 0x68, 0xd4,
-	0x6d, 0xf0, 0xb0, 0xd4, 0xc6, 0x6f, 0x8a, 0x44, 0xa4, 0x98, 0xc9, 0xbd, 0xb2, 0x30, 0x38, 0x98,
-	0xe4, 0xb6, 0xff, 0x47, 0xff, 0x66, 0x81, 0x92, 0xc3, 0xf6, 0x61, 0xae, 0xbe, 0x03, 0x00, 0x00,
-	0xff, 0xff, 0x7d, 0xb6, 0xb6, 0x14, 0xe9, 0x01, 0x00, 0x00,
+	// 310 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x52, 0x4f, 0x4b, 0xf3, 0x30,
+	0x18, 0x5f, 0x79, 0xe1, 0x3d, 0x04, 0x4f, 0x63, 0x38, 0x1d, 0x18, 0x71, 0x67, 0x97, 0xd0, 0xf9,
+	0x09, 0xd4, 0xf3, 0x40, 0x44, 0x10, 0xbc, 0xa5, 0xed, 0x63, 0x16, 0xb4, 0x7d, 0x62, 0x9e, 0xb4,
+	0xce, 0x6f, 0xe1, 0xc7, 0xf2, 0xb8, 0xa3, 0x37, 0x65, 0xfb, 0x22, 0xd2, 0x75, 0x63, 0x55, 0xc3,
+	0xf0, 0x52, 0xfa, 0xf0, 0xfb, 0xfb, 0x24, 0x61, 0x47, 0xd6, 0x61, 0x05, 0x85, 0x2a, 0x52, 0x90,
+	0x0e, 0x9e, 0x95, 0xcb, 0x64, 0x15, 0x4b, 0x3f, 0x13, 0xd6, 0xa1, 0xc7, 0x6e, 0x6f, 0x0b, 0x8b,
+	0x06, 0x16, 0x55, 0x3c, 0xe8, 0x69, 0xd4, 0xb8, 0x22, 0xc8, 0xfa, 0xaf, 0xe1, 0x0e, 0xb8, 0x46,
+	0xd4, 0x8f, 0x20, 0x57, 0x53, 0x52, 0xde, 0xcb, 0xac, 0x74, 0xca, 0x1b, 0x2c, 0xd6, 0xf8, 0xf1,
+	0x4f, 0xdc, 0x9b, 0x1c, 0xc8, 0xab, 0xdc, 0x6e, 0x0c, 0x52, 0xa4, 0x1c, 0x49, 0x26, 0x8a, 0x40,
+	0x56, 0x71, 0x02, 0x5e, 0xc5, 0x32, 0x45, 0xb3, 0x31, 0x38, 0x69, 0x75, 0x05, 0x8b, 0xe9, 0xb4,
+	0xae, 0xaa, 0xa1, 0x00, 0x32, 0xd4, 0x50, 0x86, 0x07, 0x6c, 0x7f, 0x42, 0xfa, 0xd6, 0x19, 0x0f,
+	0x97, 0xf5, 0xc7, 0xa9, 0x6b, 0x78, 0x2a, 0x81, 0xfc, 0xf0, 0x90, 0xf5, 0x7f, 0x21, 0x64, 0xb1,
+	0x20, 0x58, 0x8b, 0xce, 0xb3, 0xec, 0x06, 0x83, 0xa2, 0xef, 0x48, 0x23, 0x1a, 0x7f, 0x44, 0xec,
+	0xdf, 0x84, 0x74, 0xf7, 0x81, 0xed, 0xb5, 0x4d, 0xbb, 0xa7, 0x22, 0x74, 0x64, 0x22, 0xdc, 0x6a,
+	0x30, 0xfa, 0x23, 0xbb, 0x09, 0xad, 0xc3, 0xda, 0x65, 0x76, 0x84, 0x05, 0xb6, 0xd9, 0x11, 0x16,
+	0xda, 0xf0, 0x42, 0xbf, 0x2d, 0x78, 0x34, 0x5f, 0xf0, 0xe8, 0x73, 0xc1, 0xa3, 0xd7, 0x25, 0xef,
+	0xcc, 0x97, 0xbc, 0xf3, 0xbe, 0xe4, 0x1d, 0xd6, 0x37, 0x18, 0xb4, 0xba, 0x8a, 0xee, 0xc6, 0xda,
+	0xf8, 0x69, 0x99, 0x88, 0x14, 0x73, 0xb9, 0xa5, 0x8c, 0x0c, 0xb6, 0x26, 0x39, 0xdb, 0x3c, 0x35,
+	0xff, 0x62, 0x81, 0x92, 0xff, 0xab, 0xbb, 0x3b, 0xfb, 0x0a, 0x00, 0x00, 0xff, 0xff, 0x18, 0x02,
+	0x1f, 0x26, 0x8c, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -146,6 +222,7 @@ const _ = grpc.SupportPackageIsVersion4
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type MsgClient interface {
 	WriteCritera(ctx context.Context, in *MsgWriteCriteraRequest, opts ...grpc.CallOption) (*MsgWriteCriteraResponse, error)
+	AddToCritera(ctx context.Context, in *MsgAddToCriteraRequest, opts ...grpc.CallOption) (*MsgAddToCriteraResponse, error)
 }
 
 type msgClient struct {
@@ -165,9 +242,19 @@ func (c *msgClient) WriteCritera(ctx context.Context, in *MsgWriteCriteraRequest
 	return out, nil
 }
 
+func (c *msgClient) AddToCritera(ctx context.Context, in *MsgAddToCriteraRequest, opts ...grpc.CallOption) (*MsgAddToCriteraResponse, error) {
+	out := new(MsgAddToCriteraResponse)
+	err := c.cc.Invoke(ctx, "/provenance.reward.v1.Msg/AddToCritera", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // MsgServer is the server API for Msg service.
 type MsgServer interface {
 	WriteCritera(context.Context, *MsgWriteCriteraRequest) (*MsgWriteCriteraResponse, error)
+	AddToCritera(context.Context, *MsgAddToCriteraRequest) (*MsgAddToCriteraResponse, error)
 }
 
 // UnimplementedMsgServer can be embedded to have forward compatible implementations.
@@ -176,6 +263,9 @@ type UnimplementedMsgServer struct {
 
 func (*UnimplementedMsgServer) WriteCritera(ctx context.Context, req *MsgWriteCriteraRequest) (*MsgWriteCriteraResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method WriteCritera not implemented")
+}
+func (*UnimplementedMsgServer) AddToCritera(ctx context.Context, req *MsgAddToCriteraRequest) (*MsgAddToCriteraResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method AddToCritera not implemented")
 }
 
 func RegisterMsgServer(s grpc1.Server, srv MsgServer) {
@@ -200,6 +290,24 @@ func _Msg_WriteCritera_Handler(srv interface{}, ctx context.Context, dec func(in
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Msg_AddToCritera_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(MsgAddToCriteraRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MsgServer).AddToCritera(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/provenance.reward.v1.Msg/AddToCritera",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MsgServer).AddToCritera(ctx, req.(*MsgAddToCriteraRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Msg_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "provenance.reward.v1.Msg",
 	HandlerType: (*MsgServer)(nil),
@@ -207,6 +315,10 @@ var _Msg_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "WriteCritera",
 			Handler:    _Msg_WriteCritera_Handler,
+		},
+		{
+			MethodName: "AddToCritera",
+			Handler:    _Msg_AddToCritera_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -259,6 +371,52 @@ func (m *MsgWriteCriteraResponse) MarshalToSizedBuffer(dAtA []byte) (int, error)
 	return len(dAtA) - i, nil
 }
 
+func (m *MsgAddToCriteraRequest) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *MsgAddToCriteraRequest) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *MsgAddToCriteraRequest) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	return len(dAtA) - i, nil
+}
+
+func (m *MsgAddToCriteraResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *MsgAddToCriteraResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *MsgAddToCriteraResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	return len(dAtA) - i, nil
+}
+
 func encodeVarintTx(dAtA []byte, offset int, v uint64) int {
 	offset -= sovTx(v)
 	base := offset
@@ -280,6 +438,24 @@ func (m *MsgWriteCriteraRequest) Size() (n int) {
 }
 
 func (m *MsgWriteCriteraResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	return n
+}
+
+func (m *MsgAddToCriteraRequest) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	return n
+}
+
+func (m *MsgAddToCriteraResponse) Size() (n int) {
 	if m == nil {
 		return 0
 	}
@@ -371,6 +547,106 @@ func (m *MsgWriteCriteraResponse) Unmarshal(dAtA []byte) error {
 		}
 		if fieldNum <= 0 {
 			return fmt.Errorf("proto: MsgWriteCriteraResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipTx(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *MsgAddToCriteraRequest) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowTx
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: MsgAddToCriteraRequest: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: MsgAddToCriteraRequest: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipTx(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *MsgAddToCriteraResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowTx
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: MsgAddToCriteraResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: MsgAddToCriteraResponse: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		default:
