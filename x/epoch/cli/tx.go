d771395cd9bfef8ce9766b67cd0888df1670242f
arnabmitra <arnabmitra.mitra@gmail.com>
2022-03-17 12:47:59 -0600

    cli methods and more tests.

---
+++ x/epoch/cli/tx.go
@@ -0,0 +1,24 @@
+package cli
+
+import (
+	"fmt"
+	"github.com/provenance-io/provenance/x/epoch/types"
+
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+
+)
+
+// GetTxCmd returns the transaction commands for this module
+func GetTxCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("%s transactions subcommands", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	return cmd
+}
