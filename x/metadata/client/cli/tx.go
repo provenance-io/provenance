8ffe6103b9f70d89a333f037f7d57261e41cb0cc
Matthew Witkowski <mwitkowski@figure.com>
2022-04-04 10:02:31 -0400

    Test commit with signature.

--- x/metadata/client/cli/tx.go
+++ x/metadata/client/cli/tx.go
@@ -385,7 +385,7 @@ func WriteScopeSpecificationCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     "write-scope-specification [specification-id] [owner-addresses] [responsible-parties] [contract-specification-ids] [description-name, optional] [description, optional] [website-url, optional] [icon-url, optional]",
 		Short:   "Add/Update metadata scope specification to the provenance blockchain",
-		Example: fmt.Sprintf(`$ %[1]s tx metadata write-scope-specification scopespec1qn7jh3jvw4gytq9r5x770e8yj74s9t479r pb1sh49f6ze3vn7cdl2amh2gnc70z5mten3dpvr42 "owner" contractspec1q0mnck3mqh75mg9qvykqq0jxzs2struaa8`, version.AppName),
+		Example: fmt.Sprintf(`$ %[1]s tx metadata write-scope-specification  scopespec1qn7jh3jvw4gytq9r5x770e8yj74s9t479r pb1sh49f6ze3vn7cdl2amh2gnc70z5mten3dpvr42 "owner" contractspec1q0mnck3mqh75mg9qvykqq0jxzs2struaa8`, version.AppName),
 		Args:    cobra.RangeArgs(4, 8),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
