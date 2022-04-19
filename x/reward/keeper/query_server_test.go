f98b08e2442ca6332c084d8ede93438c95e276c1
Matthew Witkowski <mwitkowski@figure.com>
2022-04-19 13:35:33 -0400

    Added test to test n+1

--- x/reward/keeper/query_server_test.go
+++ x/reward/keeper/query_server_test.go
@@ -29,6 +29,18 @@ func (s *KeeperTestSuite) TestQueryRewardPrograms() {
 	s.Assert().Nil(err, "Error should be nil")
 	s.Assert().Equal(len(response.RewardPrograms), 1, "response should contain the added element")
 	s.Assert().True(rewardProgram.Equal(response.RewardPrograms[0]), "reward programs should match")
+
+	// Test with 2 items
+	action2 := types.NewActionDelegate()
+	coin2 := sdk.NewInt64Coin("catthejack", 10000)
+	maxCoin2 := sdk.NewInt64Coin("catthejack", 100)
+	rewardProgram2 := types.NewRewardProgram(2, "cosmos1v57fx2l2rt6ehujuu99u2fw05779m5e2ux4z2h", coin2, maxCoin2, "day", 1, 10, types.NewEligibilityCriteria("criteria", &action2), false, 1, 10)
+	s.app.RewardKeeper.SetRewardProgram(s.ctx, rewardProgram2)
+
+	response, err = queryClient.RewardPrograms(gocontext.Background(), &types.RewardProgramsRequest{})
+	s.Assert().Nil(err, "Error should be nil")
+	s.Assert().Equal(len(response.RewardPrograms), 2, "response should contain the added element")
+	s.Assert().True(rewardProgram2.Equal(response.RewardPrograms[1]), "reward programs should match")
 }
 
 func (suite *KeeperTestSuite) TestActiveRewardPrograms() {
