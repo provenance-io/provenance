96facbe4d54acba3a6729dc998724dffa74ff66f
Matthew Witkowski <mwitkowski@figure.com>
2022-04-21 17:02:55 -0400

    Added skeleton in place for reward-claim commands.

--- x/reward/client/cli/query.go
+++ x/reward/client/cli/query.go
@@ -25,6 +25,7 @@ func GetQueryCmd() *cobra.Command {
 	}
 	queryCmd.AddCommand(
 		GetRewardProgramCmd(),
+		GetRewardClaimCmd(),
 	)
 	return queryCmd
 }
@@ -53,6 +54,31 @@ func GetRewardProgramCmd() *cobra.Command {
 			}
 
 			return outputRewardProgramById(cmd, arg0)
+		},
+	}
+
+	return cmd
+}
+
+func GetRewardClaimCmd() *cobra.Command {
+	const all = "all"
+
+	cmd := &cobra.Command{
+		Use:     "reward-claim {address|\"all\"}",
+		Aliases: []string{"rc", "rewardclaim"},
+		Short:   "Query the current reward claims",
+		Long: fmt.Sprintf(`%[1]s reward-claim {address} - gets the reward claim for the address.
+%[1]s reward-claim all - gets all the reward claims`, cmdStart),
+		Args: cobra.ExactArgs(1),
+		Example: fmt.Sprintf(`%[1]s reward-claim "pb1skjwj5whet0lpe65qaq4rpq03hjxlwd9nf39lk"
+%[1]s reward-claim all`, cmdStart),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			arg0 := strings.TrimSpace(args[0])
+			if arg0 == all {
+				return outputRewardClaimsAll(cmd)
+			}
+
+			return outputRewardClaimById(cmd, arg0)
 		},
 	}
 
@@ -119,6 +145,55 @@ func outputRewardProgramById(cmd *cobra.Command, arg string) error {
 
 	if response.GetRewardProgram() == nil {
 		return fmt.Errorf("reward program %d does not exist", programId)
+	}
+
+	return clientCtx.PrintProto(response)
+}
+
+func outputRewardClaimsAll(cmd *cobra.Command) error {
+	return fmt.Errorf("not implemented yet")
+
+	clientCtx, err := client.GetClientQueryContext(cmd)
+	if err != nil {
+		return err
+	}
+
+	queryClient := types.NewQueryClient(clientCtx)
+
+	var response *types.RewardProgramsResponse
+	if response, err = queryClient.RewardPrograms(
+		context.Background(),
+		&types.RewardProgramsRequest{},
+	); err != nil {
+		return fmt.Errorf("failed to query reward programs: %s", err.Error())
+	}
+
+	return clientCtx.PrintProto(response)
+}
+
+func outputRewardClaimById(cmd *cobra.Command, arg string) error {
+	return fmt.Errorf("not implemented yet")
+
+	clientCtx, err := client.GetClientQueryContext(cmd)
+	if err != nil {
+		return err
+	}
+	queryClient := types.NewQueryClient(clientCtx)
+	id, err := strconv.Atoi(arg)
+	if err != nil {
+		return err
+	}
+
+	var response *types.RewardProgramByIDResponse
+	if response, err = queryClient.RewardProgramByID(
+		context.Background(),
+		&types.RewardProgramByIDRequest{Id: uint64(id)},
+	); err != nil {
+		return fmt.Errorf("failed to query reward claim %d: %s", id, err.Error())
+	}
+
+	if response.GetRewardProgram() == nil {
+		return fmt.Errorf("reward claim %d does not exist", id)
 	}
 
 	return clientCtx.PrintProto(response)
