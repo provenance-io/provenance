dde59d606553b6d49eaf821df92ede9fd089c310
Matthew Witkowski <mwitkowski@figure.com>
2022-04-21 16:30:20 -0400

    Refactored query.go, and changed the way we interact with reward-programs. This was done to make room for other commands.

--- x/reward/client/cli/query.go
+++ x/reward/client/cli/query.go
@@ -4,13 +4,16 @@ import (
 	"context"
 	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/version"
 	"github.com/spf13/cobra"
 
 	"github.com/provenance-io/provenance/x/reward/types"
 )
+
+var cmdStart = fmt.Sprintf("%s query reward", version.AppName)
 
 func GetQueryCmd() *cobra.Command {
 	queryCmd := &cobra.Command{
@@ -21,100 +24,102 @@ func GetQueryCmd() *cobra.Command {
 		RunE:                       client.ValidateCmd,
 	}
 	queryCmd.AddCommand(
-		QueryRewardProgramsCmd(),
-		RewardProgramByIdCmd(),
+		GetRewardProgramCmd(),
 	)
 	return queryCmd
 }
 
-func QueryRewardProgramsCmd() *cobra.Command {
+func GetRewardProgramCmd() *cobra.Command {
+	const all = "all"
+	const active = "active"
+
 	cmd := &cobra.Command{
-		Use:     "list",
-		Aliases: []string{"ls", "l"},
-		Short:   "List all the reward programs on the Provenance Blockchain. Optionally the '-a' flag can be set to list only active reward programs.",
-		Args:    cobra.NoArgs,
+		Use:     "reward-program {reward_program_id|\"all\"|\"active\"}",
+		Aliases: []string{"rp", "rewardprogram"},
+		Short:   "Query the current reward programs",
+		Long: fmt.Sprintf(`%[1]s reward-program {reward_program_id} - gets the reward program for a given id.
+%[1]s reward-program all - gets all the reward programs
+%[1]s reward-program active - gets all active the reward programs`, cmdStart),
+		Args: cobra.ExactArgs(1),
+		Example: fmt.Sprintf(`%[1]s reward-program 1
+%[1]s reward-program all
+%[1]s reward-program active`, cmdStart),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx, err := client.GetClientQueryContext(cmd)
-			if err != nil {
-				return err
-			}
-			queryClient := types.NewQueryClient(clientCtx)
-
-			// We need our response
-
-			isActive, err := cmd.Flags().GetBool("active")
-			if err != nil {
-				return err
+			arg0 := strings.TrimSpace(args[0])
+			if arg0 == all {
+				return outputRewardProgramsAll(cmd)
+			} else if arg0 == active {
+				return outputRewardProgramsActive(cmd)
 			}
 
-			if isActive {
-				var response *types.ActiveRewardProgramsResponse
-				if response, err = queryClient.ActiveRewardPrograms(
-					context.Background(),
-					&types.ActiveRewardProgramsRequest{},
-				); err != nil {
-					fmt.Printf("failed to query active reward programs: %s\n", err.Error())
-					return nil
-				}
-				return clientCtx.PrintProto(response)
-			} else {
-				var response *types.RewardProgramsResponse
-				if response, err = queryClient.RewardPrograms(
-					context.Background(),
-					&types.RewardProgramsRequest{},
-				); err != nil {
-					fmt.Printf("failed to query reward programs: %s\n", err.Error())
-					return nil
-				}
-				return clientCtx.PrintProto(response)
-			}
+			return outputRewardProgramById(cmd, arg0)
 		},
 	}
-
-	//flags.AddPaginationFlagsToCmd(cmd, "markers")
-	flags.AddQueryFlagsToCmd(cmd)
-	cmd.Flags().Bool("active", false, "Queries for only the active reward programs.")
 
 	return cmd
 }
 
-func RewardProgramByIdCmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:     "id [reward program id]",
-		Aliases: []string{},
-		Short:   "Output the reward program for the specified id",
-		Args:    cobra.ExactArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx, err := client.GetClientQueryContext(cmd)
-			if err != nil {
-				return err
-			}
-			queryClient := types.NewQueryClient(clientCtx)
-			programId, err := strconv.Atoi(args[0])
-			if err != nil {
-				return err
-			}
-
-			// We need our response
-			var response *types.RewardProgramByIDResponse
-			if response, err = queryClient.RewardProgramByID(
-				context.Background(),
-				&types.RewardProgramByIDRequest{Id: uint64(programId)},
-			); err != nil {
-				fmt.Printf("failed to query reward programs: %s\n", err.Error())
-				return nil
-			}
-
-			if response.GetRewardProgram() == nil {
-				fmt.Printf("reward program %d does not exist.\n", programId)
-				return nil
-			}
-
-			return clientCtx.PrintProto(response)
-		},
+// Query for all Reward Programs
+func outputRewardProgramsAll(cmd *cobra.Command) error {
+	clientCtx, err := client.GetClientQueryContext(cmd)
+	if err != nil {
+		return err
 	}
 
-	flags.AddQueryFlagsToCmd(cmd)
+	queryClient := types.NewQueryClient(clientCtx)
 
-	return cmd
+	var response *types.RewardProgramsResponse
+	if response, err = queryClient.RewardPrograms(
+		context.Background(),
+		&types.RewardProgramsRequest{},
+	); err != nil {
+		return fmt.Errorf("failed to query reward programs: %s", err.Error())
+	}
+
+	return clientCtx.PrintProto(response)
+}
+
+// Query for all active Reward Programs
+func outputRewardProgramsActive(cmd *cobra.Command) error {
+	clientCtx, err := client.GetClientQueryContext(cmd)
+	if err != nil {
+		return err
+	}
+	queryClient := types.NewQueryClient(clientCtx)
+
+	var response *types.ActiveRewardProgramsResponse
+	if response, err = queryClient.ActiveRewardPrograms(
+		context.Background(),
+		&types.ActiveRewardProgramsRequest{},
+	); err != nil {
+		return fmt.Errorf("failed to query active reward programs: %s", err.Error())
+	}
+	return clientCtx.PrintProto(response)
+}
+
+// Query for a RewardProgram by Id
+func outputRewardProgramById(cmd *cobra.Command, arg string) error {
+	clientCtx, err := client.GetClientQueryContext(cmd)
+	if err != nil {
+		return err
+	}
+	queryClient := types.NewQueryClient(clientCtx)
+	programId, err := strconv.Atoi(arg)
+	if err != nil {
+		return err
+	}
+
+	var response *types.RewardProgramByIDResponse
+	if response, err = queryClient.RewardProgramByID(
+		context.Background(),
+		&types.RewardProgramByIDRequest{Id: uint64(programId)},
+	); err != nil {
+		return fmt.Errorf("failed to query reward program %d: %s", programId, err.Error())
+	}
+
+	if response.GetRewardProgram() == nil {
+		return fmt.Errorf("reward program %d does not exist", programId)
+	}
+
+	return clientCtx.PrintProto(response)
 }
