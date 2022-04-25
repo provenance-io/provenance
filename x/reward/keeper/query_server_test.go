e0b12e77b884239a4720b1ebdf4f52db94b35711
Matthew Witkowski <mwitkowski@figure.com>
2022-04-25 18:40:01 -0400

    Finished initial implementation for epoch reward distribution. Still need to write tests.

--- x/reward/keeper/query_server_test.go
+++ x/reward/keeper/query_server_test.go
@@ -222,3 +222,104 @@ func setupEpoch(s *KeeperTestSuite, height uint64) {
 		},
 	})
 }
+
+func (s *KeeperTestSuite) TestEpochDistributionRewards() {
+	s.SetupTest()
+	queryClient := s.queryClient
+
+	// Test against empty set
+	response, err := queryClient.RewardClaims(gocontext.Background(), &types.RewardClaimsRequest{})
+	s.Assert().Nil(err, "error should be nil")
+	s.Assert().Equal(len(response.RewardClaims), 0, "response should contain empty list")
+
+	// Test with 1 item
+	rewardClaim := types.NewRewardClaim("testing", []types.SharesPerEpochPerRewardsProgram{
+		{
+			RewardProgramId:      1,
+			TotalShares:          2,
+			EphemeralActionCount: 3,
+			LatestRecordedEpoch:  4,
+			Claimed:              false,
+			Expired:              false,
+			TotalRewardClaimed:   sdk.NewInt64Coin("jackthecat", 100),
+		},
+	})
+	s.app.RewardKeeper.SetRewardClaim(s.ctx, rewardClaim)
+	response, err = queryClient.RewardClaims(gocontext.Background(), &types.RewardClaimsRequest{})
+	s.Assert().Nil(err, "error should be nil")
+	s.Assert().Equal(1, len(response.RewardClaims), "response should contain one reward claim")
+
+	// Test with multiple item
+	rewardClaim = types.NewRewardClaim("testing2", []types.SharesPerEpochPerRewardsProgram{
+		{
+			RewardProgramId:      2,
+			TotalShares:          3,
+			EphemeralActionCount: 4,
+			LatestRecordedEpoch:  5,
+			Claimed:              false,
+			Expired:              false,
+			TotalRewardClaimed:   sdk.NewInt64Coin("jackthecat", 200),
+		},
+	})
+	s.app.RewardKeeper.SetRewardClaim(s.ctx, rewardClaim)
+	response, err = queryClient.RewardClaims(gocontext.Background(), &types.RewardClaimsRequest{})
+	s.Assert().Nil(err, "error should be nil")
+	s.Assert().Equal(2, len(response.RewardClaims), "response should contain multiple reward claims")
+}
+
+func (s *KeeperTestSuite) TestEpochDistributionRewardsByID() {
+	s.SetupTest()
+	queryClient := s.queryClient
+
+	rewardClaim := types.NewRewardClaim("testing", []types.SharesPerEpochPerRewardsProgram{
+		{
+			RewardProgramId:      1,
+			TotalShares:          2,
+			EphemeralActionCount: 3,
+			LatestRecordedEpoch:  4,
+			Claimed:              false,
+			Expired:              false,
+			TotalRewardClaimed:   sdk.NewInt64Coin("jackthecat", 100),
+		},
+	})
+	s.app.RewardKeeper.SetRewardClaim(s.ctx, rewardClaim)
+	rewardClaim = types.NewRewardClaim("testing2", []types.SharesPerEpochPerRewardsProgram{
+		{
+			RewardProgramId:      2,
+			TotalShares:          3,
+			EphemeralActionCount: 4,
+			LatestRecordedEpoch:  5,
+			Claimed:              false,
+			Expired:              false,
+			TotalRewardClaimed:   sdk.NewInt64Coin("jackthecat", 200),
+		},
+	})
+	s.app.RewardKeeper.SetRewardClaim(s.ctx, rewardClaim)
+
+	response, err := queryClient.RewardClaimByID(gocontext.Background(), &types.RewardClaimByIDRequest{Id: "testing"})
+	s.Assert().Nil(err, "error should be nil")
+	s.Assert().NotNil(response.GetRewardClaim(), "RewardClaim should not be nil")
+	s.Assert().Equal(len(response.GetRewardClaim().SharesPerEpochPerReward), 1, "Length of shares should match")
+	s.Assert().Equal("testing", response.GetRewardClaim().Address, "RewardClaim address should match")
+	s.Assert().Equal(uint64(1), response.GetRewardClaim().SharesPerEpochPerReward[0].RewardProgramId, "RewardProgramId should match")
+	s.Assert().Equal(int64(2), response.GetRewardClaim().SharesPerEpochPerReward[0].TotalShares, "TotalShares should match")
+	s.Assert().Equal(int64(3), response.GetRewardClaim().SharesPerEpochPerReward[0].EphemeralActionCount, "EphemeralActionCount should match")
+	s.Assert().Equal(uint64(4), response.GetRewardClaim().SharesPerEpochPerReward[0].LatestRecordedEpoch, "LatestRecordedEpoch should match")
+	s.Assert().False(response.GetRewardClaim().SharesPerEpochPerReward[0].Claimed, "Claimed should match")
+	s.Assert().False(response.GetRewardClaim().SharesPerEpochPerReward[0].Expired, "Expired should match")
+	s.Assert().Equal(sdk.NewInt64Coin("jackthecat", 100), response.GetRewardClaim().SharesPerEpochPerReward[0].TotalRewardClaimed, "")
+
+	response, err = queryClient.RewardClaimByID(gocontext.Background(), &types.RewardClaimByIDRequest{Id: "testing3"})
+	s.Assert().Nil(err, "error should be nil")
+	s.Assert().Nil(response.GetRewardClaim(), "RewardClaim should be nil")
+}
+
+func newRewardProgram(coinName string, id uint64, start uint64) *types.RewardProgram {
+	action := types.NewActionDelegate()
+	coin := sdk.NewInt64Coin(coinName, 10000)
+	maxCoin := sdk.NewInt64Coin(coinName, 100)
+
+	rewardProgram := types.NewRewardProgram(id, "cosmos1v57fx2l2rt6ehujuu99u2fw05779m5e2ux4z2h", coin, maxCoin, "day", start, 2, types.NewEligibilityCriteria("criteria", &action), false, 1, 10)
+
+	return &rewardProgram
+}
