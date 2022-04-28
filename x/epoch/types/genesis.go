494da2ff6f6ed4ef33967fa8a05e19bf19f8fba5
Carlton Hanna <channa@figure.com>
2022-04-28 10:10:53 -0600

    remove testing epoch

--- x/epoch/types/genesis.go
+++ x/epoch/types/genesis.go
@@ -15,14 +15,6 @@ func NewGenesisState(epochs []EpochInfo) *GenesisState {
 func DefaultGenesis() *GenesisState {
 	epochs := []EpochInfo{
 		{
-			Identifier:              "minute",
-			StartHeight:             0,
-			Duration:                uint64(60 / 5), //duration in blocks
-			CurrentEpoch:            0,
-			CurrentEpochStartHeight: 0,
-			EpochCountingStarted:    false,
-		},
-		{
 			Identifier:              "week",
 			StartHeight:             0,
 			Duration:                uint64((24 * 60 * 60 * 7) / 5), //duration in blocks
