7a2ea6847238295fd2f70ae81edf64bc1f8caabb
Daniel Wedul <dwedul@figure.com>
2022-05-13 16:54:16 -0600

    Add a comment to the test-sim-nondeterminism-state-listening-kafka make target to explain why it's different from the others.

--- sims-state-listening.mk
+++ sims-state-listening.mk
@@ -22,6 +22,14 @@ test-sim-nondeterminism-state-listening-trace:
 SIM_DOCKER_COMPOSE_YML ?= vendor/github.com/cosmos/cosmos-sdk/plugin/plugins/kafka/docker-compose.yml
 
 test-sim-nondeterminism-state-listening-kafka: vendor
+	# This is done as a single command for the following reasons:
+	# - I want the exit code to be that of the test and not the stopping of kafka.
+	# - I want to fail early if kafka can't be started.
+	# - I want to stop kafka command to run regardless of the exit code of the test.
+	# - By default make runs each command in a new shell. That makes it impossible to store
+	#     the exit code of the test command for use after the kafka command if they're not all one command.
+	# I've left of the @ so that we can see the entire go test command and the docker commands.
+	# They end up sepparated from where they're executed, but the echos make them identifiable in the output.
 	echo "Running non-determinism-state-listening-kafka test..."; \
 		echo "Starting Kafka..."; \
 		docker-compose -f $(SIM_DOCKER_COMPOSE_YML) up -d zookeeper broker || exit $$?; \
