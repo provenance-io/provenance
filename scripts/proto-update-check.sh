c1d6a0bfb8f8f2e6b549896efa88052a30decfc1
Ergels Gaxhaj <egaxhaj@figure.com>
2022-06-01 17:38:20 -0500

    turn output color off as it is unpredictable

--- scripts/proto-update-check.sh
+++ scripts/proto-update-check.sh
@@ -6,28 +6,24 @@ set -ex
 # Warn user to update third_party libraries and push again.
 #
 
-red='\033[1;31m'
-blue='\033[;34m'
-off='\033[0m'
-
 regex='github.com/cosmos/cosmos-sdk|github.com/tendermint/tendermint'
 output=$(git diff ..origin/main -- go.mod | grep -E -c $regex)
 dir="$( cd "$( dirname "${BASH_SOURCE:-$0}" )/.."; pwd -P )"
 
 # single brackets because of `set -e` option above.
 if [ "$output" -gt 0 ]; then
-  echo -e "${blue}Downloading latest third_party proto files for comparison...${off}"
+  echo -e "Downloading latest third_party proto files for comparison..."
 
   # Download third_party proto files int build/ directory for comparison against $dir /third_party
   bash "$dir"/scripts/proto-update-deps.sh build
 
-  echo -e "\n${blue}Checking Protobuf files for differences...${off}\n"
+  echo -e "\nChecking Protobuf files for differences...\n"
 
   DIFF=$(diff -rq -x '*.yaml' --exclude=google "$dir"/build/third_party "$dir"/third_party || true)
   if [ -n "$DIFF" ]; then
-    echo -e "${blue}\n\nDiff log:\n\n$DIFF${off}\n\n"
+    echo -e "\n\nDiff log:\n\n$DIFF\n\n"
 
-    echo -e "${red}\nFound differences in Protobuf files.\n
+    echo -e "Found differences in Protobuf files.\n
     This indicates a version change was detected in one of the following libraries:
       - github.com/cosmos/cosmos-sdk
       - github.com/tendermint/tendermint
@@ -36,7 +32,7 @@ if [ "$output" -gt 0 ]; then
       1. run: make proto-update-deps
       2. run: make proto-update-check
       3. run: diff -rq -x '*.yaml' --exclude=google build/third_party third_party
-      4. commit updates and push\n${off}" >&2
+      4. commit updates and push\n" >&2
 
     exit 1
   fi
--- scripts/proto-update-deps.sh
+++ scripts/proto-update-deps.sh
@@ -5,18 +5,14 @@ set -ex
 # Download third_party proto files from the versions declared in go.mod
 #
 
-red='\033[1;31m'
-green='\033[;32m'
-off='\033[0m'
-
 __generate_usage () {
-  echo -e "${green}Updates third_party Protobuf files
+  echo -e "Updates third_party Protobuf files
 
 Usage: ./proto-update-deps.sh [dest]
 
 The dest is optional.
   The default location is ./third_party/proto/
-  If a dest is supplied then the path becomes ./<dest>/third_party/proto/.${off}
+  If a dest is supplied then the path becomes ./<dest>/third_party/proto/.
 
 "
 }
@@ -51,11 +47,11 @@ PROTO_EXPR="*/proto/**/*.proto"
 # gnu tar on ubuntu requires the '--wildcards' flag
 # check and warn user to install gnu tar to be able to also run the script on macOS.
 if [[ "$OSTYPE" == "darwin"* ]] && [[ $(command -v gtar) == "" ]]; then
- echo -e "${red}\nYou must install GNU Tar in order to use the '--wildcards' flag with the tar command.
+ echo -e "\nYou must install GNU Tar in order to use the '--wildcards' flag with the tar command.
  Run: brew install gnu-tar\n
  If you need ot use it as \"tar\", you can add a \"gnubin\" directory
  to your PATH from your bashrc like:\n
-    PATH=\"\$(brew --prefix)/opt/gnu-tar/libexec/gnubin:\$PATH\"${off}\n"
+    PATH=\"\$(brew --prefix)/opt/gnu-tar/libexec/gnubin:\$PATH\"\n"
  exit 1
 fi
 
