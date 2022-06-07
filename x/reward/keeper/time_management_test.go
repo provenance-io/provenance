507dd56d27559104c1e320c168e8dc4dce2d2149
Matthew Witkowski <mwitkowski@figure.com>
2022-06-07 16:30:46 -0400

    Created tests for time_management.

---
+++ x/reward/keeper/time_management_test.go
@@ -0,0 +1,31 @@
+package keeper_test
+
+func (suite *KeeperTestSuite) TestStartRewardProgram() {
+	suite.SetupTest()
+	suite.Assert().Fail("not implemented yet")
+}
+
+func (suite *KeeperTestSuite) TestEndRewardProgram() {
+	suite.SetupTest()
+	suite.Assert().Fail("not implemented yet")
+}
+
+func (suite *KeeperTestSuite) TestRewardProgramEpochEnd() {
+	suite.SetupTest()
+	suite.Assert().Fail("not implemented yet")
+}
+
+func (suite *KeeperTestSuite) TestRewardProgramEpochEndTransition() {
+	suite.SetupTest()
+	suite.Assert().Fail("not implemented yet")
+}
+
+func (suite *KeeperTestSuite) TestCleanup() {
+	suite.SetupTest()
+	suite.Assert().Fail("not implemented yet")
+}
+
+func (suite *KeeperTestSuite) TestUpdate() {
+	suite.SetupTest()
+	suite.Assert().Fail("not implemented yet")
+}
