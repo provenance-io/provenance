cda4c971596100d4c9cdf54949aabaf9580a3464
Ergels Gaxhaj <87285445+egaxhaj@users.noreply.github.com>
2022-06-10 14:31:40 -0500

    unblock proto update check (#843)
    * unblcok proto update check
    
    * test workflow process
    
    * fix workflow
    
    * fix git diff cmd in script for workflow to work
    
    * fix proto scripts
    
    * fetch all so we can run git diff
    
    * trigger proto update check
    
    * fix script
    
    * try with bash
    
    * run as bash
    
    * revert makefile
    
    * use single bracket with set -e
    
    * gtar requires --wildcards flag
    
    * add os check
    
    * use tput for output color
    
    * use \033 as tput does not work with wf
    
    * turn output color off as it is unpredictable
    
    * update changelog
    
    * line formating
    
    * update third_party protos
    
    * ignore exit code on grep no match
    
    * [842]: Update changelog phrasing.
    
    * [842]: Clean up the proto-update scripts. Don't require a specific version of tar. Remove multi-line echo invocations.
    
    * Update the proto github workflow to run all checks each time.
    
    Co-authored-by: Daniel Wedul <dwedul@figure.com>

--- scripts/proto-update-deps.sh
+++ scripts/proto-update-deps.sh
@@ -5,12 +5,23 @@ set -ex
 # Download third_party proto files from the versions declared in go.mod
 #
 
+cat << EOF
+Updates third_party Protobuf files
+
+Usage: ./proto-update-deps.sh [dest]
+
+The dest is optional.
+  The default location is ./third_party/proto/
+  If a dest is supplied then the path becomes ./<dest>/third_party/proto/.
+
+EOF
 
 dir="$( cd "$( dirname "${BASH_SOURCE:-$0}" )/.."; pwd -P )"
-EXT_PROTO_DIR="$dir"/third_party
+dest="$dir${1:+/$1}"
 
-# clean
-#rm -rf "${EXT_PROTO_DIR:?}/"*
+EXT_PROTO_DIR="$dest"/third_party
+
+echo "$EXT_PROTO_DIR"
 
 # Retrieve versions from go.mod (single source of truth)
 CONFIO_PROTO_URL=https://raw.githubusercontent.com/confio/ics23/go/$(go list -m github.com/confio/ics23/go | sed 's:.* ::')/proofs.proto
@@ -27,15 +38,22 @@ mkdir -p "$EXT_PROTO_DIR"/proto
 #cp -r "$dir"/third_party/proto/google "$EXT_PROTO_DIR"/proto || exit $?
 cd "$EXT_PROTO_DIR"
 PROTO_EXPR="*/proto/**/*.proto"
+
+# gnu tar on ubuntu requires the '--wildcards' flag
+tar='tar zx --strip-components 1'
+if tar --version 2> /dev/null | grep GNU > /dev/null 2>&1; then
+  tar="$tar --wildcards"
+fi
+
 curl -f -sSL "$CONFIO_PROTO_URL" -o proto/proofs.proto.orig --create-dirs
 curl -f -sSL "$GOGO_PROTO_URL" -o proto/gogoproto/gogo.proto --create-dirs
 curl -f -sSL "$COSMOS_PROTO_URL" -o proto/cosmos_proto/cosmos.proto --create-dirs
-curl -f -sSL "$COSMWASM_TARBALL_URL" | tar zx --strip-components 1 --exclude="*/third_party" "$PROTO_EXPR"
-curl -f -sSL "$COSMWASM_TARBALL_URL" | tar zx --strip-components 1 --exclude="*/third_party" "$PROTO_EXPR"
-curl -f -sSL "$WASMD_TARBALL_URL" | tar zx --strip-components 1 --exclude="*/third_party" --exclude="*/proto/ibc" "$PROTO_EXPR"
-curl -f -sSL "$IBC_GO_TARBALL_URL" | tar zx --strip-components 1 --exclude="*/third_party" "$PROTO_EXPR"
-curl -f -sSL "$COSMOS_TARBALL_URL" | tar zx --strip-components 1 --exclude="*/third_party" --exclude="*/testutil" "$PROTO_EXPR"
-curl -f -sSL "$TM_TARBALL_URL" | tar zx --strip-components 1 --exclude="*/third_party" "$PROTO_EXPR"
+curl -f -sSL "$COSMWASM_TARBALL_URL" | $tar --exclude="*/third_party" "$PROTO_EXPR"
+curl -f -sSL "$COSMWASM_TARBALL_URL" | $tar --exclude="*/third_party" "$PROTO_EXPR"
+curl -f -sSL "$WASMD_TARBALL_URL" | $tar --exclude="*/third_party" --exclude="*/proto/ibc" "$PROTO_EXPR"
+curl -f -sSL "$IBC_GO_TARBALL_URL" | $tar --exclude="*/third_party" "$PROTO_EXPR"
+curl -f -sSL "$COSMOS_TARBALL_URL" | $tar --exclude="*/third_party" --exclude="*/testutil" "$PROTO_EXPR"
+curl -f -sSL "$TM_TARBALL_URL" | $tar --exclude="*/third_party" "$PROTO_EXPR"
 
 ## insert go, java package option into proofs.proto file
 ## Issue link: https://github.com/confio/ics23/issues/32 (instead of a simple sed we need 4 lines cause bsd sed -i is incompatible)
