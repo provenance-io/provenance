acbe65661fc888e5d5092b8fbc1327ef006c0397
Carlton Hanna <73041609+channa-figure@users.noreply.github.com>
2022-06-16 15:29:20 -0600

    Add new param setting in upgrade handler  (#875)
    * Add new upgrade handler, Clean up unused upgrade method and tests
    
    * Add changelog
    
    * Upgrade comment
    
    * Update nhash per usd for 0.04 hash price

--- app/upgrades_test.go
+++ app/upgrades_test.go
@@ -6,11 +6,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
-
-	attributetypes "github.com/provenance-io/provenance/x/attribute/types"
-	markertypes "github.com/provenance-io/provenance/x/marker/types"
-	metadatatypes "github.com/provenance-io/provenance/x/metadata/types"
-	nametypes "github.com/provenance-io/provenance/x/name/types"
 )
 
 type IntegrationTestSuite struct {
@@ -23,34 +18,4 @@ type IntegrationTestSuite struct {
 func (s *IntegrationTestSuite) SetupSuite() {
 	s.app = Setup(false)
 	s.ctx = s.app.BaseApp.NewContext(false, tmproto.Header{})
-}
-
-func (s *IntegrationTestSuite) TestMarkerProposals() {
-	err := AddMsgBasedFees(s.app, s.ctx)
-	s.Assert().NoError(err)
-
-	msgfee, err := s.app.MsgFeesKeeper.GetMsgFee(s.ctx, sdk.MsgTypeURL(&nametypes.MsgBindNameRequest{}))
-	s.Assert().NoError(err)
-	s.Assert().NotNil(msgfee)
-	s.Assert().Equal(sdk.NewCoin("nhash", sdk.NewInt(10_000_000_000)), msgfee.AdditionalFee)
-
-	msgfee, err = s.app.MsgFeesKeeper.GetMsgFee(s.ctx, sdk.MsgTypeURL(&markertypes.MsgAddMarkerRequest{}))
-	s.Assert().NoError(err)
-	s.Assert().NotNil(msgfee)
-	s.Assert().Equal(sdk.NewCoin("nhash", sdk.NewInt(100_000_000_000)), msgfee.AdditionalFee)
-
-	msgfee, err = s.app.MsgFeesKeeper.GetMsgFee(s.ctx, sdk.MsgTypeURL(&attributetypes.MsgAddAttributeRequest{}))
-	s.Assert().NoError(err)
-	s.Assert().NotNil(msgfee)
-	s.Assert().Equal(sdk.NewCoin("nhash", sdk.NewInt(10_000_000_000)), msgfee.AdditionalFee)
-
-	msgfee, err = s.app.MsgFeesKeeper.GetMsgFee(s.ctx, sdk.MsgTypeURL(&metadatatypes.MsgWriteScopeRequest{}))
-	s.Assert().NoError(err)
-	s.Assert().NotNil(msgfee)
-	s.Assert().Equal(sdk.NewCoin("nhash", sdk.NewInt(10_000_000_000)), msgfee.AdditionalFee)
-
-	msgfee, err = s.app.MsgFeesKeeper.GetMsgFee(s.ctx, sdk.MsgTypeURL(&metadatatypes.MsgP8EMemorializeContractRequest{}))
-	s.Assert().NoError(err)
-	s.Assert().NotNil(msgfee)
-	s.Assert().Equal(sdk.NewCoin("nhash", sdk.NewInt(10_000_000_000)), msgfee.AdditionalFee)
 }
