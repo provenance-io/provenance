5e32fadc105b06fdf688ab253b44bf451ff0060b
Daniel Wedul <dwedul@figure.com>
2022-06-27 12:23:03 -0600

    Remove the mango-rc2 and -rc4 upgrade handlers. The logic in there is already replicated in the mango handler.

--- app/upgrades.go
+++ app/upgrades.go
@@ -36,20 +36,6 @@ var handlers = map[string]appUpgrade{
 			return app.mm.RunMigrations(ctx, app.configurator, versionMap)
 		},
 	}, // upgrade for 1.11.x
-	"mango-rc2": {
-		Handler: func(app *App, ctx sdk.Context, plan upgradetypes.Plan) (module.VersionMap, error) {
-			params := app.MsgFeesKeeper.GetParams(ctx)
-			app.MsgFeesKeeper.SetParams(ctx, params)
-			versionMap := app.UpgradeKeeper.GetModuleVersionMap(ctx)
-			return versionMap, nil
-		},
-	}, // upgrade for 1.11.1-rc2
-	"mango-rc4": {
-		Handler: func(app *App, ctx sdk.Context, plan upgradetypes.Plan) (module.VersionMap, error) {
-			versionMap := app.UpgradeKeeper.GetModuleVersionMap(ctx)
-			return app.mm.RunMigrations(ctx, app.configurator, versionMap)
-		},
-	}, // upgrade for 1.11.1-rc4
 
 	// TODO - Add new upgrade definitions here.
 }
