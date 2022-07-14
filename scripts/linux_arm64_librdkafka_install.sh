6aa87bb09c501f0e1db9bf6d5d14d41c8be8e510
Matthew Witkowski <mwitkowski@figure.com>
2022-07-14 14:28:21 -0400

    Started script to install librdkafka.

--- scripts/linux_arm64_librdkafka_install.sh
+++ scripts/linux_arm64_librdkafka_install.sh
@@ -1,8 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
-# We need to download the source code
-# We need to cd into the directory
-# We need to check that the path is in LD_LIBRARY_PATH
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+TEMP=/tmp/librdkafka
+LIB_PATH=/usr/local/lib
+LIB_RDKAFKA=librdkafka.so
+VERSION=v1.8.2
 
-echo "HERE I AM"
+# Check if we have /usr/local/lib/librdkafka.so
+if [ -f "$LIB_PATH/$LIB_RDKAFKA" ]; then
+    echo "librdkafka is already installed"
+else
+    # Build librdkafka
+    git clone -b $VERSION https://github.com/edenhill/librdkafka.git $TEMP
+    cd /tmp/librdkafka
+    ./configure
+    make
+    make install
+    cd ${PWD}
+
+    # Cleanup
+    rm -rf $TEMP
+fi
+
+# Check if we have a library
+if [ ":$LD_LIBRARY_PATH:" = *":$LIB_PATH:"* ]; then
+    echo "LD_LIBRARY_PATH is already set."
+else
+    echo "LD_LIBRARY_PATH is not set. Please set it with export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib"
+fi
