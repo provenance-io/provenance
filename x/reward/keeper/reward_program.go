55191770628956ba8da154061d1de52433d428ea
arnabmitra <arnabmitra.mitra@gmail.com>
2022-07-20 18:34:25 -0600

    Failing test now pass, check fixed for when a reward start time cannot be before BlockStart Time.

--- x/reward/keeper/reward_program.go
+++ x/reward/keeper/reward_program.go
@@ -14,10 +14,10 @@ func (k Keeper) CreateRewardProgram(ctx sdk.Context, rewardProgram types.RewardP
 		return err
 	}
 
-	y1, m1, d1 := ctx.BlockTime().UTC().Date()
-	y2, m2, d2 := rewardProgram.ProgramStartTime.UTC().Date()
-	if y2 >= y1 && m2 >= m1 && d2 >= d1 {
-		return fmt.Errorf("start time is before current block time %v-%v-%v : %v-%v-%v ", y1, m1, d1, y2, m2, d2)
+	y1 := ctx.BlockTime().UTC()
+	y2 := rewardProgram.ProgramStartTime.UTC()
+	if y2.Before(y1) {
+		return fmt.Errorf("start time is before current block time %v : %v ", y2, y1)
 	}
 
 	// error check done in reward Validate()
