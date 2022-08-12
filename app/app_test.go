2cd41e1ba2349a53632abf84487edfea06656d1f
Daniel Wedul <github@wedul.com>
2022-08-12 11:31:13 -0600

    [995]: Bring prepForZeroHeightGenesis inline with the updated simapp version and clean up the TestSimAppExportAndBlockedAddrs to be closer to the cosmos version too (and use the new stuff).

--- app/app_test.go
+++ app/app_test.go
@@ -1,24 +1,28 @@
 package app
 
 import (
-	"encoding/json"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/libs/log"
 
-	abci "github.com/tendermint/tendermint/abci/types"
 	dbm "github.com/tendermint/tm-db"
 
 	sdksim "github.com/cosmos/cosmos-sdk/simapp"
 )
 
 func TestSimAppExportAndBlockedAddrs(t *testing.T) {
-	encCfg := MakeEncodingConfig()
-	db := dbm.NewMemDB()
-	home := t.TempDir()
-	app := New(log.NewTMLogger(log.NewSyncWriter(os.Stdout)), db, nil, true, map[int64]bool{}, home, 0, encCfg, sdksim.EmptyAppOptions{})
+	opts := SetupOptions{
+		Logger:             log.NewTMLogger(log.NewSyncWriter(os.Stdout)),
+		DB:                 dbm.NewMemDB(),
+		InvCheckPeriod:     0,
+		HomePath:           t.TempDir(),
+		SkipUpgradeHeights: map[int64]bool{},
+		EncConfig:          MakeEncodingConfig(),
+		AppOpts:            sdksim.EmptyAppOptions{},
+	}
+	app := NewAppWithCustomOptions(t, false, opts)
 
 	for acc := range maccPerms {
 		require.True(
@@ -28,26 +32,21 @@ func TestSimAppExportAndBlockedAddrs(t *testing.T) {
 		)
 	}
 
-	genesisState := NewDefaultGenesisState(encCfg.Marshaler)
-	stateBytes, err := json.MarshalIndent(genesisState, "", "  ")
-	require.NoError(t, err)
-
-	// Initialize the chain
-	app.InitChain(
-		abci.RequestInitChain{
-			Validators:    []abci.ValidatorUpdate{},
-			AppStateBytes: stateBytes,
-		},
-	)
 	app.Commit()
 
 	// Making a new app object with the db, so that initchain hasn't been called
-	app2 := New(log.NewTMLogger(log.NewSyncWriter(os.Stdout)), db, nil, true, map[int64]bool{}, home, 0, encCfg, sdksim.EmptyAppOptions{})
-	_, err = app2.ExportAppStateAndValidators(false, []string{})
-	require.NoError(t, err, "ExportAppStateAndValidators should not have an error")
+	app2 := New(log.NewTMLogger(log.NewSyncWriter(os.Stdout)), opts.DB, nil, true,
+		map[int64]bool{}, opts.HomePath, 0, opts.EncConfig, sdksim.EmptyAppOptions{})
+	var err error
+	require.NotPanics(t, func() {
+		_, err = app2.ExportAppStateAndValidators(false, []string{})
+	}, "exporting app state at current height")
+	require.NoError(t, err, "ExportAppStateAndValidators at current height")
 
-	_, err = app2.ExportAppStateAndValidators(true, []string{})
-	require.NoError(t, err, "ExportAppStateAndValidators (for zero height) should not have an error")
+	require.NotPanics(t, func() {
+		_, err = app2.ExportAppStateAndValidators(true, []string{})
+	}, "exporting app state at zero height")
+	require.NoError(t, err, "ExportAppStateAndValidators at zero height")
 }
 
 func TestGetMaccPerms(t *testing.T) {
--- app/export.go
+++ app/export.go
@@ -2,9 +2,8 @@ package app
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
-
-	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
 	sdkcodec "github.com/cosmos/cosmos-sdk/codec/types"
 	servertypes "github.com/cosmos/cosmos-sdk/server/types"
@@ -13,6 +12,8 @@ import (
 	slashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
 	"github.com/cosmos/cosmos-sdk/x/staking"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
 	markertypes "github.com/provenance-io/provenance/x/marker/types"
 )
@@ -70,8 +71,7 @@ func (app *App) ExportAppStateAndValidators(
 
 // prepare for fresh start at zero height
 // NOTE zero height genesis is a temporary feature which will be deprecated
-//
-//	in favor of export at a block height
+//      in favour of export at a block height
 func (app *App) prepForZeroHeightGenesis(ctx sdk.Context, jailAllowedAddrs []string) {
 	applyAllowedAddrs := false
 
@@ -109,10 +109,8 @@ func (app *App) prepForZeroHeightGenesis(ctx sdk.Context, jailAllowedAddrs []str
 			panic(err)
 		}
 
-		delAddr, err := sdk.AccAddressFromBech32(delegation.DelegatorAddress)
-		if err != nil {
-			panic(err)
-		}
+		delAddr := sdk.MustAccAddressFromBech32(delegation.DelegatorAddress)
+
 		_, _ = app.DistrKeeper.WithdrawDelegationRewards(ctx, delAddr, valAddr)
 	}
 
@@ -134,9 +132,8 @@ func (app *App) prepForZeroHeightGenesis(ctx sdk.Context, jailAllowedAddrs []str
 		feePool.CommunityPool = feePool.CommunityPool.Add(scraps...)
 		app.DistrKeeper.SetFeePool(ctx, feePool)
 
-		err := app.DistrKeeper.Hooks().AfterValidatorCreated(ctx, val.GetOperator())
-		if err != nil {
-			log.Fatal(err)
+		if err := app.DistrKeeper.Hooks().AfterValidatorCreated(ctx, val.GetOperator()); err != nil {
+			panic(err)
 		}
 		return false
 	})
@@ -147,17 +144,16 @@ func (app *App) prepForZeroHeightGenesis(ctx sdk.Context, jailAllowedAddrs []str
 		if err != nil {
 			panic(err)
 		}
-		delAddr, err := sdk.AccAddressFromBech32(del.DelegatorAddress)
-		if err != nil {
-			panic(err)
+		delAddr := sdk.MustAccAddressFromBech32(del.DelegatorAddress)
+
+		if err := app.DistrKeeper.Hooks().BeforeDelegationCreated(ctx, delAddr, valAddr); err != nil {
+			// never called as BeforeDelegationCreated always returns nil
+			panic(fmt.Errorf("error while incrementing period: %w", err))
 		}
-		err = app.DistrKeeper.Hooks().BeforeDelegationCreated(ctx, delAddr, valAddr)
-		if err != nil {
-			log.Fatal(err)
-		}
-		err = app.DistrKeeper.Hooks().AfterDelegationModified(ctx, delAddr, valAddr)
-		if err != nil {
-			log.Fatal(err)
+
+		if err := app.DistrKeeper.Hooks().AfterDelegationModified(ctx, delAddr, valAddr); err != nil {
+			// never called as AfterDelegationModified always returns nil
+			panic(fmt.Errorf("error while creating a new delegation period record: %w", err))
 		}
 	}
 
@@ -191,7 +187,7 @@ func (app *App) prepForZeroHeightGenesis(ctx sdk.Context, jailAllowedAddrs []str
 	counter := int16(0)
 
 	for ; iter.Valid(); iter.Next() {
-		addr := sdk.ValAddress(iter.Key()[1:])
+		addr := sdk.ValAddress(stakingtypes.AddressFromValidatorsKey(iter.Key()))
 		validator, found := app.StakingKeeper.GetValidator(ctx, addr)
 		if !found {
 			panic("expected validator, not found")
@@ -206,7 +202,10 @@ func (app *App) prepForZeroHeightGenesis(ctx sdk.Context, jailAllowedAddrs []str
 		counter++
 	}
 
-	iter.Close()
+	if err := iter.Close(); err != nil {
+		app.Logger().Error("error while closing the key-value store reverse prefix iterator: ", err)
+		return
+	}
 
 	_, err := app.StakingKeeper.ApplyAndReturnValidatorSetUpdates(ctx)
 	if err != nil {
