c4ee078e06964bb5ec80e9c4e0ba596f70249173
Matthew Witkowski <mwitkowski@figure.com>
2022-08-17 15:18:45 -0400

    Renamed to UpgradeICA because we are configuring ICA. IBC should upgrade automatically from RunMigrations.

--- app/upgrades.go
+++ app/upgrades.go
@@ -46,7 +46,7 @@ var handlers = map[string]appUpgrade{
 		Added: []string{icacontrollertypes.StoreKey, icahosttypes.StoreKey},
 		Handler: func(app *App, ctx sdk.Context, plan upgradetypes.Plan) (module.VersionMap, error) {
 			versionMap := app.UpgradeKeeper.GetModuleVersionMap(ctx)
-			UpdateIBC(ctx, app, &versionMap)
+			UpgradeICA(ctx, app, &versionMap)
 			return app.mm.RunMigrations(ctx, app.configurator, versionMap)
 		},
 	}, // upgrade for 1.12.0-rc1
@@ -117,11 +117,7 @@ func isEmptyUpgrade(upgrades storetypes.StoreUpgrades) bool {
 	return len(upgrades.Renamed) == 0 && len(upgrades.Deleted) == 0 && len(upgrades.Added) == 0
 }
 
-func UpdateIBC(ctx sdk.Context, app *App, versionMap *module.VersionMap) {
-	UpdateIBCv3(ctx, app, versionMap)
-}
-
-func UpdateIBCv3(ctx sdk.Context, app *App, versionMap *module.VersionMap) {
+func UpgradeICA(ctx sdk.Context, app *App, versionMap *module.VersionMap) {
 	app.Logger().Info("Upgrading to IBCv3")
 
 	// Set the consensus version so InitGenesis is not ran
