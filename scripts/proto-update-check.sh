88eea806208d130ff0a3cba6664c193829125e8b
Daniel Wedul <github@wedul.com>
2022-08-22 13:58:45 -0600

    [1017]: Refactor the proto-update-check.sh script. Exit early when it's identified that there aren't any changes. Extract from the update-deps script, the list of libraries to that might have changes of interest. Differences will only be output once. Library differences will be listed (if checked and there are any). Pay attention to a FORCE env var (same as providing the --force flag, but can be done using the make target).

--- scripts/proto-update-check.sh
+++ scripts/proto-update-check.sh
@@ -2,54 +2,94 @@
 set -ex
 
 #
-# Check go.mod for any version updates of tendermint or cosmos-sdk.
-# Warn user to update third_party libraries and push again.
+# Check go.mod for any version updates of third-party proto releated libraries.
+# If any such changes are found, get fresh copies of the protos and compare them to what's in the third_party directory.
+# If any differences are found, issue a warning about needing to update those dependencies.
 #
-
-if [ "$1" == '--force' ]; then
-    output='1'
-else
-    regex='github.com/cosmos/cosmos-sdk|github.com/tendermint/tendermint'
-    output=$(git diff ..origin/main -- go.mod | grep -E -c "$regex" || true)
-fi
+# The check of go.mod can be skipped by either providing the --force flag when executing this script,
+# or by setting the FORCE env var to something (other than an empty string).
+#
+# Exit codes:
+#   0 - Everything is good, no updates needed.
+#   anything else - an update is needed.
 
 # single brackets because the github runners don't have the enhanced (double-bracket) version.
-if [ "$output" -gt "0" ]; then
-  printf 'Downloading latest third_party proto files for comparison...\n'
-
-  # Download third_party proto files in the build/ directory for comparison against /third_party
-  repo_root="$( cd "$( dirname "${BASH_SOURCE:-$0}" )/.."; pwd -P )"
-  fresh_dir="$repo_root/build/third_party"
-  existing_dir="$repo_root/third_party"
-
-  bash "$repo_root/scripts/proto-update-deps.sh" "$fresh_dir"
-
-  printf '\nChecking Protobuf files for differences...\n'
-
-  DIFF=$( diff -rq -x '*.yaml' --exclude=google "$fresh_dir" "$existing_dir" || true )
-  if [ -n "$DIFF" ]; then
-    cat << EOF >&2
-
-Diff log:
-
-$DIFF
-
-Found differences in Protobuf files.
-This indicates a version change was detected in one of the following libraries:
-  - github.com/cosmos/cosmos-sdk
-  - github.com/tendermint/tendermint
-
-Review the diff log above and update accordingly. Perform the following steps locally.
-  1. run: make proto-update-deps
-  2. rerun: make proto-update-check  and make sure it passes.
-  3. commit updates and push
-
-EOF
-    exit 1
-  fi
-
-  # Version change detected but Protobuf files are already up to date
-  exit 0
+if [ "$1" == '--force' ]; then
+    FORCE='1'
 fi
 
-exit 0
+repo_root="$( cd "$( dirname "${BASH_SOURCE:-$0}" )/.."; pwd -P )"
+update_deps="$repo_root/scripts/proto-update-deps.sh"
+
+if [ -z "$FORCE" ]; then
+    # The update-deps script uses go list -m to get library rewrites and version info on several libraries.
+    # Search that script to get all of the libraries it checks up on.
+    # Start by ignoring all commments, then find all go list -m {lib} entries and extract just the library.
+    libs=( $( sed 's:#.*$::' "$update_deps" | grep -Eo 'go list -m [^ ]+' | sed 's:.* ::' ) )
+    # Do our own go list -m to get both the library and possibly what it's rewritten to.
+    # We want to check go.mod for changes in any of those.
+    # We'll build a regexp to provide to grep to match any libraries of interest.
+    libs_regexp=''
+    for lib in "${libs[@]}"; do
+        # go list -m "$lib" will look like one of these:
+        # * '{lib} {version}'
+        # * '{lib} {version} => {rw lib} {rw version}'
+        # Sed replacements:
+        #   s: [^ ]* => :|:  -- Changes [space]{version}[space]=>[space] into a pipe.
+        #   s: [^ ]*$::      -- Gets rid of the version at the end of the line (either {version} or {rw version}).
+        #   s:\.:\\.:        -- Adds a \ before all periods (to escape them for grep).
+        # Result:
+        # * '{lib} {version}' becomes '{lib}' (with periods escaped).
+        # * '{lib} {version} => {rw lib} {rw version}' becomes '{lib}|{rw lib}' (with periods escaped).
+        lib_regexp="$( go list -m "$lib" | sed 's: [^ ]* => :|:;  s: [^ ]*$::;  s:\.:\\.:g;' )"
+        if [ -n "$libs_regexp" ]; then
+          libs_regexp="$libs_regexp|"
+        fi
+        libs_regexp="$libs_regexp$lib_regexp"
+    done
+    # Diff go.mod with main (using ..origin/main here to accomodate github actions) with 0 lines of context.
+    # Get rid of the lines that start with an @ since those are more context info and might contain false positives.
+    # Remove any comments
+    # And look for the libraries from earlier.
+    # If grep finds anything it exits with a 0 so the || exit 0 isn't run.
+    # If grep does not find anything, it'll exit 1 (or more) and the || exit 0 will run; there's no changes of interest, so nothing to do.
+    git diff -U0 ..origin/main -- go.mod \
+        | grep -v '^@' \
+        | sed 's://.*$::' \
+        | grep -E "$libs_regexp" \
+            || exit 0
+fi
+
+# If we get here, either we're forcing the check or there were changes of interest.
+
+printf '\nDownloading latest third_party proto files for comparison...\n'
+
+# Download third_party proto files in the build/ directory for comparison against /third_party
+fresh_dir="$repo_root/build/third_party"
+existing_dir="$repo_root/third_party"
+
+rm -rf "$fresh_dir"
+bash "$update_deps" "$fresh_dir"
+
+printf '\nChecking Protobuf files for differences...\n'
+
+# Diff will exit 0 if there are no changes, and something else if thera are.
+# If there aren't changes, we can be done, all is good.
+# The || true at the end lets us keep going when there are differecnes (since we're running under set -e).
+diff -r --exclude '*.yaml' --exclude=google "$fresh_dir" "$existing_dir" && exit 0 || true
+
+# There were differences, output a message and exit 1.
+cat << EOF
+
+Differences were identified in protobuf files between what's in /third_party, and freshly downloaded versions.
+
+Often this can be fixed by running:
+  make proto-update-deps
+and committing anything updated.
+
+This check can be run locally using:
+  make proto-update-check
+
+EOF
+
+exit 1
