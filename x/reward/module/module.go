33ed5441ba6aef0263d6860d618aa5ff78a964b5
Daniel Wedul <github@wedul.com>
2022-09-16 13:22:46 -0600

    [1013]: Fix uses of deprecated sdkerrors stuff in the rewards stuff (and stuff).

--- x/reward/module/module.go
+++ x/reward/module/module.go
@@ -11,11 +11,11 @@ import (
 	simulation "github.com/provenance-io/provenance/x/reward/simulation"
 	"github.com/provenance-io/provenance/x/reward/types"
 
+	cerrs "cosmossdk.io/errors"
 	sdkclient "github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
 	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
@@ -68,7 +68,7 @@ func (AppModuleBasic) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
 func (AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, config sdkclient.TxEncodingConfig, bz json.RawMessage) error {
 	var data types.GenesisState
 	if err := cdc.UnmarshalJSON(bz, &data); err != nil {
-		return sdkerrors.Wrapf(err, "failed to unmarshal %q genesis state", types.ModuleName)
+		return cerrs.Wrapf(err, "failed to unmarshal %q genesis state", types.ModuleName)
 	}
 
 	return data.Validate()
