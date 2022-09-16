c678cfbb29d0b78189af2184d689f82994fdd16c
Carlton N Hanna <nullpointer0x00@gmail.com>
2022-09-16 14:56:17 -0600

    add codec

--- x/msgfees/types/codec.go
+++ x/msgfees/types/codec.go
@@ -17,6 +17,7 @@ func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 		&UpdateMsgFeeProposal{},
 		&RemoveMsgFeeProposal{},
 		&UpdateNhashPerUsdMilProposal{},
+		&UpdateConversionFeeDenomProposal{},
 	)
 	registry.RegisterImplementations(
 		(*sdk.Msg)(nil),
