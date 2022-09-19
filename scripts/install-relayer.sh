3f6fa6512e5577e3a376a206f8d61faff6c890d5
Matthew Witkowski <mwitkowski@figure.com>
2022-09-19 09:51:40 -0400

    Added quotes around every variable within the relayer install script.

--- scripts/install-relayer.sh
+++ scripts/install-relayer.sh
@@ -4,36 +4,37 @@ RESET_CONFIG="${RESET_CONFIG:-0}"
 VERSION="${VERSION:-v2.0.0}"
 RELAYER_HOME="${RELAYER_HOME:-$HOME/.relayer}"
 RELAY_CMD="${RELAY_CMD:-rly}"
-RELAYER_CONFIG=${RELAYER_HOME}/config/config.yaml
+RELAYER_CONFIG="$RELAYER_HOME"/config/config.yaml
 PROVENANCE_CONFIG=scripts/relayer-config.yaml
 
 # Install relayer if it doesn't exist
-if ! [ -x "$(command -v ${RELAY_CMD})" ]; then
+RELAYER_EXISTS=$(command -v "$RELAY_CMD")
+if ! [ -x "$RELAYER_EXISTS" ]; then
     echo "relayer not installed. Installing..."
     # Install relayer
     DIR=$(pwd)
     git clone https://github.com/cosmos/relayer.git /tmp/relayer
     cd /tmp/relayer
-    git checkout $VERSION
+    git checkout "$VERSION"
     make install
 
     # Cleanup installer
-    cd $DIR
+    cd "$DIR"
     rm -rf /tmp/relayer
 fi
 
 # Confirm the user wants to delete their previous config
 if test -f "$RELAYER_CONFIG"; then
-    if [[ $RESET_CONFIG != 1 ]]; then
+    if [[ "$RESET_CONFIG" != 1 ]]; then
         exit
     fi
     echo "Removing old config.yaml"
-    rm $RELAYER_CONFIG
+    rm "$RELAYER_CONFIG"
 fi
 
 # Setup configuration
 echo "Creating new provenance relayer config"
-${RELAY_CMD} config init
-cp $PROVENANCE_CONFIG $RELAYER_CONFIG
+"${RELAY_CMD}" config init --home "$RELAYER_HOME"
+cp "$PROVENANCE_CONFIG" "$RELAYER_CONFIG"
 
 echo "Setup complete"
