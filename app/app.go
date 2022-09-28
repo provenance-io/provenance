3db49e144945331edbd6d8406062b2e71805d646
Matthew Witkowski <mwitkowski@figure.com>
2022-09-28 12:24:17 -0400

    Fixed comment that was accidentally added back in from merge.

--- app/app.go
+++ app/app.go
@@ -285,8 +285,6 @@ type App struct {
 	ScopedTransferKeeper capabilitykeeper.ScopedKeeper
 	ScopedICAHostKeeper  capabilitykeeper.ScopedKeeper
 
-	// TODO: Required for v1.13.x: Add keepers for new modules and wire them up. https://github.com/provenance-io/provenance/issues/1007
-
 	// the module manager
 	mm *module.Manager
 
