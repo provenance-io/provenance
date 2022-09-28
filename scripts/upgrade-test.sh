00c8912f05299366178fd2532e2293fa87e79005
Daniel Wedul <github@wedul.com>
2022-09-28 11:28:07 -0600

    Overhaul the script to make all the stuff configurable through env vars. Also, check the submit-proposal command first to see which version of it is needed, and handle that accordingly.

--- scripts/upgrade-test.sh
+++ scripts/upgrade-test.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 # This script will submit a test upgrade proposal, and vote for it.
 # It's assumed that you're running it from the root of this repo.
-# Uses build/provenanced as the executable unless $PROV is set (and exported).
-# Uses build/run/provenanced as PIO_HOME (--home) unless $PIO_HOME is set (and exported).
+# This script looks to the following env vars for configuration:
+#   PIO_HOME:  Default: build/run/provenanced
+#   PIO_CHAIN_ID:  Default: testing
+#   PIO_KEYRING_BACKEND:  Default: test
+#   PROV:  The provenanced executable. Default: build/provenanced
+#   DEPOSIT:  The --deposit amount for the upgrade gov proposal. Default: 10000000nhash
+#   GAS_PRICES:  The --gas-prices amount for the tx commands. Default: 1905nhash
+#   GAS:  The --gas amount for the tx commands. Default: auto
+#   GAS_ADJUSTMENT:  The --gas-adjustment amount for tx commands. Only used for --gas=auto. Default: 1.5
+#   HEIGHT_PLUS:  The number of blocks in the future to do the upgrade (must happen after voting ends). Default: 40
 
 if [[ -z "$1" ]]; then
     printf 'Usage: %s <color>\n' "$0"
@@ -12,40 +20,51 @@ fi
 color="$1"
 shift
 
-prov="${PROV:-build/provenanced}"
 
 set -ex
 export PIO_TESTNET=true
-export PIO_HOME="${PIO_HOME:-build/run/provenanced}"
 export PIO_OUTPUT=json
+export PIO_HOME="${PIO_HOME:-build/run/provenanced}"
+export PIO_CHAIN_ID="${PIO_CHAIN_ID:-testing}"
+export PIO_KEYRING_BACKEND="${PIO_KEYRING_BACKEND:-test}"
+prov="${PROV:-build/provenanced}"
+deposit="${DEPOSIT:-10000000nhash}"
+gasPrices="${GAS_PRICES:-1905nhash}"
+gas="${GAS:-auto}"
+gasAdj="${GAS_ADJUSTMENT:-1.5}"
+gasArgs="--gas-prices=$gasPrices --gas=$gas"
+if [ "$gas" == 'auto' ]; then
+    gasArgs="$gasArgs --gas-adjustment=$gasAdj"
+fi
+heightPlus="${HEIGHT_PLUS:-40}"
+
+prop_cmd='submit-proposal'
+no_val=''
+if "$prov" tx gov "$prop_cmd" software-upgrade --help 2>&1 | grep -qF 'proposal.json' > /dev/null 2>&1; then
+    prop_cmd='submit-legacy-proposal'
+    no_val='--no-validate'
+fi
+
 
 valAddr="$( "$prov" keys list --output json | jq -r '.[] | select( .name == "validator" ) | .address' )"
 curHeight="$( "$prov" status | jq -r '.SyncInfo.latest_block_height' )"
-targetHeight="$(( curHeight + 40 ))"
+targetHeight="$(( curHeight + heightPlus ))"
 
-propRes="$( "$prov" tx gov submit-proposal software-upgrade "$color" \
+propRes="$( "$prov" tx gov "$prop_cmd" software-upgrade "$color" \
     --title "Upgrade for $color" \
     --description "Upgrading provenance to $color" \
     --upgrade-info="$color" \
-    --from "$valAddr" \
     --upgrade-height "$targetHeight" \
-    --deposit 10000000nhash \
-    --chain-id=testing \
-    --keyring-backend test \
-    --gas-prices='1905nhash' \
-    --gas=auto \
-    --gas-adjustment=1.5 \
+    --deposit "$deposit" \
+    --from "$valAddr" \
+    $gasArgs $no_val \
     --yes )"
 
 propId="$( tail -n 1 <<< "$propRes" | jq -r '.logs[0].events[] | select( .type == "submit_proposal" ) | .attributes[] | select( .key == "proposal_id" ) | .value' )"
 
 "$prov" tx gov vote "$propId" yes \
-    --gas-prices='1905nhash' \
-    --gas=auto \
-    --gas-adjustment=1.5 \
     --from "$valAddr" \
-    --keyring-backend=test \
-    --chain-id=testing \
+    $gasArgs \
     --yes
 
 "$prov" query gov tally "$propId"
