e5fb5b9dff7318b578684761e247d677394d6d47
Daniel Wedul <github@wedul.com>
2022-10-07 15:36:11 -0600

    [767]: Update the run-sims-with-all-dbs script to exit with a non-zero code if one or more of the tests did.

--- scripts/run-sims-with-all-dbs.sh
+++ scripts/run-sims-with-all-dbs.sh
@@ -7,26 +7,28 @@ DB_TYPES="${DB_TYPES:-goleveldb cleveldb rocksdb badgerdb}"
 OUTPUT_DIR="${OUTPUT_DIR:-build/sim-times}"
 
 run_sims_with_all_dbs () {
-    local sim db_type time_file
+    local sim db_type time_file rv
     time_file="$OUTPUT_DIR/sim-times.log"
     printf 'Testing sims: %s\n' "${SIMS[*]}"
     printf 'With DB Backends: %s\n' "${DB_TYPES[*]}"
     printf 'Storing timing results in %s\n' "$time_file"
-    [[ -d "$OUTPUT_DIR" ]] || mkdir -p "$OUTPUT_DIR"
+    [[ -d "$OUTPUT_DIR" ]] || mkdir -p "$OUTPUT_DIR" || return $?
     [[ -e "$time_file" ]] && rm "$time_file"
+    rv=0
     for sim in $SIMS; do
         for db_type in $DB_TYPES; do
-            time_sim "$sim" "$db_type" 2> >( grep '[^[:space:]]' | tee -a "$time_file" )
+            time_sim "$sim" "$db_type" 2> >( grep '[^[:space:]]' | tee -a "$time_file" ) || rv=$?
         done
     done
     sleep 1
     printf 'Results stored in %s\n' "$OUTPUT_DIR"
+    return $rv
 }
 
 # Usage: time_sim <sim> <db_type>
 # This will output timing info to stderr, and everything else to stdout.
 time_sim () {
-    local sim db_type log
+    local sim db_type log rv
     sim="$1"
     db_type="$2"
     [[ "$sim" =~ ^test-sim- ]] || sim="test-sim-$sim"
@@ -39,7 +41,9 @@ time_sim () {
     # Redirect both stout and stderr to both the log file and stderr.
     # The time output does not get redirected by either the tee or 2>&1 and goes straight to stderr.
     time DB_BACKEND="$db_type" make "$sim" > >( tee "$log") 2>&1
+    rv=$?
     printf 'Done [%d]: %s' "$?" "$log" >&2
+    return $rv
 }
 
 CURDIR="$( cd "$( dirname "${BASH_SOURCE:-$0}" )"; pwd -P )"
@@ -51,5 +55,10 @@ cd "$CURDIR"
 cd ..
 printf 'Running make commands from directory %s\n\n' "$( pwd )"
 run_sims_with_all_dbs $@
+RV=$?
 
-exit $?
+if [[ "$RV" -ne '0' ]]; then
+    printf 'One or more tests failed.\n'
+fi
+
+exit $RV
