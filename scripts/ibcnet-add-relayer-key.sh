d22a6de6fb68e8e26f8f7c1708a9bbff53fd6e71
Matthew Witkowski <mwitkowski@figure.com>
2022-11-28 16:51:32 -0500

    Updated scripts to check if genesis file was already updated. Also added check for jq.

--- scripts/ibcnet-add-relayer-key.sh
+++ scripts/ibcnet-add-relayer-key.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# Verify that jq exists
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found."
+    exit 1
+fi
+
 # We probably want to verify that the address doesn't exist
-echo $(jq . build/ibc0-0/config/genesis.json | jq '.app_state.auth.accounts += [{"@type": "/cosmos.auth.v1beta1.BaseAccount", "address": "tp18uev5722xrwpfd2hnqducmt3qdjsyktmtw558y", "pub_key": null, "account_number": "1", "sequence": "0"}]' | jq '.app_state.bank.balances += [{"address": "tp18uev5722xrwpfd2hnqducmt3qdjsyktmtw558y", "coins": [{"denom": "nhash","amount": "100000000000000000"}]}]' | jq '.app_state.bank.balances[0].coins[0].amount = "99900000000000000000"') > build/ibc0-0/config/genesis.json
-echo $(jq . build/ibc1-0/config/genesis.json | jq '.app_state.auth.accounts += [{"@type": "/cosmos.auth.v1beta1.BaseAccount", "address": "tp18uev5722xrwpfd2hnqducmt3qdjsyktmtw558y", "pub_key": null, "account_number": "1", "sequence": "0"}]' | jq '.app_state.bank.balances += [{"address": "tp18uev5722xrwpfd2hnqducmt3qdjsyktmtw558y", "coins": [{"denom": "nhash","amount": "100000000000000000"}]}]' | jq '.app_state.bank.balances[0].coins[0].amount = "99900000000000000000"') > build/ibc1-0/config/genesis.json
+ACCOUNTS=$(jq . build/ibc0-0/config/genesis.json | jq '.app_state.auth.accounts' | jq length)
+if [ "$ACCOUNTS" -eq "1" ]; then
+    echo "Updating genesis for ibc0."
+    echo $(jq . build/ibc0-0/config/genesis.json | jq '.app_state.auth.accounts += [{"@type": "/cosmos.auth.v1beta1.BaseAccount", "address": "tp18uev5722xrwpfd2hnqducmt3qdjsyktmtw558y", "pub_key": null, "account_number": "1", "sequence": "0"}]' | jq '.app_state.bank.balances += [{"address": "tp18uev5722xrwpfd2hnqducmt3qdjsyktmtw558y", "coins": [{"denom": "nhash","amount": "100000000000000000"}]}]' | jq '.app_state.bank.balances[0].coins[0].amount = "99900000000000000000"') > build/ibc0-0/config/genesis.json
+else
+    echo "Genesis file is already updated for ibc0. Skipping..."
+fi
+
+ACCOUNTS=$(jq . build/ibc1-0/config/genesis.json | jq '.app_state.auth.accounts' | jq length)
+if [ "$ACCOUNTS" -eq "1" ]; then
+    echo "Updating genesis for ibc1."
+    echo $(jq . build/ibc1-0/config/genesis.json | jq '.app_state.auth.accounts += [{"@type": "/cosmos.auth.v1beta1.BaseAccount", "address": "tp18uev5722xrwpfd2hnqducmt3qdjsyktmtw558y", "pub_key": null, "account_number": "1", "sequence": "0"}]' | jq '.app_state.bank.balances += [{"address": "tp18uev5722xrwpfd2hnqducmt3qdjsyktmtw558y", "coins": [{"denom": "nhash","amount": "100000000000000000"}]}]' | jq '.app_state.bank.balances[0].coins[0].amount = "99900000000000000000"') > build/ibc1-0/config/genesis.json
+else
+    echo "Genesis file is already updated for ibc1. Skipping..."
+fi
