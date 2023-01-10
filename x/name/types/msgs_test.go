a570de3d4bb97eb90f0491c75695d090b4c5bf84
llama <109820353+llama-del-rey@users.noreply.github.com>
2023-01-11 02:29:03 +0500

    improve test coverage

--- x/name/types/msgs_test.go
+++ x/name/types/msgs_test.go
@@ -23,10 +23,37 @@ func TestMsgCreateRootNameRequestGetSignBytes(t *testing.T) {
 
 func TestMsgCreateRootNameRequestGetSigners(t *testing.T) {
 	authority := sdk.AccAddress("input111111111111111")
-	name := "Proposal Title"
-	owner := "Proposal description"
+	name := "human-readable-name"
+	owner := "owner"
 	msg := NewMsgCreateRootNameRequest(authority.String(), name, owner, false)
 	res := msg.GetSigners()
 	require.Equal(t, 1, len(res))
 	require.True(t, authority.Equals(res[0]))
+}
+
+func TestMsgCreateRootNameRequestInvalidOwnerAddress(t *testing.T) {
+	authority := sdk.AccAddress("input111111111111111")
+	name := "human-readable-name"
+	owner := "..."
+	msg := NewMsgCreateRootNameRequest(authority.String(), name, owner, false)
+	res := msg.ValidateBasic()
+	require.EqualError(t, res, "invalid account address")
+}
+
+func TestMsgCreateRootNameRequestInvalidNameLength(t *testing.T) {
+	authority := sdk.AccAddress("input111111111111111")
+	name := ""
+	owner := "cosmos1ghekyjucln7y67ntx7cf27m9dpuxxemn4c8g4r"
+	msg := NewMsgCreateRootNameRequest(authority.String(), name, owner, false)
+	res := msg.ValidateBasic()
+	require.EqualError(t, res, "proto: negative length found during unmarshaling")
+}
+
+func TestMsgCreateRootNameRequestNameContainsSegments(t *testing.T) {
+	authority := sdk.AccAddress("input111111111111111")
+	name := "..."
+	owner := "cosmos1ghekyjucln7y67ntx7cf27m9dpuxxemn4c8g4r"
+	msg := NewMsgCreateRootNameRequest(authority.String(), name, owner, false)
+	res := msg.ValidateBasic()
+	require.EqualError(t, res, "invalid name: \".\" is reserved")
 }
