df2886e28f59479b472d1b05eac5bd1c8622e4d2
Daniel Wedul <github@wedul.com>
2023-02-01 16:28:56 -0700

    Add a unit test for TestIncreaseMaxCommissions.

--- app/upgrades_test.go
+++ app/upgrades_test.go
@@ -1,31 +1,48 @@
 package app
 
 import (
-	"github.com/stretchr/testify/suite"
+	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-
-	icatypes "github.com/cosmos/ibc-go/v6/modules/apps/27-interchain-accounts/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 )
 
-type IntegrationTestSuite struct {
-	suite.Suite
+func TestIncreaseMaxCommissions(t *testing.T) {
+	iniBal := sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(100000000000000)))
+	validatorAccs := make([]authtypes.GenesisAccount, 5)
+	balances := make([]banktypes.Balance, len(validatorAccs))
+	for i := range validatorAccs {
+		privKey := secp256k1.GenPrivKey()
+		validatorAccs[i] = authtypes.NewBaseAccount(privKey.PubKey().Address().Bytes(), privKey.PubKey(), uint64(i), 0)
+		balances[i] = banktypes.Balance{
+			Address: validatorAccs[i].GetAddress().String(),
+			Coins:   iniBal,
+		}
+	}
 
-	app *App
-	ctx sdk.Context
-}
+	app := SetupWithGenesisAccounts(t, "", validatorAccs, balances...)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
 
-func (s *IntegrationTestSuite) SetupSuite() {
-	s.app = Setup(s.T())
-	s.ctx = s.app.BaseApp.NewContext(false, tmproto.Header{})
-}
+	// make sure at least one doesn't have the new value so that there's something that's actually being tested.
+	expectedMaxRate := sdk.OneDec()
+	canTest := false
+	for _, validator := range app.StakingKeeper.GetAllValidators(ctx) {
+		if !expectedMaxRate.Equal(validator.Commission.MaxRate) {
+			canTest = true
+			break
+		}
+	}
+	require.True(t, canTest, "all validators already have Commission.MaxRate = %s", expectedMaxRate.String())
 
-func (s *IntegrationTestSuite) TestUpgradeICA() {
-	s.SetupSuite()
-	versionMap := s.app.UpgradeKeeper.GetModuleVersionMap(s.ctx)
-	UpgradeICA(s.ctx, s.app, &versionMap)
-	s.Assert().Equal(s.app.mm.Modules[icatypes.ModuleName].ConsensusVersion(), versionMap[icatypes.ModuleName], "consensus version should be set to skip init genesis")
-	s.Assert().Equal([]string{"*"}, s.app.ICAHostKeeper.GetAllowMessages(s.ctx), "ica host should accept all messages")
-	s.Assert().True(s.app.ICAHostKeeper.IsHostEnabled(s.ctx), "ica host should be enabled")
+	IncreaseMaxCommissions(ctx, app)
+
+	for i, validator := range app.StakingKeeper.GetAllValidators(ctx) {
+		assert.Equal(t, expectedMaxRate, validator.Commission.MaxRate, "validator[%d].Commission.MaxRate", i)
+	}
 }
