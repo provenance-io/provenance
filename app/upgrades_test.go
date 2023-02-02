10992fe1bb9f9946a06e3b7de528bbec094f1740
Daniel Wedul <github@wedul.com>
2023-02-01 17:44:30 -0700

    Overhaul the new unit test to try to get it to not weirdly fail during init genesis.

--- app/upgrades_test.go
+++ app/upgrades_test.go
@@ -1,59 +1,46 @@
-package app
+package app_test
 
 import (
+	"github.com/provenance-io/provenance/app"
+	"github.com/stretchr/testify/suite"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-
-	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
-	"github.com/cosmos/cosmos-sdk/testutil/mock"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
-	tmtypes "github.com/tendermint/tendermint/types"
 )
 
-func TestIncreaseMaxCommissions(t *testing.T) {
-	validators := make([]*tmtypes.Validator, 5)
-	for i := range validators {
-		key := mock.NewPV()
-		pubKey, err := key.GetPubKey()
-		require.NoError(t, err, "[%d] GetPubKey", i)
-		validators[i] = tmtypes.NewValidator(pubKey, 1)
-	}
-	valSet := tmtypes.NewValidatorSet(validators)
+type UpgradeTestSuite struct {
+	suite.Suite
 
-	iniBal := sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(100000000000000)))
-	accs := make([]authtypes.GenesisAccount, 1)
-	balances := make([]banktypes.Balance, len(accs))
-	for i := range accs {
-		key := secp256k1.GenPrivKey()
-		accs[i] = authtypes.NewBaseAccount(key.PubKey().Address().Bytes(), key.PubKey(), uint64(i), 0)
-		balances[i] = banktypes.Balance{
-			Address: accs[i].GetAddress().String(),
-			Coins:   iniBal,
-		}
-	}
+	app *app.App
+	ctx sdk.Context
+}
 
-	app := SetupWithGenesisValSet(t, "", valSet, accs, balances...)
-	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+func TestDumbTestSuite(t *testing.T) {
+	suite.Run(t, new(UpgradeTestSuite))
+}
+
+func (s *UpgradeTestSuite) SetupTest() {
+	s.app = app.Setup(s.T())
+	s.ctx = s.app.BaseApp.NewContext(false, tmproto.Header{})
+}
+
+func (s *UpgradeTestSuite) TestIncreaseMaxCommissions() {
 	// make sure at least one doesn't have the new value so that there's something that's actually being tested.
 	expectedMaxRate := sdk.OneDec()
 	canTest := false
-	for i, validator := range app.StakingKeeper.GetAllValidators(ctx) {
-		t.Logf("Before: validator[%d] has Commission.MaxRate = %s", i, validator.Commission.MaxRate.String())
+	for i, validator := range s.app.StakingKeeper.GetAllValidators(s.ctx) {
+		s.T().Logf("Before: validator[%d] has Commission.MaxRate = %s", i, validator.Commission.MaxRate.String())
 		if !expectedMaxRate.Equal(validator.Commission.MaxRate) {
 			canTest = true
 		}
 	}
-	require.True(t, canTest, "all validators already have Commission.MaxRate = %s", expectedMaxRate.String())
+	s.Require().True(canTest, "all validators already have Commission.MaxRate = %s", expectedMaxRate.String())
 
-	IncreaseMaxCommissions(ctx, app)
+	app.IncreaseMaxCommissions(s.ctx, s.app)
 
-	for i, validator := range app.StakingKeeper.GetAllValidators(ctx) {
-		t.Logf("After: validator[%d] has Commission.MaxRate = %s", i, validator.Commission.MaxRate.String())
-		assert.Equal(t, expectedMaxRate, validator.Commission.MaxRate, "validator[%d].Commission.MaxRate", i)
+	for i, validator := range s.app.StakingKeeper.GetAllValidators(s.ctx) {
+		s.T().Logf("After: validator[%d] has Commission.MaxRate = %s", i, validator.Commission.MaxRate.String())
+		s.Assert().Equal(expectedMaxRate, validator.Commission.MaxRate, "validator[%d].Commission.MaxRate", i)
 	}
 }
