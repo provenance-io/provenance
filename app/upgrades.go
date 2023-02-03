1b09c8e348c0ef1b57e751ca18885cee370c7191
Daniel Wedul <github@wedul.com>
2023-02-03 16:50:29 -0700

    Remove duplicated piece that I forgot to replace earlier.

--- app/upgrades.go
+++ app/upgrades.go
@@ -104,9 +104,6 @@ var handlers = map[string]appUpgrade{
 			ctx.Logger().Info("Starting migrations. This may take a significant amount of time to complete. Do not restart node.")
 			IncreaseMaxCommissions(ctx, app)
 			IncreaseMaxGas(ctx, app)
-			if err := SetSanctionParams(ctx, app); err != nil {
-				return nil, err
-			}
 			versionMap, err := app.mm.RunMigrations(ctx, app.configurator, app.UpgradeKeeper.GetModuleVersionMap(ctx))
 			if err != nil {
 				return nil, err
