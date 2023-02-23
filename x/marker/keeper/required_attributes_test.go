e67c2c4501a0c558336f6baa74c4f371203febd0
Carlton N Hanna <nullpointer0x00@gmail.com>
2023-02-23 09:15:06 -0700

    add test for contains required attributes that retrieves attrs from attributes module

--- x/marker/keeper/required_attributes_test.go
+++ x/marker/keeper/required_attributes_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
@@ -63,6 +65,110 @@ func TestNormalizeRequiredAttributes(t *testing.T) {
 		} else {
 			require.NoError(t, err)
 			require.Equal(t, tc.expectedNormalized, result)
+		}
+	}
+}
+
+func TestContainsRequiredAttributes(t *testing.T) {
+	app := simapp.Setup(t)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+	owner, _ := sdk.AccAddressFromBech32("cosmos1v57fx2l2rt6ehujuu99u2fw05779m5e2ux4z2h")
+	acct := app.AccountKeeper.NewAccountWithAddress(ctx, owner)
+	app.AccountKeeper.SetAccount(ctx, acct)
+	app.NameKeeper.SetNameRecord(ctx, "kyc.provenance.io", owner, false)
+	app.NameKeeper.SetNameRecord(ctx, "not-kyc.provenance.io", owner, false)
+
+	app.AttributeKeeper.SetAttribute(ctx,
+		attrTypes.Attribute{
+			Name:          "kyc.provenance.io",
+			Value:         []byte("string value"),
+			Address:       "cosmos1v57fx2l2rt6ehujuu99u2fw05779m5e2ux4z2h",
+			AttributeType: attrTypes.AttributeType_String,
+		},
+		owner,
+	)
+	app.AttributeKeeper.SetAttribute(ctx,
+		attrTypes.Attribute{
+			Name:          "not-kyc.provenance.io",
+			Value:         []byte("string value"),
+			Address:       "cosmos1v57fx2l2rt6ehujuu99u2fw05779m5e2ux4z2h",
+			AttributeType: attrTypes.AttributeType_String,
+		},
+		owner,
+	)
+	testCases := []struct {
+		name               string
+		requiredAttributes []string
+		address            string
+		expectedResult     bool
+		expectedError      string
+	}{
+		{
+			name:               "should succeed - empty required attrs",
+			requiredAttributes: []string{},
+			address:            "cosmos1v57fx2l2rt6ehujuu99u2fw05779m5e2ux4z2h",
+			expectedResult:     true,
+			expectedError:      "",
+		},
+		{
+			name:               "should succeed - wildcard match",
+			requiredAttributes: []string{"*.io"},
+			address:            "cosmos1v57fx2l2rt6ehujuu99u2fw05779m5e2ux4z2h",
+			expectedResult:     true,
+			expectedError:      "",
+		},
+		{
+			name:               "should succeed - wildcard match 2",
+			requiredAttributes: []string{"*.provenance.io"},
+			address:            "cosmos1v57fx2l2rt6ehujuu99u2fw05779m5e2ux4z2h",
+			expectedResult:     true,
+			expectedError:      "",
+		},
+		{
+			name:               "should succeed - exact match",
+			requiredAttributes: []string{"kyc.provenance.io"},
+			address:            "cosmos1v57fx2l2rt6ehujuu99u2fw05779m5e2ux4z2h",
+			expectedResult:     true,
+			expectedError:      "",
+		},
+		{
+			name:               "should succeed - exact match multiple",
+			requiredAttributes: []string{"kyc.provenance.io", "kyc.provenance.io"},
+			address:            "cosmos1v57fx2l2rt6ehujuu99u2fw05779m5e2ux4z2h",
+			expectedResult:     true,
+			expectedError:      "",
+		},
+		{
+			name:               "should fail - no match for notfound.provenance.io",
+			requiredAttributes: []string{"notfound.provenance.io", "kyc.provenance.io"},
+			address:            "cosmos1v57fx2l2rt6ehujuu99u2fw05779m5e2ux4z2h",
+			expectedResult:     false,
+			expectedError:      "",
+		},
+		{
+			name:               "should succeed - account has no attributes and required attributes empty",
+			requiredAttributes: []string{},
+			address:            "cosmos1sh49f6ze3vn7cdl2amh2gnc70z5mten3y08xck",
+			expectedResult:     true,
+			expectedError:      "",
+		},
+		{
+			name:               "should fail - account has no attributes and required attributes populated",
+			requiredAttributes: []string{"kyc.provenance.io"},
+			address:            "cosmos1sh49f6ze3vn7cdl2amh2gnc70z5mten3y08xck",
+			expectedResult:     false,
+			expectedError:      "",
+		},
+	}
+	for _, tc := range testCases {
+		result, err := app.MarkerKeeper.ContainsRequiredAttributes(ctx, tc.requiredAttributes, tc.address)
+		if len(tc.expectedError) > 0 {
+			assert.NotNil(t, err, tc.name)
+			assert.EqualError(t, err, tc.expectedError, tc.name)
+
+		} else {
+			assert.NoError(t, err, tc.name)
+			assert.Equal(t, tc.expectedResult, result, tc.name)
 		}
 	}
 }
