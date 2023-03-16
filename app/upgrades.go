43a70854dbb33ddee3093efd6c1c8eaa60e84338
Carlton N Hanna <nullpointer0x00@gmail.com>
2023-03-16 14:51:31 -0600

    add back paua handler

--- app/upgrades.go
+++ app/upgrades.go
@@ -7,6 +7,8 @@ import (
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/cosmos-sdk/x/quarantine"
+	"github.com/cosmos/cosmos-sdk/x/sanction"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
@@ -37,6 +39,24 @@ var handlers = map[string]appUpgrade{
 			return app.mm.RunMigrations(ctx, app.configurator, versionMap)
 		},
 	},
+	"paua": { // upgrade for v1.14.0
+		Added: []string{quarantine.ModuleName, sanction.ModuleName},
+		Handler: func(ctx sdk.Context, app *App, plan upgradetypes.Plan) (module.VersionMap, error) {
+			ctx.Logger().Info("Starting migrations. This may take a significant amount of time to complete. Do not restart node.")
+			versionMap, err := app.mm.RunMigrations(ctx, app.configurator, app.UpgradeKeeper.GetModuleVersionMap(ctx))
+			if err != nil {
+				return nil, err
+			}
+			err = SetSanctionParams(ctx, app) // Needs to happen after RunMigrations adds the sanction module.
+			if err != nil {
+				return nil, err
+			}
+			IncreaseMaxCommissions(ctx, app)
+			// Skipping IncreaseMaxGas(ctx, app) in mainnet for now.
+			return versionMap, nil
+		},
+	},
+
 	// TODO - Add new upgrade definitions here.
 }
 
@@ -93,6 +113,36 @@ func GetUpgradeStoreLoader(app *App, info upgradetypes.Plan) baseapp.StoreLoader
 		"upgrade.renamed", storeUpgrades.Renamed,
 	)
 	return upgradetypes.UpgradeStoreLoader(info.Height, &storeUpgrades)
+}
+
+func IncreaseMaxCommissions(ctx sdk.Context, app *App) {
+	oneHundredPct := sdk.OneDec()
+	fivePct := sdk.MustNewDecFromStr("0.05")
+	validators := app.StakingKeeper.GetAllValidators(ctx)
+	ctx.Logger().Info("Increasing all validator's max commission to 100% and max change rate to 5%", "count", len(validators))
+	for _, validator := range validators {
+		validator.Commission.MaxRate = oneHundredPct
+		// Note: This MaxChangeRate bump was added after paua-rc1 was run on testnet.
+		// So, even though it's called by the paua-rc1 upgrade handler now,
+		// it wasn't part of the actual paua-rc1 upgrade that was performed on testnet.
+		if validator.Commission.MaxChangeRate.LT(fivePct) {
+			validator.Commission.MaxChangeRate = fivePct
+		}
+		app.StakingKeeper.SetValidator(ctx, validator)
+	}
+}
+
+func SetSanctionParams(ctx sdk.Context, app *App) error {
+	ctx.Logger().Info("Setting sanction params")
+	params := &sanction.Params{
+		ImmediateSanctionMinDeposit:   sdk.NewCoins(sdk.NewInt64Coin("nhash", 1_000_000_000_000_000)),
+		ImmediateUnsanctionMinDeposit: sdk.NewCoins(sdk.NewInt64Coin("nhash", 1_000_000_000_000_000)),
+	}
+	err := app.SanctionKeeper.SetParams(ctx, params)
+	if err != nil {
+		return fmt.Errorf("could not set sanction params: %w", err)
+	}
+	return nil
 }
 
 // RemoveIsSendEnabledEntries removes all entries in the bankkeepers send enabled table
