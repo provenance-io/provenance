0663f5d470e0443531a4cb129d4b0e54d62d3498
Carlton Hanna <nullpointer0x00@gmail.com>
2023-03-31 08:16:21 -0600

    Add support for account addresses by attribute name lookup (#1448)
    * add lookup table for addresses by attribute name
    
    * add proto for queries
    
    * fix keys, add get accounts by name method, update tests
    
    * make asserts unified
    
    * add change log
    
    * add pagination to query, implement query
    
    * modify key address parsing, finish query server query, add tests
    
    * fix non distict delete, update tests
    
    * remove union distinct and do a dup check with map structure
    
    * refactor lookup key creation to use a counter instead of hash
    
    * remove unneeded check

--- x/attribute/keeper/query_server.go
+++ x/attribute/keeper/query_server.go
@@ -123,3 +123,35 @@ func (k Keeper) Scan(c context.Context, req *types.QueryScanRequest) (*types.Que
 
 	return &types.QueryScanResponse{Account: req.Account, Attributes: attributes, Pagination: pageRes}, nil
 }
+
+// AttributeAccounts queries for all accounts that have a specific attribute
+func (k Keeper) AttributeAccounts(c context.Context, req *types.QueryAttributeAccountsRequest) (*types.QueryAttributeAccountsResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+	ctx := sdk.UnwrapSDKContext(c)
+	accounts := make([]string, 0)
+	store := ctx.KVStore(k.storeKey)
+	keyPrefix := types.AttributeNameKeyPrefix(req.AttributeName)
+	attributeStore := prefix.NewStore(store, keyPrefix)
+
+	pageRes, err := query.FilteredPaginate(attributeStore, req.Pagination, func(key []byte, value []byte, accumulate bool) (bool, error) {
+		addressLength := int32(key[0])
+		address := sdk.AccAddress(key[1 : addressLength+1])
+		for _, account := range accounts {
+			if account == address.String() {
+				return false, nil
+			}
+		}
+		if accumulate {
+			accounts = append(accounts, address.String())
+		}
+		return true, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.QueryAttributeAccountsResponse{Accounts: accounts, Pagination: pageRes}, nil
+}
