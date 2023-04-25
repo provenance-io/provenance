44f013ccffb7352894f22e84474e505863e9a733
Carlton N Hanna <nullpointer0x00@gmail.com>
2023-04-25 16:12:54 -0600

    revert changes that were not suppose to go in

--- networks/ibc/blockchain-relayer/entrypoint.sh
+++ networks/ibc/blockchain-relayer/entrypoint.sh
@@ -8,7 +8,7 @@ export RELAYER_HOME="/relayer"
 export RELAY_PATH="local_local2"
 
 # Setup the connection, client, and channel
-#rly tx link "$RELAY_PATH" --home "$RELAYER_HOME"
+rly tx link "$RELAY_PATH" --home "$RELAYER_HOME"
 
 # Start the relayer
-#rly start "$RELAY_PATH" -p events --home "$RELAYER_HOME"
+rly start "$RELAY_PATH" -p events --home "$RELAYER_HOME"
