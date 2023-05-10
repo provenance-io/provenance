84e0ba09e45aff6dbd3fa4204d5a73bc9d491f31
Matthew Witkowski <mwitkowski@figure.com>
2023-05-10 16:07:11 -0400

    Removed test code.

--- x/trigger/keeper/msg_server.go
+++ x/trigger/keeper/msg_server.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdktx "github.com/cosmos/cosmos-sdk/types/tx"
 
 	"github.com/provenance-io/provenance/x/trigger/types"
 )
@@ -25,18 +24,6 @@ var _ types.MsgServer = msgServer{}
 // CreateTrigger creates new trigger from msg
 func (s msgServer) CreateTrigger(goCtx context.Context, msg *types.MsgCreateTriggerRequest) (*types.MsgCreateTriggerResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
-
-	cachedValue := msg.Event.GetCachedValue()
-	if cachedValue == nil {
-		return nil, fmt.Errorf("cached value does not exist")
-	}
-	event := cachedValue.(types.TriggerEventI)
-	fmt.Printf("Prefix Name is: %s\n", event.GetEventPrefix())
-
-	msgs, err := sdktx.GetMsgs(msg.Actions, "RunAction - sdk.MsgCreateTriggerRequest")
-	if len(msgs) == 0 || err != nil {
-		return nil, err
-	}
 
 	trigger, err := s.NewTriggerWithID(ctx, msg.GetAuthority(), msg.GetEvent(), msg.GetActions())
 	if err != nil {
