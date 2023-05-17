5b32af9c0fc90c79dc9e428d81f9ae74d29a05ba
Daniel Wedul <github@wedul.com>
2023-05-17 13:42:26 -0600

    Delete the MsgP8eMemorializeContractRequest message fee and create the gov v1 MsgSubmitProposal fee in the rust upgrades.

--- app/upgrades.go
+++ app/upgrades.go
@@ -1,13 +1,18 @@
 package app
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	govtypesv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
+	govtypesv1beta1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
+	msgfeetypes "github.com/provenance-io/provenance/x/msgfees/types"
 )
 
 var (
@@ -47,8 +52,44 @@ var handlers = map[string]appUpgrade{
 			return app.mm.RunMigrations(ctx, app.configurator, versionMap)
 		},
 	},
-	"rust-rc1": {}, // upgrade for v1.16.0-rc1,
-	"rust":     {}, // upgrade for v1.16.0,
+	"rust-rc1": { // upgrade for v1.16.0-rc1,
+		Handler: func(ctx sdk.Context, app *App, plan upgradetypes.Plan) (module.VersionMap, error) {
+			versionMap, err := runModuleMigrations(ctx, app)
+			if err != nil {
+				return nil, err
+			}
+
+			// We only need to call AddGovV1SubmitFee on testnet.
+			err = AddGovV1SubmitFee(ctx, app)
+			if err != nil {
+				return nil, err
+			}
+
+			err = RemoveP8eMemorializeContractFee(ctx, app)
+			if err != nil {
+				return nil, err
+			}
+
+			return versionMap, nil
+		},
+	},
+	"rust": { // upgrade for v1.16.0,
+		Handler: func(ctx sdk.Context, app *App, plan upgradetypes.Plan) (module.VersionMap, error) {
+			versionMap, err := runModuleMigrations(ctx, app)
+			if err != nil {
+				return nil, err
+			}
+
+			// No need to call AddGovV1SubmitFee in here as mainnet already has it defined.
+
+			err = RemoveP8eMemorializeContractFee(ctx, app)
+			if err != nil {
+				return nil, err
+			}
+
+			return versionMap, nil
+		},
+	},
 	// TODO - Add new upgrade definitions here.
 }
 
@@ -129,3 +170,80 @@ func runModuleMigrations(ctx sdk.Context, app *App) (module.VersionMap, error) {
 // Create a use of runModuleMigrations so that the linter neither complains about it not being used,
 // nor complains about a nolint:unused directive that isn't needed because the function is used.
 var _ = runModuleMigrations
+
+// AddGovV1SubmitFee adds a msg-fee for the gov v1 MsgSubmitProposal if there isn't one yet.
+func AddGovV1SubmitFee(ctx sdk.Context, app *App) (err error) {
+	typeURL := sdk.MsgTypeURL(&govtypesv1.MsgSubmitProposal{})
+	defer func() {
+		if err != nil {
+			ctx.Logger().Error(fmt.Sprintf("Error encountered while setting message fee for %q", typeURL), "error", err)
+		}
+	}()
+
+	ctx.Logger().Info(fmt.Sprintf("Creating message fee for %q if it doesn't already exist.", typeURL))
+	fee, err := app.MsgFeesKeeper.GetMsgFee(ctx, typeURL)
+	if err != nil {
+		return fmt.Errorf("error getting existing v1 fee for %q: %w", typeURL, err)
+	}
+	// If there's already a fee for it, do nothing.
+	if fee != nil {
+		ctx.Logger().Info(fmt.Sprintf("Message fee for %q already exists with amount %q. Nothing more to do.", fee.MsgTypeUrl, fee.AdditionalFee.String()))
+		return nil
+	}
+
+	// Copy the fee from the beta entry if it exists, otherwise, just make it fresh.
+	betaTypeURL := sdk.MsgTypeURL(&govtypesv1beta1.MsgSubmitProposal{})
+	betaFee, err := app.MsgFeesKeeper.GetMsgFee(ctx, betaTypeURL)
+	if err != nil {
+		return fmt.Errorf("error getting existing v1beta1 message fee for %q: %w", betaTypeURL, err)
+	}
+	if betaFee != nil {
+		fee = betaFee
+		fee.MsgTypeUrl = typeURL
+		ctx.Logger().Info(fmt.Sprintf("Copying %q fee to %q.", betaTypeURL, fee.MsgTypeUrl))
+	} else {
+		fee = &msgfeetypes.MsgFee{
+			MsgTypeUrl:           typeURL,
+			AdditionalFee:        sdk.NewInt64Coin("nhash", 100_000_000_000), // 100 hash
+			Recipient:            "",
+			RecipientBasisPoints: 0,
+		}
+		ctx.Logger().Info(fmt.Sprintf("Creating %q fee.", fee.MsgTypeUrl))
+	}
+
+	err = app.MsgFeesKeeper.SetMsgFee(ctx, *fee)
+	if err != nil {
+		return fmt.Errorf("error setting message fee for %q: %w", fee.MsgTypeUrl, err)
+	}
+	ctx.Logger().Info("Successfully set fee for %q with amount %q.", fee.MsgTypeUrl, fee.AdditionalFee.String())
+
+	return nil
+}
+
+// RemoveP8eMemorializeContractFee removes the message fee for the now-non-existent MsgP8eMemorializeContractRequest.
+func RemoveP8eMemorializeContractFee(ctx sdk.Context, app *App) (err error) {
+	typeURL := "/provenance.metadata.v1.MsgP8eMemorializeContractRequest"
+	defer func() {
+		if err != nil {
+			ctx.Logger().Error(fmt.Sprintf("Error encountered while removing message fee for %q", typeURL), "error", err)
+		}
+	}()
+
+	ctx.Logger().Info(fmt.Sprintf("Removing message fee for %q if one exists.", typeURL))
+	// Get the existing fee for log output, but ignore any errors so we try to delete the entry either way.
+	fee, _ := app.MsgFeesKeeper.GetMsgFee(ctx, typeURL)
+	err = app.MsgFeesKeeper.RemoveMsgFee(ctx, typeURL)
+	switch {
+	case errors.Is(err, msgfeetypes.ErrMsgFeeDoesNotExist):
+		ctx.Logger().Info(fmt.Sprintf("Message fee for %q already does not exist. Nothing more to do.", typeURL))
+		err = nil
+	case err != nil:
+		return fmt.Errorf("error removing message fee for %q: %w", typeURL, err)
+	case fee != nil:
+		ctx.Logger().Info(fmt.Sprintf("Successfully removed message fee for %q with amount %q.", fee.MsgTypeUrl, fee.AdditionalFee.String()))
+	default:
+		ctx.Logger().Info(fmt.Sprintf("Successfully removed message fee for %q.", typeURL))
+	}
+
+	return nil
+}
