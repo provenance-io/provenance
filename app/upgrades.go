84b9b8cabcfe431021e41ef5ad22a236d139d415
Daniel Wedul <github@wedul.com>
2023-05-18 18:14:42 -0600

    Put app/upgrades.go back to what it was since I'm doing the upgrade work in a different PR.

--- app/upgrades.go
+++ app/upgrades.go
@@ -7,6 +7,8 @@ import (
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/cosmos-sdk/x/quarantine"
+	"github.com/cosmos/cosmos-sdk/x/sanction"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
@@ -28,6 +30,31 @@ type appUpgrade struct {
 }
 
 var handlers = map[string]appUpgrade{
+	"paua": { // upgrade for v1.14.0
+		Added: []string{quarantine.ModuleName, sanction.ModuleName},
+		Handler: func(ctx sdk.Context, app *App, plan upgradetypes.Plan) (module.VersionMap, error) {
+			ctx.Logger().Info("Starting migrations. This may take a significant amount of time to complete. Do not restart node.")
+			versionMap, err := app.mm.RunMigrations(ctx, app.configurator, app.UpgradeKeeper.GetModuleVersionMap(ctx))
+			if err != nil {
+				return nil, err
+			}
+			err = SetSanctionParams(ctx, app) // Needs to happen after RunMigrations adds the sanction module.
+			if err != nil {
+				return nil, err
+			}
+			IncreaseMaxCommissions(ctx, app)
+			// Skipping IncreaseMaxGas(ctx, app) in mainnet for now.
+			return versionMap, nil
+		},
+	},
+	"paua-rc2": { // upgrade for v1.14.0-rc3
+		Handler: func(ctx sdk.Context, app *App, plan upgradetypes.Plan) (module.VersionMap, error) {
+			// Reapply the max commissions thing again so testnet gets the max change rate bump too.
+			IncreaseMaxCommissions(ctx, app)
+			UndoMaxGasIncrease(ctx, app)
+			return app.UpgradeKeeper.GetModuleVersionMap(ctx), nil
+		},
+	},
 	"quicksilver-rc1": { // upgrade for v1.15.0-rc2
 		Handler: func(ctx sdk.Context, app *App, plan upgradetypes.Plan) (module.VersionMap, error) {
 			app.MarkerKeeper.RemoveIsSendEnabledEntries(ctx)
@@ -47,8 +74,6 @@ var handlers = map[string]appUpgrade{
 			return app.mm.RunMigrations(ctx, app.configurator, versionMap)
 		},
 	},
-	"rust-rc1": {}, // upgrade for v1.16.0-rc1
-	"rust":     {}, // upgrade for v1.16.0
 	// TODO - Add new upgrade definitions here.
 }
 
@@ -105,4 +130,41 @@ func GetUpgradeStoreLoader(app *App, info upgradetypes.Plan) baseapp.StoreLoader
 		"upgrade.renamed", storeUpgrades.Renamed,
 	)
 	return upgradetypes.UpgradeStoreLoader(info.Height, &storeUpgrades)
+}
+
+func IncreaseMaxCommissions(ctx sdk.Context, app *App) {
+	oneHundredPct := sdk.OneDec()
+	fivePct := sdk.MustNewDecFromStr("0.05")
+	validators := app.StakingKeeper.GetAllValidators(ctx)
+	ctx.Logger().Info("Increasing all validator's max commission to 100% and max change rate to 5%", "count", len(validators))
+	for _, validator := range validators {
+		validator.Commission.MaxRate = oneHundredPct
+		// Note: This MaxChangeRate bump was added after paua-rc1 was run on testnet.
+		// So, even though it's called by the paua-rc1 upgrade handler now,
+		// it wasn't part of the actual paua-rc1 upgrade that was performed on testnet.
+		if validator.Commission.MaxChangeRate.LT(fivePct) {
+			validator.Commission.MaxChangeRate = fivePct
+		}
+		app.StakingKeeper.SetValidator(ctx, validator)
+	}
+}
+
+func UndoMaxGasIncrease(ctx sdk.Context, app *App) {
+	ctx.Logger().Info("Setting max gas per block back to 60,000,000")
+	params := app.GetConsensusParams(ctx)
+	params.Block.MaxGas = 60_000_000
+	app.StoreConsensusParams(ctx, params)
+}
+
+func SetSanctionParams(ctx sdk.Context, app *App) error {
+	ctx.Logger().Info("Setting sanction params")
+	params := &sanction.Params{
+		ImmediateSanctionMinDeposit:   sdk.NewCoins(sdk.NewInt64Coin("nhash", 1_000_000_000_000_000)),
+		ImmediateUnsanctionMinDeposit: sdk.NewCoins(sdk.NewInt64Coin("nhash", 1_000_000_000_000_000)),
+	}
+	err := app.SanctionKeeper.SetParams(ctx, params)
+	if err != nil {
+		return fmt.Errorf("could not set sanction params: %w", err)
+	}
+	return nil
 }
