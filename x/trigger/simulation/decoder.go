6e0a7f4e63601a241c3c14a4b4491b4744489fee
Matthew Witkowski <mwitkowski@figure.com>
2023-05-31 09:47:34 -0400

    Decoder was added.

--- x/trigger/simulation/decoder.go
+++ x/trigger/simulation/decoder.go
@@ -1,18 +1,66 @@
 package simulation
 
 import (
+	"bytes"
+	"fmt"
+
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/types/kv"
+	"github.com/provenance-io/provenance/x/trigger/types"
 )
 
 // NewDecodeStore returns a decoder function closure that unmarshalls the KVPair's
 // Value
-func NewDecodeStore(_ codec.Codec) func(kvA, kvB kv.Pair) string {
+func NewDecodeStore(cdc codec.Codec) func(kvA, kvB kv.Pair) string {
 	return func(kvA, kvB kv.Pair) string {
-		/* switch {
+		switch {
+		case bytes.Equal(kvA.Key[:1], types.TriggerKeyPrefix):
+			var attribA, attribB types.Trigger
+
+			cdc.MustUnmarshal(kvA.Value, &attribA)
+			cdc.MustUnmarshal(kvB.Value, &attribB)
+
+			return fmt.Sprintf("%v\n%v", attribA, attribB)
+		case bytes.Equal(kvA.Key[:1], types.EventListenerKeyPrefix):
+			var attribA, attribB types.Trigger
+
+			cdc.MustUnmarshal(kvA.Value, &attribA)
+			cdc.MustUnmarshal(kvB.Value, &attribB)
+
+			return fmt.Sprintf("%v\n%v", attribA, attribB)
+		case bytes.Equal(kvA.Key[:1], types.QueueKeyPrefix):
+			var attribA, attribB types.QueuedTrigger
+
+			cdc.MustUnmarshal(kvA.Value, &attribA)
+			cdc.MustUnmarshal(kvB.Value, &attribB)
+
+			return fmt.Sprintf("%v\n%v", attribA, attribB)
+		case bytes.Equal(kvA.Key[:1], types.GasLimitKeyPrefix):
+			var attribA, attribB uint64
+			attribA = types.GetGasLimitFromBytes(kvA.Value)
+			attribB = types.GetGasLimitFromBytes(kvB.Value)
+
+			return fmt.Sprintf("%v\n%v", attribA, attribB)
+		case bytes.Equal(kvA.Key[:1], types.NextTriggerIDKey):
+			var attribA, attribB uint64
+			attribA = types.GetGasLimitFromBytes(kvA.Value)
+			attribB = types.GetGasLimitFromBytes(kvB.Value)
+
+			return fmt.Sprintf("%v\n%v", attribA, attribB)
+		case bytes.Equal(kvA.Key[:1], types.QueueStartIndexKey):
+			var attribA, attribB uint64
+			attribA = types.GetGasLimitFromBytes(kvA.Value)
+			attribB = types.GetGasLimitFromBytes(kvB.Value)
+
+			return fmt.Sprintf("%v\n%v", attribA, attribB)
+		case bytes.Equal(kvA.Key[:1], types.QueueLengthKey):
+			var attribA, attribB uint64
+			attribA = types.GetGasLimitFromBytes(kvA.Value)
+			attribB = types.GetGasLimitFromBytes(kvB.Value)
+
+			return fmt.Sprintf("%v\n%v", attribA, attribB)
 		default:
 			panic(fmt.Sprintf("unexpected %s key %X (%s)", types.ModuleName, kvA.Key, kvA.Key))
-		} */
-		return ""
+		}
 	}
 }
