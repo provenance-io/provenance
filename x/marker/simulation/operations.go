c0f22df667d83e592f9b41e0811fa03711912391
Matthew Witkowski <mwitkowski@figure.com>
2023-06-05 14:02:58 -0400

    Added in a fix that was causing the marker's supply in simulation to exceed the max.

--- x/marker/simulation/operations.go
+++ x/marker/simulation/operations.go
@@ -344,7 +344,7 @@ func Dispatch(
 	if sdk.MsgTypeURL(msg) == "/provenance.marker.v1.MsgAddMarkerRequest" && ak.GetAccount(ctx, account.GetAddress()) != nil {
 		err = testutil.FundAccount(bk, ctx, account.GetAddress(), sdk.NewCoins(sdk.Coin{
 			Denom:  "stake",
-			Amount: sdk.NewInt(1_000_000_000_000_000),
+			Amount: sdk.NewInt(100_000_000_000_000),
 		}))
 		fees = fees.Add(sdk.Coin{
 			Denom:  "stake",
