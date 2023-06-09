6ecb90884552042bd92a270e8714586c0ba72dc4
Matthew Witkowski <mwitkowski@figure.com>
2023-06-09 17:07:00 -0400

    Adjusted gas meter to normal meter and catch out of gas panic.

--- x/trigger/keeper/trigger_dispatcher.go
+++ x/trigger/keeper/trigger_dispatcher.go
@@ -6,6 +6,7 @@ import (
 	errorsmod "cosmossdk.io/errors"
 
 	types "github.com/cosmos/cosmos-sdk/codec/types"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdktx "github.com/cosmos/cosmos-sdk/types/tx"
 	"github.com/cosmos/cosmos-sdk/x/group/errors"
@@ -43,7 +44,7 @@ func (k Keeper) ProcessTriggers(ctx sdk.Context) {
 // RunActions Runs all the actions and constrains them by gasLimit.
 func (k Keeper) runActions(ctx sdk.Context, gasLimit uint64, actions []*types.Any) {
 	cacheCtx, flush := ctx.CacheContext()
-	gasMeter := sdk.NewInfiniteGasMeter()
+	gasMeter := sdk.NewGasMeter(gasLimit)
 	cacheCtx = cacheCtx.WithGasMeter(gasMeter)
 
 	msgs, err := sdktx.GetMsgs(actions, "RunActions")
@@ -55,7 +56,7 @@ func (k Keeper) runActions(ctx sdk.Context, gasLimit uint64, actions []*types.An
 		)
 		return
 	}
-	results, err := k.handleMsgs(cacheCtx, msgs, gasLimit)
+	results, err := k.handleMsgs(cacheCtx, msgs)
 	if err != nil {
 		k.Logger(ctx).Error(
 			"HandleMsgs",
@@ -71,14 +72,15 @@ func (k Keeper) runActions(ctx sdk.Context, gasLimit uint64, actions []*types.An
 }
 
 // handleMsgs Handles each message and verifies gas limit has not been exceeded.
-func (k Keeper) handleMsgs(ctx sdk.Context, msgs []sdk.Msg, gasLimit uint64) ([]sdk.Result, error) {
+func (k Keeper) handleMsgs(ctx sdk.Context, msgs []sdk.Msg) ([]sdk.Result, error) {
 	results := make([]sdk.Result, len(msgs))
+
 	for i, msg := range msgs {
 		handler := k.router.Handler(msg)
 		if handler == nil {
 			return nil, errorsmod.Wrapf(errors.ErrInvalid, "no message handler found for %q", sdk.MsgTypeURL(msg))
 		}
-		r, err := handler(ctx, msg)
+		r, err := k.safeHandle(ctx, msg, handler)
 		if err != nil {
 			return nil, errorsmod.Wrapf(err, "message %s at position %d", sdk.MsgTypeURL(msg), i)
 		}
@@ -87,11 +89,25 @@ func (k Keeper) handleMsgs(ctx sdk.Context, msgs []sdk.Msg, gasLimit uint64) ([]
 			return nil, fmt.Errorf("got nil sdk.Result for message %q at position %d", sdk.MsgTypeURL(msg), i)
 		}
 
-		if ctx.GasMeter().GasConsumed() > gasLimit {
-			return nil, fmt.Errorf("gas %d exceeded limit %d for message %q at position %d", ctx.GasMeter().GasConsumed(), gasLimit, sdk.MsgTypeURL(msg), i)
-		}
-
 		results[i] = *r
 	}
 	return results, nil
+}
+
+// safeHandle Handles one message and safely returns an error if it panics
+func (k Keeper) safeHandle(ctx sdk.Context, msg sdk.Msg, handler MsgServiceHandler) (result *sdk.Result, err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			_, ok := e.(storetypes.ErrorOutOfGas)
+			if ok {
+				result = nil
+				err = fmt.Errorf("gas %d exceeded limit %d for message %q", ctx.GasMeter().GasConsumed(), ctx.GasMeter().Limit(), sdk.MsgTypeURL(msg))
+				return
+			}
+
+			// It is not an out of gas panic so pass it up
+			panic(e)
+		}
+	}()
+	return handler(ctx, msg)
 }
