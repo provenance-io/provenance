f560c43f9e0e8079e3b62b4e8fc8411baee5590c
Matthew Witkowski <mwitkowski@figure.com>
2023-06-12 14:25:20 -0400

    Fixed linting on reward genesis file.

--- x/reward/simulation/genesis.go
+++ x/reward/simulation/genesis.go
@@ -9,6 +9,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+
 	"github.com/provenance-io/provenance/internal/pioconfig"
 	"github.com/provenance-io/provenance/x/reward/types"
 )
