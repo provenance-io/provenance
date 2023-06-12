dbaa27a363788c76b290cca4c0c37f2968075edf
Matthew Witkowski <mwitkowski@figure.com>
2023-06-12 14:24:00 -0400

    Updated documentation for keys.

--- x/trigger/types/keys.go
+++ x/trigger/types/keys.go
@@ -26,14 +26,57 @@ const (
 	GasLimitLength   = 8
 )
 
+// KVStore Key Prefixes used for iterator/scans against the store and identification of key types
+// The following keys are used to store Triggers
+// The <trigger_id_bytes> are 8 bytes to uniquely identify a trigger
+// The remaining key is used to track the next valid trigger id
+//
+//   - 0x01<trigger_id_bytes>: Trigger
+//     | 1 |        8        |
+//
+//   - 0x05: Trigger ID
+//     | 1 |
+//
+// The keys prefixed with 0x02 are used to quickly iterate trigger events during the detection phase
+// The <event_type_bytes> is 32 bytes representing the event type's name.
+// The next 8 bytes <order_bytes> are to help order the events and improve searching
+// The last 8 bytes <trigger_id_bytes> is a the trigger id that the event listener belongs to.
+//   - 0x02<event_type_bytes><order_bytes><trigger_id_bytes>: Trigger
+//     | 1 |       32       |      8      |        8        |
+//
+// The key in this section is used to track gas limits for triggers.
+// The <trigger_id_bytes> are 8 bytes that match the trigger that the gas limit belongs to.
+//
+//   - 0x04<trigger_id_bytes>: uint64 (GasLimit)
+//     | 1 |        8        |
+//
+// These keys are used to represent a queue in the store
+// The <queue_index_bytes> are 8 bytes representing the item's spot in the queue
+// The two additional keys are used as metadata to track and manage the queue.
+//
+//   - 0x03<queue_index_bytes>: QueuedTrigger
+//     | 1 |        8         |
+//
+//   - 0x06: uint64 (Queue Start Index)
+//     | 1 |
+//
+//   - 0x07: uint64 (Queue Length)
+//     | 1 |
 var (
-	TriggerKeyPrefix       = []byte{0x01}
+	// TriggerKeyPrefix is an initial byte to help group all trigger keys
+	TriggerKeyPrefix = []byte{0x01}
+	// EventListenerKeyPrefix is an initial byte to help group all event listener keys
 	EventListenerKeyPrefix = []byte{0x02}
-	QueueKeyPrefix         = []byte{0x03}
-	GasLimitKeyPrefix      = []byte{0x04}
-	NextTriggerIDKey       = []byte{0x05}
-	QueueStartIndexKey     = []byte{0x06}
-	QueueLengthKey         = []byte{0x07}
+	// QueueKeyPrefix is an initial byte to help group all queue entry keys
+	QueueKeyPrefix = []byte{0x03}
+	// GasLimitKeyPrefix is an initial byte to help group all gas limit keys
+	GasLimitKeyPrefix = []byte{0x04}
+	// NextTriggerIDKey is the key to obtain the next valid trigger id
+	NextTriggerIDKey = []byte{0x05}
+	// QueueStartIndexKey is the key to obtain the queue's starting index
+	QueueStartIndexKey = []byte{0x06}
+	// QueueStartIndexKey is the key to obtain the queue's length
+	QueueLengthKey = []byte{0x07}
 )
 
 // GetEventListenerKey converts an event name, order, and trigger ID into an event registry key format.
