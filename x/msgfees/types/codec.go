ed8fd4d764bce4dcc2edb4e5d8be9b7c15f88e68
Carlton N Hanna <nullpointer0x00@gmail.com>
2023-07-20 09:53:40 -0600

    run lint-fix

--- x/msgfees/types/codec.go
+++ x/msgfees/types/codec.go
@@ -1,11 +1,12 @@
 package types
 
 import (
+	proto "github.com/gogo/protobuf/proto"
+
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	govtypesv1beta1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
-	proto "github.com/gogo/protobuf/proto"
 )
 
 // ignoring RegisterLegacyAminoCodec registers all the necessary types and interfaces for the
