91b718c6ebe01a2a2eba92316ff39dd695906936
Daniel Wedul <github@wedul.com>
2023-07-25 16:29:13 -0600

    [1607]: Require addresses in the genesis state to be unique.

--- x/escrow/genesis.go
+++ x/escrow/genesis.go
@@ -10,10 +10,22 @@ func DefaultGenesisState() *GenesisState {
 }
 
 func (g GenesisState) Validate() error {
+	addrs := make(map[string]int)
 	var errs []error
 	for i, ae := range g.Escrows {
+		if ae == nil {
+			errs = append(errs, fmt.Errorf("invalid escrows[%d]: cannot be nil", i))
+			continue
+		}
 		if err := ae.Validate(); err != nil {
 			errs = append(errs, fmt.Errorf("invalid escrows[%d]: %w", i, err))
+			continue
+		}
+		j, seen := addrs[ae.Address]
+		if seen {
+			errs = append(errs, fmt.Errorf("invalid escrows[%d]: duplicate address also at index %d", i, j))
+		} else {
+			addrs[ae.Address] = i
 		}
 	}
 	return errors.Join(errs...)
