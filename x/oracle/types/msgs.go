2b97f265bb13844e7a637450570d991101a0dd72
Matthew Witkowski <mwitkowski@figure.com>
2023-07-27 16:32:50 -0400

    Added empty Msg interface implementation for UpdateOracle.

---
+++ x/oracle/types/msgs.go
@@ -0,0 +1,17 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+var _ sdk.Msg = &MsgUpdateOracleRequest{}
+
+// GetSigners indicates that the message must have been signed by the parent.
+func (msg MsgUpdateOracleRequest) GetSigners() []sdk.AccAddress {
+	return []sdk.AccAddress{sdk.MustAccAddressFromBech32(msg.Authority)}
+}
+
+// ValidateBasic runs stateless validation checks on the message.
+func (msg MsgUpdateOracleRequest) ValidateBasic() error {
+	return nil
+}
