bf5cd7853c34c8753ba5e83372066590d0839324
Daniel Wedul <github@wedul.com>
2023-08-25 18:51:10 -0600

    [1658]: Create GetMarketAddress.

--- x/exchange/keys.go
+++ x/exchange/keys.go
@@ -1,5 +1,13 @@
 package exchange
 
+import (
+	"fmt"
+
+	"github.com/tendermint/tendermint/crypto"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
 const (
 	// ModuleName is the name of the exchange module.
 	ModuleName = "exchange"
@@ -7,3 +15,8 @@ const (
 	// StoreKey is the store key string for the exchange module.
 	StoreKey = ModuleName
 )
+
+// GetMarketAddress returns the module account address for the given marketID.
+func GetMarketAddress(marketID uint32) sdk.AccAddress {
+	return sdk.AccAddress(crypto.AddressHash([]byte(fmt.Sprintf("%s/%d", ModuleName, marketID))))
+}
