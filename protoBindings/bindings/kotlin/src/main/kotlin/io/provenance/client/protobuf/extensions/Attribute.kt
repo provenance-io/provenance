279d8d38c909f31281bdab2ffc5b51de1698f97c
Mike Woods <mwoods@figure.com>
2023-08-28 16:55:03 -0400

    Add existing extension methods to Provenance coroutine-based client for feature parity

--- protoBindings/bindings/kotlin/src/main/kotlin/io/provenance/client/protobuf/extensions/Attribute.kt
+++ protoBindings/bindings/kotlin/src/main/kotlin/io/provenance/client/protobuf/extensions/Attribute.kt
@@ -3,7 +3,8 @@ package io.provenance.client.protobuf.extensions
 import io.provenance.attribute.v1.QueryAttributesRequest
 import io.provenance.client.protobuf.paginationBuilder
 import java.util.concurrent.TimeUnit
-import io.provenance.attribute.v1.QueryGrpc.QueryBlockingStub as Attribute
+import io.provenance.attribute.v1.QueryGrpc.QueryBlockingStub as BlockingAttribute
+import io.provenance.attribute.v1.QueryGrpcKt.QueryCoroutineStub as CoroutineAttribute
 
 /**
  * Given an address, fetch all [io.provenance.attribute.v1.Attribute] instances associated with it.
@@ -14,9 +15,40 @@ import io.provenance.attribute.v1.QueryGrpc.QueryBlockingStub as Attribute
  * @param deadlineInSeconds
  * @return A list of [io.provenance.attribute.v1.Attribute]
  */
-fun Attribute.getAllAttributes(
+fun BlockingAttribute.getAllAttributes(
     address: String,
-    deadlineInSeconds: Long? = null
+    deadlineInSeconds: Long? = null,
+): List<io.provenance.attribute.v1.Attribute> {
+    var offset = 0
+    var total: Long
+    val limit = 100
+    val attributes = mutableListOf<io.provenance.attribute.v1.Attribute>()
+
+    do {
+        val request = QueryAttributesRequest.newBuilder()
+            .setAccount(address)
+            .setPagination(paginationBuilder(offset, limit))
+            .build()
+
+        val client = if (deadlineInSeconds == null) {
+            this
+        } else {
+            this.withDeadlineAfter(deadlineInSeconds, TimeUnit.SECONDS)
+        }
+
+        val results = client.attributes(request)
+
+        total = results.pagination?.total ?: results.attributesCount.toLong()
+        offset += limit
+        attributes.addAll(results.attributesList)
+    } while (attributes.count() < total)
+
+    return attributes
+}
+
+suspend fun CoroutineAttribute.getAllAttributes(
+    address: String,
+    deadlineInSeconds: Long? = null,
 ): List<io.provenance.attribute.v1.Attribute> {
     var offset = 0
     var total: Long
