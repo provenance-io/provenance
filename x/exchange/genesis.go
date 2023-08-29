ff391a1c973b2014048e76e093c40d1c3554098c
Daniel Wedul <github@wedul.com>
2023-08-28 18:59:43 -0600

    [1658]: Update GenesisState.Validate to prevent duplicate ids and make sure orders have a valid market id. Unit tests on the genesis funcs.

--- x/exchange/genesis.go
+++ x/exchange/genesis.go
@@ -22,19 +22,48 @@ func (g GenesisState) Validate() error {
 
 	if g.Params != nil {
 		if err := g.Params.Validate(); err != nil {
-			errs = append(errs, err)
+			errs = append(errs, fmt.Errorf("invalid params: %w", err))
 		}
 	}
 
+	marketIDs := make(map[uint32]int)
 	for i, market := range g.Markets {
+		if market.MarketId == 0 {
+			errs = append(errs, fmt.Errorf("invalid market[%d]: market id cannot be zero", i))
+			continue
+		}
+
+		j, seen := marketIDs[market.MarketId]
+		if seen {
+			errs = append(errs, fmt.Errorf("invalid market[%d]: duplicate market id %d seen at [%d]", i, market.MarketId, j))
+			continue
+		}
+		marketIDs[market.MarketId] = i
+
 		if err := market.Validate(); err != nil {
 			errs = append(errs, fmt.Errorf("invalid market[%d]: %w", i, err))
 		}
 	}
 
+	orderIDs := make(map[uint64]int)
 	for i, order := range g.Orders {
+		if order.OrderId != 0 {
+			j, seen := orderIDs[order.OrderId]
+			if seen {
+				errs = append(errs, fmt.Errorf("invalid order[%d]: duplicate order id %d seen at [%d]", i, order.OrderId, j))
+				continue
+			}
+			orderIDs[order.OrderId] = i
+		}
+
 		if err := order.Validate(); err != nil {
 			errs = append(errs, fmt.Errorf("invalid order[%d]: %w", i, err))
+			continue
+		}
+
+		_, knownMarket := marketIDs[order.GetMarketID()]
+		if !knownMarket {
+			errs = append(errs, fmt.Errorf("invalid order[%d]: unknown market id %d", i, order.GetMarketID()))
 		}
 	}
 
--- x/exchange/genesis_test.go
+++ x/exchange/genesis_test.go
@@ -1,7 +1,434 @@
 package exchange
 
-// TODO[1658]: func TestNewGenesisState(t *testing.t)
+import (
+	"testing"
 
-// TODO[1658]: func TestDefaultGenesisState(t *testing.t)
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
-// TODO[1658]: func TestGenesisState_Validate(t *testing.t)
+	sdkmath "cosmossdk.io/math"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func TestNewGenesisState(t *testing.T) {
+	coin := func(amount int64, denom string) sdk.Coin {
+		return sdk.Coin{Denom: denom, Amount: sdkmath.NewInt(amount)}
+	}
+
+	tests := []struct {
+		name     string
+		params   *Params
+		markets  []Market
+		orders   []Order
+		expected *GenesisState
+	}{
+		{
+			name:     "all nil",
+			params:   nil,
+			markets:  nil,
+			orders:   nil,
+			expected: &GenesisState{},
+		},
+		{
+			name: "only params",
+			params: &Params{
+				DefaultSplit: 54,
+				DenomSplits: []DenomSplit{
+					{Denom: "farnsworth", Split: 88},
+					{Denom: "fry", Split: 1000},
+				},
+			},
+			expected: &GenesisState{
+				Params: &Params{
+					DefaultSplit: 54,
+					DenomSplits: []DenomSplit{
+						{Denom: "farnsworth", Split: 88},
+						{Denom: "fry", Split: 1000},
+					},
+				},
+			},
+		},
+		{
+			name: "only markets",
+			markets: []Market{
+				{
+					MarketId:         1,
+					MarketDetails:    MarketDetails{Name: "Market One"},
+					FeeCreateAskFlat: []sdk.Coin{coin(5, "askd")},
+					AcceptingOrders:  true,
+				},
+				{
+					MarketId:                 2,
+					MarketDetails:            MarketDetails{Name: "Market Two"},
+					FeeCreateBidFlat:         []sdk.Coin{coin(3, "bidd")},
+					FeeSettlementBuyerRatios: []*FeeRatio{{Price: coin(7, "buyrr"), Fee: coin(1, "buyrr")}},
+					AccessGrants:             []*AccessGrant{{Address: "addr1", Permissions: AllPermissions()}},
+					ReqAttrCreateBid:         []string{"just.some.attr"},
+				},
+			},
+			expected: &GenesisState{
+				Params: nil,
+				Markets: []Market{
+					{
+						MarketId:         1,
+						MarketDetails:    MarketDetails{Name: "Market One"},
+						FeeCreateAskFlat: []sdk.Coin{coin(5, "askd")},
+						AcceptingOrders:  true,
+					},
+					{
+						MarketId:                 2,
+						MarketDetails:            MarketDetails{Name: "Market Two"},
+						FeeCreateBidFlat:         []sdk.Coin{coin(3, "bidd")},
+						FeeSettlementBuyerRatios: []*FeeRatio{{Price: coin(7, "buyrr"), Fee: coin(1, "buyrr")}},
+						AccessGrants:             []*AccessGrant{{Address: "addr1", Permissions: AllPermissions()}},
+						ReqAttrCreateBid:         []string{"just.some.attr"},
+					},
+				},
+				Orders: nil,
+			},
+		},
+		{
+			name: "just orders",
+			orders: []Order{
+				*NewOrder(1).WithAsk(&AskOrder{
+					MarketId: 1,
+					Seller:   "seller_addr",
+					Assets:   []sdk.Coin{coin(55, "fry")},
+					Price:    coin(888, "nibbler"),
+				}),
+				*NewOrder(1).WithBid(&BidOrder{
+					MarketId: 1,
+					Buyer:    "buyer_addr",
+					Assets:   []sdk.Coin{coin(55, "fry")},
+					Price:    coin(888, "nibbler"),
+				}),
+			},
+			expected: &GenesisState{
+				Params:  nil,
+				Markets: nil,
+				Orders: []Order{
+					*NewOrder(1).WithAsk(&AskOrder{
+						MarketId: 1,
+						Seller:   "seller_addr",
+						Assets:   []sdk.Coin{coin(55, "fry")},
+						Price:    coin(888, "nibbler"),
+					}),
+					*NewOrder(1).WithBid(&BidOrder{
+						MarketId: 1,
+						Buyer:    "buyer_addr",
+						Assets:   []sdk.Coin{coin(55, "fry")},
+						Price:    coin(888, "nibbler"),
+					}),
+				},
+			},
+		},
+		{
+			name: "all three together",
+			params: &Params{
+				DefaultSplit: 54,
+				DenomSplits: []DenomSplit{
+					{Denom: "farnsworth", Split: 88},
+					{Denom: "fry", Split: 1000},
+				},
+			},
+			markets: []Market{
+				{
+					MarketId:         1,
+					MarketDetails:    MarketDetails{Name: "Market One"},
+					FeeCreateAskFlat: []sdk.Coin{coin(5, "askd")},
+					AcceptingOrders:  true,
+				},
+				{
+					MarketId:                 2,
+					MarketDetails:            MarketDetails{Name: "Market Two"},
+					FeeCreateBidFlat:         []sdk.Coin{coin(3, "bidd")},
+					FeeSettlementBuyerRatios: []*FeeRatio{{Price: coin(7, "buyrr"), Fee: coin(1, "buyrr")}},
+					AccessGrants:             []*AccessGrant{{Address: "addr1", Permissions: AllPermissions()}},
+					ReqAttrCreateBid:         []string{"just.some.attr"},
+				},
+			},
+			orders: []Order{
+				*NewOrder(1).WithAsk(&AskOrder{
+					MarketId: 1,
+					Seller:   "seller_addr",
+					Assets:   []sdk.Coin{coin(55, "fry")},
+					Price:    coin(888, "nibbler"),
+				}),
+				*NewOrder(1).WithBid(&BidOrder{
+					MarketId: 1,
+					Buyer:    "buyer_addr",
+					Assets:   []sdk.Coin{coin(55, "fry")},
+					Price:    coin(888, "nibbler"),
+				}),
+			},
+			expected: &GenesisState{
+				Params: &Params{
+					DefaultSplit: 54,
+					DenomSplits: []DenomSplit{
+						{Denom: "farnsworth", Split: 88},
+						{Denom: "fry", Split: 1000},
+					},
+				},
+				Markets: []Market{
+					{
+						MarketId:         1,
+						MarketDetails:    MarketDetails{Name: "Market One"},
+						FeeCreateAskFlat: []sdk.Coin{coin(5, "askd")},
+						AcceptingOrders:  true,
+					},
+					{
+						MarketId:                 2,
+						MarketDetails:            MarketDetails{Name: "Market Two"},
+						FeeCreateBidFlat:         []sdk.Coin{coin(3, "bidd")},
+						FeeSettlementBuyerRatios: []*FeeRatio{{Price: coin(7, "buyrr"), Fee: coin(1, "buyrr")}},
+						AccessGrants:             []*AccessGrant{{Address: "addr1", Permissions: AllPermissions()}},
+						ReqAttrCreateBid:         []string{"just.some.attr"},
+					},
+				},
+				Orders: []Order{
+					*NewOrder(1).WithAsk(&AskOrder{
+						MarketId: 1,
+						Seller:   "seller_addr",
+						Assets:   []sdk.Coin{coin(55, "fry")},
+						Price:    coin(888, "nibbler"),
+					}),
+					*NewOrder(1).WithBid(&BidOrder{
+						MarketId: 1,
+						Buyer:    "buyer_addr",
+						Assets:   []sdk.Coin{coin(55, "fry")},
+						Price:    coin(888, "nibbler"),
+					}),
+				},
+			},
+		},
+		{
+			name:     "defaults",
+			params:   DefaultParams(),
+			markets:  nil,
+			orders:   nil,
+			expected: DefaultGenesisState(),
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := NewGenesisState(tc.params, tc.markets, tc.orders)
+			assert.Equal(t, tc.expected, actual, "NewGenesisState")
+		})
+	}
+}
+
+func TestGenesisState_Validate(t *testing.T) {
+	addr1 := sdk.AccAddress("addr1_______________").String()
+	coins := func(coins string) sdk.Coins {
+		rv, err := sdk.ParseCoinsNormalized(coins)
+		require.NoError(t, err, "sdk.ParseCoinsNormalized(%q)", coins)
+		return rv
+	}
+	coin := func(amount int64, denom string) sdk.Coin {
+		return sdk.Coin{Denom: denom, Amount: sdkmath.NewInt(amount)}
+	}
+	askOrder := func(orderID uint64, marketID uint32, assets string, price string) Order {
+		priceCoin, err := sdk.ParseCoinNormalized(price)
+		require.NoError(t, err, "sdk.ParseCoinNormalized(%q)", price)
+		return *NewOrder(orderID).WithAsk(&AskOrder{
+			MarketId: marketID,
+			Seller:   addr1,
+			Assets:   coins(assets),
+			Price:    priceCoin,
+		})
+	}
+	bidOrder := func(orderID uint64, marketID uint32, assets string, price string) Order {
+		priceCoin, err := sdk.ParseCoinNormalized(price)
+		require.NoError(t, err, "sdk.ParseCoinNormalized(%q)", price)
+		return *NewOrder(orderID).WithBid(&BidOrder{
+			MarketId: marketID,
+			Buyer:    addr1,
+			Assets:   coins(assets),
+			Price:    priceCoin,
+		})
+	}
+
+	tests := []struct {
+		name     string
+		genState GenesisState
+		expErr   []string
+	}{
+		{
+			name:     "zero state",
+			genState: GenesisState{},
+			expErr:   nil,
+		},
+		{
+			name:     "default state",
+			genState: *DefaultGenesisState(),
+			expErr:   nil,
+		},
+		{
+			name:     "empty params",
+			genState: GenesisState{Params: &Params{}},
+			expErr:   nil,
+		},
+		{
+			name:     "invalid params",
+			genState: GenesisState{Params: &Params{DefaultSplit: 10_001}},
+			expErr:   []string{"invalid params: default split 10001 cannot be greater than 10000"},
+		},
+		{
+			name: "three markets, 3 orders each all valid",
+			genState: GenesisState{
+				Params: DefaultParams(),
+				Markets: []Market{
+					{MarketId: 1},
+					{MarketId: 2},
+					{MarketId: 3},
+				},
+				Orders: []Order{
+					askOrder(1, 1, "99fry", "9leela"),
+					bidOrder(2, 1, "100fry", "10leela"),
+					bidOrder(3, 1, "101fry", "11leela"),
+					askOrder(4, 2, "12zapp", "1farnsworth"),
+					askOrder(5, 2, "14zapp", "1farnsworth"),
+					bidOrder(6, 2, "80zapp", "10farnsworth"),
+					bidOrder(7, 3, "5wong", "50nibbler"),
+					bidOrder(8, 3, "5wong", "50nibbler"),
+					bidOrder(9, 3, "5wong", "50nibbler"),
+				},
+			},
+			expErr: nil,
+		},
+		{
+			name: "three markets: all invalid",
+			genState: GenesisState{
+				Markets: []Market{
+					{MarketId: 1, FeeCreateAskFlat: sdk.Coins{coin(-1, "kif")}},
+					{MarketId: 2, FeeCreateAskFlat: sdk.Coins{coin(-2, "kif")}},
+					{MarketId: 3, FeeCreateAskFlat: sdk.Coins{coin(-3, "kif")}},
+				},
+			},
+			expErr: []string{
+				`invalid market[0]: invalid create ask flat fee option "-1kif": negative coin amount: -1`,
+				`invalid market[1]: invalid create ask flat fee option "-2kif": negative coin amount: -2`,
+				`invalid market[2]: invalid create ask flat fee option "-3kif": negative coin amount: -3`,
+			},
+		},
+		{
+			name: "three markets: all market id zero",
+			genState: GenesisState{
+				Markets: []Market{
+					{MarketId: 0},
+					{MarketId: 0},
+					{MarketId: 0},
+				},
+			},
+			expErr: []string{
+				`invalid market[0]: market id cannot be zero`,
+				`invalid market[1]: market id cannot be zero`,
+				`invalid market[2]: market id cannot be zero`,
+			},
+		},
+		{
+			name: "three markets: all market id one",
+			genState: GenesisState{
+				Markets: []Market{
+					{MarketId: 1},
+					{MarketId: 1},
+					{MarketId: 1},
+				},
+			},
+			expErr: []string{
+				`invalid market[1]: duplicate market id 1 seen at [0]`,
+				`invalid market[2]: duplicate market id 1 seen at [0]`,
+			},
+		},
+		{
+			name: "three orders: all invalid",
+			genState: GenesisState{
+				Markets: []Market{{MarketId: 4}, {MarketId: 5}, {MarketId: 6}},
+				Orders: []Order{
+					askOrder(0, 4, "28fry", "2bender"),
+					bidOrder(0, 5, "28fry", "2bender"),
+					askOrder(0, 6, "28fry", "2bender"),
+				},
+			},
+			expErr: []string{
+				`invalid order[0]: invalid order id: must not be zero`,
+				`invalid order[1]: invalid order id: must not be zero`,
+				`invalid order[2]: invalid order id: must not be zero`,
+			},
+		},
+		{
+			name: "three orders: all unknown markets",
+			genState: GenesisState{
+				Markets: []Market{{MarketId: 4}, {MarketId: 5}, {MarketId: 6}},
+				Orders: []Order{
+					askOrder(1, 1, "28fry", "2bender"),
+					bidOrder(2, 2, "28fry", "2bender"),
+					askOrder(3, 3, "28fry", "2bender"),
+				},
+			},
+			expErr: []string{
+				`invalid order[0]: unknown market id 1`,
+				`invalid order[1]: unknown market id 2`,
+				`invalid order[2]: unknown market id 3`,
+			},
+		},
+		{
+			name: "three orders: all id one",
+			genState: GenesisState{
+				Markets: []Market{{MarketId: 4}, {MarketId: 5}, {MarketId: 6}},
+				Orders: []Order{
+					askOrder(1, 4, "28fry", "2bender"),
+					bidOrder(1, 5, "28fry", "2bender"),
+					askOrder(1, 6, "28fry", "2bender"),
+				},
+			},
+			expErr: []string{
+				`invalid order[1]: duplicate order id 1 seen at [0]`,
+				`invalid order[2]: duplicate order id 1 seen at [0]`,
+			},
+		},
+		{
+			name: "multiple errors",
+			genState: GenesisState{
+				Params: &Params{DefaultSplit: 10_001},
+				Markets: []Market{
+					{MarketId: 1},
+					{MarketId: 2, FeeCreateBidFlat: sdk.Coins{coin(-1, "zapp")}},
+					{MarketId: 3},
+				},
+				Orders: []Order{
+					askOrder(1, 1, "28fry", "2bender"),
+					bidOrder(2, 4, "28fry", "2bender"),
+					askOrder(3, 3, "28fry", "2bender"),
+				},
+			},
+			expErr: []string{
+				"invalid params: default split 10001 cannot be greater than 10000",
+				`invalid market[1]: invalid create bid flat fee option "-1zapp": negative coin amount: -1`,
+				`invalid order[1]: unknown market id 4`,
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			var err error
+			testFunc := func() {
+				err = tc.genState.Validate()
+			}
+			require.NotPanics(t, testFunc, "GenesisState.Validate")
+
+			// TODO[1658]: Replace this with testutils.AssertErrorContents(t, err, tc.expErr, "GenesisState.Validate")
+			if len(tc.expErr) > 0 {
+				if assert.Error(t, err, "GenesisState.Validate") {
+					for _, exp := range tc.expErr {
+						assert.ErrorContains(t, err, exp, "GenesisState.Validate")
+					}
+				}
+			} else {
+				assert.NoError(t, err, "GenesisState.Validate")
+			}
+		})
+	}
+}
