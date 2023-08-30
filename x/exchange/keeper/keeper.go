3b0b558d373a946843fc80146597d2aeaf3b5583
Daniel Wedul <github@wedul.com>
2023-08-29 18:11:04 -0600

    [1658]: Add the authority to the keeper.

--- x/exchange/keeper/keeper.go
+++ x/exchange/keeper/keeper.go
@@ -3,19 +3,23 @@ package keeper
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 )
 
 type Keeper struct {
 	cdc      codec.BinaryCodec
 	storeKey storetypes.StoreKey
 	// TODO[1658]: Finish the Keeper struct.
+	authority string
 }
 
 func NewKeeper(cdc codec.BinaryCodec, storeKey storetypes.StoreKey) Keeper {
 	// TODO[1658]: Finish NewKeeper.
 	rv := Keeper{
-		cdc:      cdc,
-		storeKey: storeKey,
+		cdc:       cdc,
+		storeKey:  storeKey,
+		authority: authtypes.NewModuleAddress(govtypes.ModuleName).String(),
 	}
 	return rv
 }
