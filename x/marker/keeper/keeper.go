b799176f4a870ea44e2c56beb38d5967433b2e24
Matthew Witkowski <mwitkowski@figure.com>
2023-09-12 11:41:14 -0400

    Fix import linting.

--- x/marker/keeper/keeper.go
+++ x/marker/keeper/keeper.go
@@ -3,8 +3,9 @@ package keeper
 import (
 	"fmt"
 
-	"cosmossdk.io/math"
 	"github.com/tendermint/tendermint/libs/log"
+
+	"cosmossdk.io/math"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
