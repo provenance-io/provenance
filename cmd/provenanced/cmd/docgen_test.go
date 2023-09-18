fdd8e850debf11959fc2f9121053fe9dfd49a3c7
Matthew Witkowski <mwitkowski@figure.com>
2023-09-18 17:10:53 -0400

    Finish tests for docgen command.

--- cmd/provenanced/cmd/docgen_test.go
+++ cmd/provenanced/cmd/docgen_test.go
@@ -23,6 +23,7 @@ func TestDocGen(t *testing.T) {
 		createTarget bool
 		flags        []string
 		err          string
+		extensions   []string
 	}{
 		{
 			name:         "failure - no flags specified",
@@ -42,36 +43,42 @@ func TestDocGen(t *testing.T) {
 			target:       "tmp",
 			createTarget: true,
 			flags:        []string{"--yaml"},
+			extensions:   []string{".yaml"},
 		},
 		{
 			name:         "success - rest is generated",
 			target:       "tmp",
 			createTarget: true,
 			flags:        []string{"--rest"},
+			extensions:   []string{".rst"},
 		},
 		{
 			name:         "success - manpage is generated",
 			target:       "tmp",
 			createTarget: true,
 			flags:        []string{"--manpage"},
+			extensions:   []string{".1"},
 		},
 		{
 			name:         "success - markdown is generated",
 			target:       "tmp",
 			createTarget: true,
 			flags:        []string{"--markdown"},
+			extensions:   []string{".md"},
 		},
 		{
 			name:         "success - multiple types supported",
 			target:       "tmp",
 			createTarget: true,
 			flags:        []string{"--markdown", "--yaml"},
+			extensions:   []string{".md", ".yaml"},
 		},
 		{
 			name:         "success - generates a new directory",
 			target:       "tmp2",
 			createTarget: false,
 			flags:        []string{"--yaml"},
+			extensions:   []string{".md", ".yaml"},
 		},
 	}
 
@@ -81,16 +88,16 @@ func TestDocGen(t *testing.T) {
 
 			targetPath := filepath.Join(home, tc.target)
 			if tc.createTarget {
-				require.NoError(t, os.Mkdir(targetPath, 0755))
+				require.NoError(t, os.Mkdir(targetPath, 0755), "Mkdir successfully created directory")
 			}
 
 			logger := log.NewNopLogger()
 			cfg, err := genutiltest.CreateDefaultTendermintConfig(home)
-			require.NoError(t, err)
+			require.NoError(t, err, "Created default tendermint config")
 
 			appCodec := sdksim.MakeTestEncodingConfig().Codec
 			err = genutiltest.ExecInitCmd(testMbm, home, appCodec)
-			require.NoError(t, err)
+			require.NoError(t, err, "Executed init command")
 
 			serverCtx := server.NewContext(viper.New(), cfg, logger)
 			clientCtx := client.Context{}.WithCodec(appCodec).WithHomeDir(home)
@@ -105,15 +112,31 @@ func TestDocGen(t *testing.T) {
 
 			if len(tc.err) > 0 {
 				err := cmd.ExecuteContext(ctx)
-				require.Error(t, err)
-				require.Equal(t, tc.err, err.Error())
+				require.Error(t, err, "should throw an error")
+				require.Equal(t, tc.err, err.Error(), "should return the correct error")
+				files, err := os.ReadDir(targetPath)
+				require.NoError(t, err, "ReadDir should not return an error")
+				require.Equal(t, 0, len(files), "should not generate files when failed")
 			} else {
 				err := cmd.ExecuteContext(ctx)
-				require.NoError(t, err)
-				// We probably want to check generation of files
+				require.NoError(t, err, "should not return an error")
+
+				files, err := os.ReadDir(targetPath)
+				require.NoError(t, err, "ReadDir should not return an error")
+				require.NotZero(t, len(files), "should generate files when successful")
+
+				for _, file := range files {
+					ext := filepath.Ext(file.Name())
+
+					contains := false
+					for _, extension := range tc.extensions {
+						contains = contains || ext == extension
+					}
+					require.True(t, contains, "should generate files with correct extension")
+				}
 			}
 
-			require.NoError(t, os.RemoveAll(targetPath))
+			require.NoError(t, os.RemoveAll(targetPath), "RemoveAll should be able to remove the temporary target directory")
 		})
 	}
 }
