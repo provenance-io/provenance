b5c663c9796a5482638d618e639a79d9c2d5fe5d
Matthew Witkowski <mwitkowski@figure.com>
2023-10-06 17:00:38 -0400

    Start conversion to provwasm. Still had some issues with optimize.

--- x/ibcratelimit/contracts/rate-limiter/src/packet.rs
+++ x/ibcratelimit/contracts/rate-limiter/src/packet.rs
@@ -1,6 +1,6 @@
 use crate::state::FlowType;
 use cosmwasm_std::{Addr, Deps, StdError, Uint256};
-use osmosis_std_derive::CosmwasmExt;
+use provwasm_std::types::cosmos::bank::v1beta1::QuerySupplyOfRequest;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
@@ -35,45 +35,6 @@ pub struct Packet {
     pub timeout_height: Height,
     pub timeout_timestamp: Option<u64>,
 }
-
-// SupplyOf query message definition.
-// osmosis-std doesn't currently support the SupplyOf query, so I'm defining it localy so it can be used to obtain the channel value
-#[derive(
-    Clone,
-    PartialEq,
-    Eq,
-    ::prost::Message,
-    serde::Serialize,
-    serde::Deserialize,
-    schemars::JsonSchema,
-    CosmwasmExt,
-)]
-#[proto_message(type_url = "/cosmos.bank.v1beta1.QuerySupplyOfRequest")]
-#[proto_query(
-    path = "/cosmos.bank.v1beta1.Query/SupplyOf",
-    response_type = QuerySupplyOfResponse
-)]
-pub struct QuerySupplyOfRequest {
-    #[prost(string, tag = "1")]
-    pub denom: ::prost::alloc::string::String,
-}
-
-#[derive(
-    Clone,
-    PartialEq,
-    Eq,
-    ::prost::Message,
-    serde::Serialize,
-    serde::Deserialize,
-    schemars::JsonSchema,
-    CosmwasmExt,
-)]
-#[proto_message(type_url = "/cosmos.bank.v1beta1.QuerySupplyOf")]
-pub struct QuerySupplyOfResponse {
-    #[prost(message, optional, tag = "1")]
-    pub amount: ::core::option::Option<osmosis_std::types::cosmos::base::v1beta1::Coin>,
-}
-// End of SupplyOf query message definition
 
 use std::str::FromStr; // Needed to parse the coin's String as Uint256
 
