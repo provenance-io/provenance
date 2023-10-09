7b48bbc6d85511f4d3f4660a9f0648c0c7628df0
Daniel Wedul <github@wedul.com>
2023-10-09 14:41:38 -0600

    [1658]: Create TODOs for all the keeper unit tests.

---
+++ x/exchange/keeper/grpc_query_test.go
@@ -0,0 +1,27 @@
+package keeper_test
+
+// TODO[1658]: func (s *TestSuite) TestNewQueryServer()
+
+// TODO[1658]: func (s *TestSuite) TestQueryServer_QueryOrderFeeCalc()
+
+// TODO[1658]: func (s *TestSuite) TestQueryServer_QueryGetOrder()
+
+// TODO[1658]: func (s *TestSuite) TestQueryServer_QueryGetMarketOrders()
+
+// TODO[1658]: func (s *TestSuite) TestQueryServer_QueryGetOwnerOrders()
+
+// TODO[1658]: func (s *TestSuite) TestQueryServer_QueryGetAssetOrders()
+
+// TODO[1658]: func (s *TestSuite) TestQueryServer_QueryGetAllOrders()
+
+// TODO[1658]: func (s *TestSuite) TestQueryServer_QueryGetMarket()
+
+// TODO[1658]: func (s *TestSuite) TestQueryServer_QueryGetAllMarkets()
+
+// TODO[1658]: func (s *TestSuite) TestQueryServer_QueryParams()
+
+// TODO[1658]: func (s *TestSuite) TestQueryServer_QueryValidateCreateMarket()
+
+// TODO[1658]: func (s *TestSuite) TestQueryServer_QueryValidateMarket()
+
+// TODO[1658]: func (s *TestSuite) TestQueryServer_QueryValidateManageFees()
