1d01aac0422b604f5e99ffa24f9f5c29110e35c7
Matthew Witkowski <mwitkowski@figure.com>
2023-10-30 11:02:57 -0400

    Add tests for UndoSendRateLimit.

--- x/ibcratelimit/keeper/rate_limit_test.go
+++ x/ibcratelimit/keeper/rate_limit_test.go
@@ -22,7 +22,7 @@ func (s *TestSuite) TestCheckAndUpdateRateLimits() {
 			mockKeeper: NewMockPermissionedKeeper(true),
 		},
 		{
-			name:       "success - rate limit checked and updated on send",
+			name:       "success - rate limit checked and updated on recv",
 			contract:   "cosmos1w6t0l7z0yerj49ehnqwqaayxqpe3u7e23edgma",
 			msgType:    ibcratelimit.MsgRecvPacket,
 			packet:     NewMockPacket(NewMockSerializedPacketData(), true),
@@ -95,12 +95,60 @@ func (s *TestSuite) TestCheckAndUpdateRateLimits() {
 
 func (s *TestSuite) TestUndoSendRateLimit() {
 	tests := []struct {
-		name string
-	}{}
+		name       string
+		contract   string
+		packet     exported.PacketI
+		err        string
+		mockKeeper *MockPermissionedKeeper
+	}{
+		{
+			name:       "success - undo rate limit",
+			contract:   "cosmos1w6t0l7z0yerj49ehnqwqaayxqpe3u7e23edgma",
+			packet:     NewMockPacket(NewMockSerializedPacketData(), true),
+			mockKeeper: NewMockPermissionedKeeper(true),
+		},
+		{
+			name:     "failure - an invalid contract throws error",
+			contract: "",
+			packet:   NewMockPacket(NewMockSerializedPacketData(), false),
+			err:      "empty address string is not allowed",
+		},
+		{
+			name:     "failure - throws error on bad packet",
+			contract: "cosmos1w6t0l7z0yerj49ehnqwqaayxqpe3u7e23edgma",
+			packet:   NewMockPacket(NewMockSerializedPacketData(), false),
+			err:      "bad message",
+		},
+		{
+			name:     "failure - throws error on nil packet",
+			contract: "cosmos1w6t0l7z0yerj49ehnqwqaayxqpe3u7e23edgma",
+			packet:   NewMockPacket(NewMockSerializedPacketData(), false),
+			err:      "bad message",
+		},
+		{
+			name:       "failure - throws error on bad contract operation",
+			contract:   "cosmos1w6t0l7z0yerj49ehnqwqaayxqpe3u7e23edgma",
+			packet:     NewMockPacket(NewMockSerializedPacketData(), true),
+			mockKeeper: NewMockPermissionedKeeper(false),
+			err:        "rate limit exceeded: contract error",
+		},
+	}
 
 	for _, tc := range tests {
 		s.Run(tc.name, func() {
+			permissionedKeeper := s.app.RateLimitingKeeper.PermissionedKeeper
+			if tc.mockKeeper != nil {
+				s.app.RateLimitingKeeper.PermissionedKeeper = tc.mockKeeper
+			}
 
+			err := s.app.RateLimitingKeeper.UndoSendRateLimit(s.ctx, tc.contract, tc.packet)
+			if len(tc.err) > 0 {
+				s.Assert().EqualError(err, tc.err, "should return the correct error")
+			} else {
+				s.Assert().NoError(err)
+			}
+
+			s.app.RateLimitingKeeper.PermissionedKeeper = permissionedKeeper
 		})
 	}
 }
