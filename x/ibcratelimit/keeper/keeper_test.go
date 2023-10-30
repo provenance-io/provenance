498650165e3de30de19a169f2fb73a935f0dc212
Matthew Witkowski <mwitkowski@figure.com>
2023-10-30 09:16:57 -0400

    Start adding keeper tests.

--- x/ibcratelimit/keeper/keeper_test.go
+++ x/ibcratelimit/keeper/keeper_test.go
@@ -5,74 +5,108 @@ import (
 
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
+	"github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/provenance-io/provenance/app"
-	"github.com/provenance-io/provenance/x/ibcratelimit/keeper"
+	"github.com/provenance-io/provenance/x/ibcratelimit"
 	"github.com/stretchr/testify/suite"
 )
 
 type TestSuite struct {
 	suite.Suite
 
-	app    *app.App
-	ctx    sdk.Context
-	keeper keeper.Keeper
+	app *app.App
+	ctx sdk.Context
+
+	queryClient ibcratelimit.QueryClient
 }
 
 func (s *TestSuite) SetupTest() {
 	s.app = app.Setup(s.T())
 	s.ctx = s.app.BaseApp.NewContext(false, tmproto.Header{})
 	s.ctx = s.ctx.WithBlockHeight(0)
+
+	queryHelper := baseapp.NewQueryServerTestHelper(s.ctx, s.app.InterfaceRegistry())
+	ibcratelimit.RegisterQueryServer(queryHelper, s.app.RateLimitingKeeper)
+	s.queryClient = ibcratelimit.NewQueryClient(queryHelper)
 }
 
 func TestKeeperTestSuite(t *testing.T) {
 	suite.Run(t, new(TestSuite))
 }
 
-func (s *TestSuite) TestNewKeeper() {
-	tests := []struct {
-		name string
-	}{}
-
-	for _, tc := range tests {
-		s.Run(tc.name, func() {
-
-		})
-	}
-}
-
 func (s *TestSuite) TestGetSetParams() {
 	tests := []struct {
-		name string
-	}{}
+		name     string
+		contract string
+	}{
+		{
+			name: "success - get empty params",
+		},
+		{
+			name:     "success - set and get new params",
+			contract: "contractaddress",
+		},
+	}
 
 	for _, tc := range tests {
 		s.Run(tc.name, func() {
-
+			params := ibcratelimit.NewParams(tc.contract)
+			s.app.RateLimitingKeeper.SetParams(s.ctx, params)
+			newParams, err := s.app.RateLimitingKeeper.GetParams(s.ctx)
+			s.Assert().NoError(err)
+			s.Assert().Equal(params, newParams, "should have expected params")
 		})
 	}
 }
 
 func (s *TestSuite) TestGetContractAddress() {
 	tests := []struct {
-		name string
-	}{}
+		name     string
+		contract string
+	}{
+		{
+			name: "success - get empty contract",
+		},
+		{
+			name:     "success - set and get new contract address",
+			contract: "contractaddress",
+		},
+	}
 
 	for _, tc := range tests {
 		s.Run(tc.name, func() {
-
+			params := ibcratelimit.NewParams(tc.contract)
+			s.app.RateLimitingKeeper.SetParams(s.ctx, params)
+			contract := s.app.RateLimitingKeeper.GetContractAddress(s.ctx)
+			s.Assert().Equal(tc.contract, contract, "should have expected contract")
 		})
 	}
 }
 
 func (s *TestSuite) TestContractConfigured() {
 	tests := []struct {
-		name string
-	}{}
+		name     string
+		contract string
+		expected bool
+	}{
+		{
+			name:     "success - get empty contract",
+			expected: false,
+		},
+		{
+			name:     "success - set and get new contract address",
+			contract: "contractaddress",
+			expected: true,
+		},
+	}
 
 	for _, tc := range tests {
 		s.Run(tc.name, func() {
-
+			params := ibcratelimit.NewParams(tc.contract)
+			s.app.RateLimitingKeeper.SetParams(s.ctx, params)
+			configured := s.app.RateLimitingKeeper.ContractConfigured(s.ctx)
+			s.Assert().Equal(tc.expected, configured, "should have expected configured output")
 		})
 	}
 }
