be582958b3e35c6e306c12514787980468c66b71
Matthew Witkowski <mwitkowski@figure.com>
2023-11-06 11:30:41 -0500

    Add genesis sim tests.

--- x/ibcratelimit/simulation/genesis.go
+++ x/ibcratelimit/simulation/genesis.go
@@ -1,24 +1,61 @@
 package simulation
 
-// DONTCOVER
-
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 
 	"github.com/cosmos/cosmos-sdk/types/module"
+	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
 
 	"github.com/provenance-io/provenance/x/ibcratelimit"
 )
 
-// RandomizedGenState generates a random GenesisState for ibchooks
-func RandomizedGenState(simState *module.SimulationState) {
-	genesis := ibcratelimit.DefaultGenesis()
+// Simulation parameter constants
+const (
+	Contract = "contract"
+)
 
-	bz, err := json.MarshalIndent(genesis, "", " ")
+// ContractFn randomized conntract address
+func ContractFn(r *rand.Rand, accs []simtypes.Account) string {
+	randomAccount, _ := RandomAccs(r, accs, 1)
+	if r.Intn(2) > 0 || len(randomAccount) == 0 {
+		return ""
+	}
+	return randomAccount[0].Address.String()
+}
+
+// RandomizedGenState generates a random GenesisState for trigger
+func RandomizedGenState(simState *module.SimulationState) {
+	var contract string
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, Contract, &contract, simState.Rand,
+		func(r *rand.Rand) { contract = ContractFn(r, simState.Accounts) },
+	)
+
+	genesis := ibcratelimit.NewGenesisState(ibcratelimit.NewParams(contract))
+	simState.GenState[ibcratelimit.ModuleName] = simState.Cdc.MustMarshalJSON(genesis)
+
+	bz, err := json.MarshalIndent(simState.GenState[ibcratelimit.ModuleName], "", " ")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Selected randomly generated ibcratelimit parameters:\n%s\n", bz)
-	simState.GenState[ibcratelimit.ModuleName] = simState.Cdc.MustMarshalJSON(genesis)
+	fmt.Printf("Selected randomly generated ratelimitedibc parameters:\n%s\n", bz)
+}
+
+// randIntBetween generates a random number between min and max inclusive.
+func randIntBetween(r *rand.Rand, min, max int) int {
+	return r.Intn(max-min+1) + min
+}
+
+func RandomAccs(r *rand.Rand, accs []simtypes.Account, count uint64) ([]simtypes.Account, error) {
+	if uint64(len(accs)) < count {
+		return nil, fmt.Errorf("cannot choose %d accounts because there are only %d", count, len(accs))
+	}
+	raccs := make([]simtypes.Account, 0, len(accs))
+	raccs = append(raccs, accs...)
+	r.Shuffle(len(raccs), func(i, j int) {
+		raccs[i], raccs[j] = raccs[j], raccs[i]
+	})
+	return raccs[:count], nil
 }
