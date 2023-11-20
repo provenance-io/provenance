d0816736e6cae96301b5daae821c8c25fa3bb173
Daniel Wedul <github@wedul.com>
2023-11-20 16:26:55 -0700

    [1753]: Fix errors.go with a TODO for when it can be put back.

--- x/sanction/errors/errors.go
+++ x/sanction/errors/errors.go
@@ -1,15 +1,27 @@
 package errors
 
-import (
-	"cosmossdk.io/errors"
-)
-
-// sanctionCodespace is the codespace for all errors defined in sanction package
-const sanctionCodespace = "sanction"
+import sdkserrs "github.com/cosmos/cosmos-sdk/x/sanction/errors"
 
 var (
-	ErrInvalidParams      = errors.Register(sanctionCodespace, 2, "invalid params")
-	ErrUnsanctionableAddr = errors.Register(sanctionCodespace, 3, "address cannot be sanctioned")
-	ErrInvalidTempStatus  = errors.Register(sanctionCodespace, 4, "invalid temp status")
-	ErrSanctionedAccount  = errors.Register(sanctionCodespace, 5, "account is sanctioned")
+	ErrInvalidParams      = sdkserrs.ErrInvalidParams
+	ErrUnsanctionableAddr = sdkserrs.ErrUnsanctionableAddr
+	ErrInvalidTempStatus  = sdkserrs.ErrInvalidTempStatus
+	ErrSanctionedAccount  = sdkserrs.ErrSanctionedAccount
 )
+
+// TODO: Once we have an official version of the SDK without the sanction module:
+//       1. Delete everything above this comment.
+//       2. Uncomment everything below this comment.
+//       3. Delete this comment.
+
+// import cerrs "cosmossdk.io/errors"
+
+// // sanctionCodespace is the codespace for all errors defined in sanction package
+// const sanctionCodespace = "sanction"
+
+// var (
+// 	ErrInvalidParams      = cerrs.Register(sanctionCodespace, 2, "invalid params")
+// 	ErrUnsanctionableAddr = cerrs.Register(sanctionCodespace, 3, "address cannot be sanctioned")
+// 	ErrInvalidTempStatus  = cerrs.Register(sanctionCodespace, 4, "invalid temp status")
+// 	ErrSanctionedAccount  = cerrs.Register(sanctionCodespace, 5, "account is sanctioned")
+// )
