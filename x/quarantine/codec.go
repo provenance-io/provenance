77e8535994c0886504153a8d8016d867a7f8ac1c
Daniel Wedul <github@wedul.com>
2023-11-20 12:56:55 -0700

    [1752]: Clean up codec.go by getting rid of the amino codec stuff and switching to the allRequestMsgs pattern used in other modules.

--- x/quarantine/codec.go
+++ x/quarantine/codec.go
@@ -1,38 +1,19 @@
 package quarantine
 
 import (
-	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/gogo/protobuf/proto"
+
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
-	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
 func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
-	registry.RegisterImplementations((*sdk.Msg)(nil),
-		&MsgOptIn{},
-		&MsgOptOut{},
-		&MsgAccept{},
-		&MsgDecline{},
-		&MsgUpdateAutoResponses{},
-	)
+	messages := make([]proto.Message, len(allRequestMsgs))
+	for i, msg := range allRequestMsgs {
+		messages[i] = msg
+	}
+	registry.RegisterImplementations((*sdk.Msg)(nil), messages...)
 
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
-}
-
-var (
-	amino = codec.NewLegacyAmino()
-
-	// ModuleCdc references the global x/quarantine module codec. Note, the codec should
-	// ONLY be used in certain instances of tests and for JSON encoding as Amino is
-	// still used for that purpose.
-	//
-	// The actual codec used for serialization should be provided to x/quarantine and
-	// defined at the application level.
-	ModuleCdc = codec.NewAminoCodec(amino)
-)
-
-func init() {
-	cryptocodec.RegisterCrypto(amino)
-	sdk.RegisterLegacyAminoCodec(amino)
 }
