7d1bebe158198a25b9a3de2ceb7255b7dd5fbf4a
Daniel Wedul <github@wedul.com>
2023-11-20 12:21:59 -0700

    [1752]: Go back to importing our full SDK version and just make a note in error/errors.go for what to do later.

--- x/quarantine/errors/errors.go
+++ x/quarantine/errors/errors.go
@@ -1,8 +1,19 @@
 package errors
 
-import "cosmossdk.io/errors"
+import sdkqerrs "github.com/cosmos/cosmos-sdk/x/quarantine/errors"
 
-// quarantineCodespace is the codespace for all errors defined in quarantine package
-const quarantineCodespace = "quarantine"
+var ErrInvalidValue = sdkqerrs.ErrInvalidValue
 
-var ErrInvalidValue = errors.Register(quarantineCodespace, 2, "invalid value")
+// TODO: Once we have an official version of the SDK without the quarantine module:
+//       1. Delete everything above this comment.
+//       2. Uncomment everything below this comment.
+//       3. Delete this comment.
+
+// package errors
+//
+// import cerrs "cosmossdk.io/errors"
+//
+// // quarantineCodespace is the codespace for all errors defined in quarantine package
+// const quarantineCodespace = "quarantine"
+//
+// var ErrInvalidValue = cerrs.Register(quarantineCodespace, 2, "invalid value")
