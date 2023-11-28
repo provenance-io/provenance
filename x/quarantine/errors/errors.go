331aca2921dc67bcbdec95dcdfcabc64664b5ec7
Daniel Wedul <github@wedul.com>
2023-11-28 13:33:24 -0700

    [1752]: Tag 1760 in the TODO in errors.go.

--- x/quarantine/errors/errors.go
+++ x/quarantine/errors/errors.go
@@ -4,10 +4,10 @@ import sdkqerrs "github.com/cosmos/cosmos-sdk/x/quarantine/errors"
 
 var ErrInvalidValue = sdkqerrs.ErrInvalidValue
 
-// TODO: Once we have an official version of the SDK without the quarantine module:
-//       1. Delete everything above this comment.
-//       2. Uncomment everything below this comment.
-//       3. Delete this comment.
+// TODO[1760]: Once we have an official version of the SDK without the quarantine module:
+//             1. Delete everything above this comment.
+//             2. Uncomment everything below this comment.
+//             3. Delete this comment.
 
 // package errors
 //
