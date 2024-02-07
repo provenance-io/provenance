4df8fc7037286c63036cdea45374984fa8d78203
Daniel Wedul <github@wedul.com>
2024-02-07 12:33:19 -0700

    [1834]: Add context functions for passing in the transfer agent.

--- x/marker/types/send_restrictions.go
+++ x/marker/types/send_restrictions.go
@@ -2,7 +2,10 @@ package types
 
 import sdk "github.com/cosmos/cosmos-sdk/types"
 
-var bypassKey = "bypass-marker-restriction"
+var (
+	bypassKey        = "bypass-marker-restriction"
+	transferAgentKey = "marker-transfer-agent"
+)
 
 // WithBypass returns a new context that will cause the marker bank send restriction to be skipped.
 func WithBypass(ctx sdk.Context) sdk.Context {
@@ -22,4 +25,24 @@ func HasBypass(ctx sdk.Context) bool {
 	}
 	bypass, isBool := bypassValue.(bool)
 	return isBool && bypass
+}
+
+// WithTransferAgent returns a new context that contains the provided marker transfer agent.
+func WithTransferAgent(ctx sdk.Context, transferAgent sdk.AccAddress) sdk.Context {
+	return ctx.WithValue(transferAgentKey, transferAgent)
+}
+
+// WithoutTransferAgent returns a new context with a nil marker transfer agent.
+func WithoutTransferAgent(ctx sdk.Context) sdk.Context {
+	return ctx.WithValue(transferAgentKey, sdk.AccAddress(nil))
+}
+
+// GetTransferAgent gets the marker transfer agent from the provided context.
+func GetTransferAgent(ctx sdk.Context) sdk.AccAddress {
+	val := ctx.Value(transferAgentKey)
+	if val == nil {
+		return nil
+	}
+	rv, _ := val.(sdk.AccAddress)
+	return rv
 }
