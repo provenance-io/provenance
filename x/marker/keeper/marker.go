66a75d16d5f1be918d545751ae9ce38823b56977
Daniel Wedul <github@wedul.com>
2024-02-22 17:39:41 -0700

    Lint fix (needed extra empty line in keeper/marker.go).

--- x/marker/keeper/marker.go
+++ x/marker/keeper/marker.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	sdkmath "cosmossdk.io/math"
+
 	"github.com/cosmos/cosmos-sdk/telemetry"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
