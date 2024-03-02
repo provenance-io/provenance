1e8a9895e6c49a9c12750661fd1c1464e0501b2a
Daniel Wedul <github@wedul.com>
2024-03-01 19:29:50 -0700

    [1703]: Consume the commitment settlment fee only if the settlement succeeds.

--- x/exchange/keeper/msg_server.go
+++ x/exchange/keeper/msg_server.go
@@ -114,11 +114,11 @@ func (k MsgServer) MarketCommitmentSettle(goCtx context.Context, msg *exchange.M
 	if !k.CanSettleCommitments(ctx, msg.MarketId, msg.Admin) {
 		return nil, permError("settle commitments for", msg.Admin, msg.MarketId)
 	}
-	err := k.consumeCommitmentSettlementFee(ctx, msg)
+	err := k.SettleCommitments(ctx, msg)
 	if err != nil {
 		return nil, sdkerrors.ErrInvalidRequest.Wrap(err.Error())
 	}
-	err = k.SettleCommitments(ctx, msg)
+	err = k.consumeCommitmentSettlementFee(ctx, msg)
 	if err != nil {
 		return nil, sdkerrors.ErrInvalidRequest.Wrap(err.Error())
 	}
