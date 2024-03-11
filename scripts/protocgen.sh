623f2a61d9c2d4016bcfd33242398b4198f590c7
Matthew Witkowski <mwitkowski@provenance.io>
2024-03-11 15:00:57 -0400

    Remove pulsar until we need it.

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -21,21 +21,12 @@ proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1
 for dir in $proto_dirs; do
   proto_files=$(find "${dir}" -maxdepth 1 -name '*.proto')
   for file in $proto_files; do
-    # Check if the go_package in the file is pointing to evmos
+    # Check if the go_package in the file is pointing to provenance
     if grep -q "option go_package.*provenance" "$file"; then
       buf generate --template proto/buf.gen.gogo.yaml "$file"
     fi
   done
 done
-
-# command to generate docs using protoc-gen-doc
-# buf generate \
-#   -I "proto" \
-#   -I "third_party/proto" \
-#   --doc_out=./docs \
-#   --doc_opt=./docs/protodoc-markdown.tmpl,proto-docs.md \
-#   $(find "$(pwd)/proto" -maxdepth 5 -name '*.proto')
-
 
 # move proto files to the right places
 cp -r github.com/provenance-io/provenance/* ./
