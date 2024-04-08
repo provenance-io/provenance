229b09001cb7c72785a33aed2f205c481d88c7f5
Carlton N Hanna <nullpointer0x00@gmail.com>
2024-04-08 13:06:18 -0600

    removal of store in upgrade handler

--- app/upgrades.go
+++ app/upgrades.go
@@ -167,7 +167,8 @@ var upgrades = map[string]appUpgrade{
 		},
 	},
 	"umber-rc1": { // upgrade for v1.19.0-rc1
-		Added: []string{crisistypes.ModuleName},
+		Added:   []string{crisistypes.ModuleName},
+		Deleted: []string{"reward"},
 		Handler: func(ctx sdk.Context, app *App, vm module.VersionMap) (module.VersionMap, error) {
 			var err error
 
@@ -196,7 +197,8 @@ var upgrades = map[string]appUpgrade{
 		},
 	},
 	"umber": { // upgrade for v1.19.0
-		Added: []string{crisistypes.ModuleName},
+		Added:   []string{crisistypes.ModuleName},
+		Deleted: []string{"reward"},
 		Handler: func(ctx sdk.Context, app *App, vm module.VersionMap) (module.VersionMap, error) {
 			var err error
 
