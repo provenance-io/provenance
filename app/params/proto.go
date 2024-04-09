af579eacd0c6047264a033dd097e884bc786f7a4
Daniel Wedul <github@wedul.com>
2024-04-09 12:06:40 -0600

    [1901]: Add a TODO to the MakeTestEncodingConfig function to get rid of it.

--- app/params/proto.go
+++ app/params/proto.go
@@ -17,7 +17,13 @@ import (
 // MakeTestEncodingConfig creates an EncodingConfig for a non-amino based test configuration.
 // This function should be used only internally (in the SDK).
 // App user shouldn't create new codecs - use the app.AppCodec instead.
-// [DEPRECATED]
+//
+// TODO[1760]: Update all the simulation stuff that uses this to instead get what's needed from the SimState.
+// That will involve passing the SimSate into many places where we used to provide a codec.
+// Then delete this file and amino.go, and clean up stuff in the Makefile that uses the test_amino tag.
+//
+// Deprecated: Either get this from the app (app.GetEncodingConfig()) or use MakeTestEncodingConfig (from the app package),
+// or get what's needed from the SimSate.
 func MakeTestEncodingConfig() EncodingConfig {
 	cdc := amino.NewLegacyAmino()
 	signingOptions := signing.Options{
