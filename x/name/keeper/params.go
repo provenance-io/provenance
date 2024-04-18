5aaecbd09c3976d83baacb4f7297be5905ac92f1
Carlton Hanna <nullpointer0x00@gmail.com>
2024-04-18 11:34:37 -0600

    Name param store migration  (#1937)
    * remove param store, convert params to using module's store
    
    * rename default value variable, add params tests
    
    * add migration method, add remove todos
    
    * update comment for marker key, add migration log tests
    
    * add method calls to upgrades
    
    * change key name for each module

--- x/name/keeper/params.go
+++ x/name/keeper/params.go
@@ -2,57 +2,44 @@ package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-
 	"github.com/provenance-io/provenance/x/name/types"
 )
 
-// GetParams returns the total set of name parameters with fall thorugh to default values.
+// GetParams returns the total set of name parameters with fallback to default values.
 func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
-	return types.Params{
-		MaxSegmentLength:       k.GetMaxSegmentLength(ctx),
-		MinSegmentLength:       k.GetMinSegmentLength(ctx),
-		MaxNameLevels:          k.GetMaxNameLevels(ctx),
-		AllowUnrestrictedNames: k.GetAllowUnrestrictedNames(ctx),
+	store := ctx.KVStore(k.storeKey)
+	params = types.DefaultParams() // Assuming DefaultParams initializes all defaults
+
+	bz := store.Get(types.NameParamStoreKey) // General key for all parameters
+	if bz != nil {
+		k.cdc.MustUnmarshal(bz, &params) // Deserialize parameters from bytes
 	}
+	return params
 }
 
-// SetParams sets the distribution parameters to the param space.
+// SetParams sets the name parameters to the store.
 func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
-	k.paramSpace.SetParamSet(ctx, &params)
+	store := ctx.KVStore(k.storeKey)
+	bz := k.cdc.MustMarshal(&params) // Serialize parameters to bytes
+	store.Set(types.NameParamStoreKey, bz)
 }
 
-// GetMaxNameLevels returns the current maximum number of name segments allowed (or default if unset)
-func (k Keeper) GetMaxNameLevels(ctx sdk.Context) (max uint32) {
-	max = types.DefaultMaxSegments
-	if k.paramSpace.Has(ctx, types.ParamStoreKeyMaxNameLevels) {
-		k.paramSpace.Get(ctx, types.ParamStoreKeyMaxNameLevels, &max)
-	}
-	return
+// GetMaxNameLevels returns the current maximum number of name segments allowed.
+func (k Keeper) GetMaxNameLevels(ctx sdk.Context) uint32 {
+	return k.GetParams(ctx).MaxNameLevels
 }
 
-// GetMaxSegmentLength returns the current maximum length allowed for a name segment (or default if unset)
-func (k Keeper) GetMaxSegmentLength(ctx sdk.Context) (max uint32) {
-	max = types.DefaultMaxSegmentLength
-	if k.paramSpace.Has(ctx, types.ParamStoreKeyMaxSegmentLength) {
-		k.paramSpace.Get(ctx, types.ParamStoreKeyMaxSegmentLength, &max)
-	}
-	return
+// GetMaxSegmentLength returns the current maximum length allowed for a name segment.
+func (k Keeper) GetMaxSegmentLength(ctx sdk.Context) uint32 {
+	return k.GetParams(ctx).MaxSegmentLength
 }
 
-// GetMinSegmentLength returns the current minimum allowed name segment length (or default if unset)
-func (k Keeper) GetMinSegmentLength(ctx sdk.Context) (min uint32) {
-	min = types.DefaultMinSegmentLength
-	if k.paramSpace.Has(ctx, types.ParamStoreKeyMinSegmentLength) {
-		k.paramSpace.Get(ctx, types.ParamStoreKeyMinSegmentLength, &min)
-	}
-	return
+// GetMinSegmentLength returns the current minimum allowed name segment length.
+func (k Keeper) GetMinSegmentLength(ctx sdk.Context) uint32 {
+	return k.GetParams(ctx).MinSegmentLength
 }
 
-// GetAllowUnrestrictedNames returns the current unrestricted names allowed parameter (or default if unset)
-func (k Keeper) GetAllowUnrestrictedNames(ctx sdk.Context) (enabled bool) {
-	enabled = types.DefaultAllowUnrestrictedNames
-	if k.paramSpace.Has(ctx, types.ParamStoreKeyAllowUnrestrictedNames) {
-		k.paramSpace.Get(ctx, types.ParamStoreKeyAllowUnrestrictedNames, &enabled)
-	}
-	return
+// GetAllowUnrestrictedNames returns whether unrestricted names are allowed.
+func (k Keeper) GetAllowUnrestrictedNames(ctx sdk.Context) bool {
+	return k.GetParams(ctx).AllowUnrestrictedNames
 }
