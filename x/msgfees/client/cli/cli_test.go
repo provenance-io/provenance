1f118c7bf467d77012052651a8acdb2ce1068c74
Carlton N Hanna <nullpointer0x00@gmail.com>
2024-05-13 12:56:01 -0600

    add back proposal interfaces impls

--- x/msgfees/client/cli/cli_test.go
+++ x/msgfees/client/cli/cli_test.go
@@ -1,6 +1,7 @@
 package cli_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -8,13 +9,22 @@ import (
 
 	sdkmath "cosmossdk.io/math"
 
+	cmtcli "github.com/cometbft/cometbft/libs/cli"
+
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/crypto/hd"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	testnet "github.com/cosmos/cosmos-sdk/testutil/network"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	govv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 
 	"github.com/provenance-io/provenance/internal/pioconfig"
 	"github.com/provenance-io/provenance/testutil"
+	testcli "github.com/provenance-io/provenance/testutil/cli"
+	"github.com/provenance-io/provenance/x/msgfees/client/cli"
 	"github.com/provenance-io/provenance/x/msgfees/types"
 )
 
@@ -30,6 +40,11 @@ type IntegrationTestSuite struct {
 	account2Addr  sdk.AccAddress
 	account2Key   *secp256k1.PrivKey
 	acc2NameCount int
+
+	accountAddresses []sdk.AccAddress
+
+	keyring    keyring.Keyring
+	keyringDir string
 }
 
 func TestIntegrationTestSuite(t *testing.T) {
@@ -55,6 +70,31 @@ func (s *IntegrationTestSuite) SetupSuite() {
 	s.cfg = testutil.DefaultTestNetworkConfig()
 	s.cfg.TimeoutCommit = 500 * time.Millisecond
 	s.cfg.NumValidators = 1
+	s.GenerateAccountsWithKeyrings(1)
+
+	var genBalances []banktypes.Balance
+	for i := range s.accountAddresses {
+		genBalances = append(genBalances, banktypes.Balance{Address: s.accountAddresses[i].String(), Coins: sdk.NewCoins(
+			sdk.NewInt64Coin("nhash", 100_000_000), sdk.NewInt64Coin(s.cfg.BondDenom, 100_000_000),
+		).Sort()})
+	}
+	var bankGenState banktypes.GenesisState
+	bankGenState.Params = banktypes.DefaultParams()
+	bankGenState.Balances = genBalances
+	bankDataBz, err := s.cfg.Codec.MarshalJSON(&bankGenState)
+	s.Require().NoError(err, "should be able to marshal bank genesis state when setting up suite")
+	s.cfg.GenesisState[banktypes.ModuleName] = bankDataBz
+
+	var authData authtypes.GenesisState
+	var genAccounts []authtypes.GenesisAccount
+	authData.Params = authtypes.DefaultParams()
+	genAccounts = append(genAccounts, authtypes.NewBaseAccount(s.accountAddresses[0], nil, 3, 0))
+	accounts, err := authtypes.PackAccounts(genAccounts)
+	s.Require().NoError(err, "should be able to pack accounts for genesis state when setting up suite")
+	authData.Accounts = accounts
+	authDataBz, err := s.cfg.Codec.MarshalJSON(&authData)
+	s.Require().NoError(err, "should be able to marshal auth genesis state when setting up suite")
+	s.cfg.GenesisState[authtypes.ModuleName] = authDataBz
 
 	var msgfeeGen types.GenesisState
 	err = s.cfg.Codec.UnmarshalJSON(s.cfg.GenesisState[types.ModuleName], &msgfeeGen)
@@ -73,8 +113,96 @@ func (s *IntegrationTestSuite) SetupSuite() {
 	s.Require().NoError(err, "waiting for height 1")
 }
 
+func (s *IntegrationTestSuite) GenerateAccountsWithKeyrings(number int) {
+	path := hd.CreateHDPath(118, 0, 0).String()
+	s.keyringDir = s.T().TempDir()
+	kr, err := keyring.New(s.T().Name(), "test", s.keyringDir, nil, s.cfg.Codec)
+	s.Require().NoError(err, "Keyring.New")
+	s.keyring = kr
+	for i := 0; i < number; i++ {
+		keyId := fmt.Sprintf("test_key%v", i)
+		info, _, err := kr.NewMnemonic(keyId, keyring.English, path, keyring.DefaultBIP39Passphrase, hd.Secp256k1)
+		s.Require().NoError(err, "Keyring.NewMneomonic")
+		addr, err := info.GetAddress()
+		if err != nil {
+			panic(err)
+		}
+		s.accountAddresses = append(s.accountAddresses, addr)
+	}
+}
+
 func (s *IntegrationTestSuite) TearDownSuite() {
 	testutil.Cleanup(s.testnet, s.T())
+}
+
+func (s *IntegrationTestSuite) TestMsgFeesProposal() {
+	testCases := []struct {
+		name         string
+		args         []string
+		expectErrMsg string
+		expectedCode uint32
+		signer       string
+	}{
+		{
+			name: "success - add msg fee",
+			args: []string{
+				"add",
+				"--msg-type=/provenance.metadata.v1.MsgWriteRecordRequest",
+				"--additional-fee=612nhash",
+			},
+			expectedCode: 0,
+			signer:       s.accountAddresses[0].String(),
+		},
+		{
+			name: "success - update msg fee",
+			args: []string{
+				"update",
+				"--msg-type=/provenance.metadata.v1.MsgWriteRecordRequest",
+				"--additional-fee=612000nhash",
+			},
+			expectedCode: 0,
+			signer:       s.accountAddresses[0].String(),
+		},
+		{
+			name: "success - remove msg fee",
+			args: []string{
+				"remove",
+				"--msg-type=/provenance.metadata.v1.MsgWriteRecordRequest",
+			},
+			expectedCode: 0,
+			signer:       s.accountAddresses[0].String(),
+		},
+		{
+			name:         "failure - invalid number of args",
+			args:         []string{"add", "extra-arg"},
+			expectErrMsg: "accepts 1 arg(s), received 2",
+			signer:       s.accountAddresses[0].String(),
+		},
+		{
+			name:         "failure - invalid proposal type",
+			args:         []string{"invalid-type"},
+			expectErrMsg: "unknown proposal type",
+			signer:       s.accountAddresses[0].String(),
+		},
+	}
+
+	for _, tc := range testCases {
+		s.Run(tc.name, func() {
+			cmd := cli.GetCmdMsgFeesProposal()
+			tc.args = append(tc.args,
+				fmt.Sprintf("--%s=%s", flags.FlagFrom, tc.signer),
+				fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+				fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastSync),
+				fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewInt64Coin(s.cfg.BondDenom, 10)).String()),
+				fmt.Sprintf("--%s=json", cmtcli.OutputFlag),
+			)
+
+			testcli.NewCLITxExecutor(cmd, tc.args).
+				WithExpErrMsg(tc.expectErrMsg).
+				WithExpCode(tc.expectedCode).
+				Execute(s.T(), s.testnet)
+		})
+	}
 }
 
 // TODO: Add query tests
---
+++ x/msgfees/types/proposal.go
@@ -0,0 +1,70 @@
+package types
+
+import (
+	"errors"
+
+	govtypesv1beta1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
+)
+
+const (
+	// ProposalTypeAddMsgFee to add a new msg based fee
+	ProposalTypeAddMsgFee string = "AddMsgFee"
+	// ProposalTypeUpdateMsgFee to update an existing msg based fee
+	ProposalTypeUpdateMsgFee string = "UpdateMsgFee"
+	// ProposalTypeRemoveMsgFee to remove an existing msg based fee
+	ProposalTypeRemoveMsgFee string = "RemoveMsgFee"
+	// ProposalTypeUpdateUsdConversionRate to update the usd conversion rate param
+	ProposalTypeUpdateUsdConversionRate string = "UpdateUsdConversionRate"
+	// ProposalTypeUpdateConversionFeeDenom to update the conversion rate denom
+	ProposalTypeUpdateConversionFeeDenom string = "UpdateConversionFeeDenom"
+)
+
+var (
+	_ govtypesv1beta1.Content = &AddMsgFeeProposal{}
+	_ govtypesv1beta1.Content = &UpdateMsgFeeProposal{}
+	_ govtypesv1beta1.Content = &RemoveMsgFeeProposal{}
+	_ govtypesv1beta1.Content = &UpdateNhashPerUsdMilProposal{}
+	_ govtypesv1beta1.Content = &UpdateConversionFeeDenomProposal{}
+)
+
+func (p AddMsgFeeProposal) ProposalRoute() string { return RouterKey }
+func (p AddMsgFeeProposal) ProposalType() string  { return ProposalTypeAddMsgFee }
+func (p AddMsgFeeProposal) ValidateBasic() error {
+	return errors.New("proposal msg no longer supported")
+}
+
+func (p UpdateMsgFeeProposal) ProposalRoute() string { return RouterKey }
+
+func (p UpdateMsgFeeProposal) ProposalType() string { return ProposalTypeUpdateMsgFee }
+
+func (p UpdateMsgFeeProposal) ValidateBasic() error {
+	return errors.New("proposal msg no longer supported")
+}
+
+func (p RemoveMsgFeeProposal) ProposalRoute() string { return RouterKey }
+
+func (p RemoveMsgFeeProposal) ProposalType() string { return ProposalTypeRemoveMsgFee }
+
+func (p RemoveMsgFeeProposal) ValidateBasic() error {
+	return errors.New("proposal msg no longer supported")
+}
+
+func (p UpdateNhashPerUsdMilProposal) ProposalRoute() string { return RouterKey }
+
+func (p UpdateNhashPerUsdMilProposal) ProposalType() string {
+	return ProposalTypeUpdateUsdConversionRate
+}
+
+func (p UpdateNhashPerUsdMilProposal) ValidateBasic() error {
+	return errors.New("proposal msg no longer supported")
+}
+
+func (p UpdateConversionFeeDenomProposal) ProposalRoute() string { return RouterKey }
+
+func (p UpdateConversionFeeDenomProposal) ProposalType() string {
+	return ProposalTypeUpdateConversionFeeDenom
+}
+
+func (p UpdateConversionFeeDenomProposal) ValidateBasic() error {
+	return errors.New("proposal msg no longer supported")
+}
