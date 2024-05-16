778a0ea6ba0521baf8c44032d9617d06cf963614
Carlton N Hanna <nullpointer0x00@gmail.com>
2024-05-16 11:42:29 -0600

    add back codec for deprecated propopsals, fix error messages in msgfees to match

--- x/marker/types/codec.go
+++ x/marker/types/codec.go
@@ -6,6 +6,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/cosmos/cosmos-sdk/x/authz"
+	govtypesv1beta1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
 	"github.com/cosmos/gogoproto/proto"
 )
 
@@ -14,6 +15,18 @@ func RegisterInterfaces(registry types.InterfaceRegistry) {
 	messages := make([]proto.Message, len(AllRequestMsgs))
 	copy(messages, AllRequestMsgs)
 	registry.RegisterImplementations((*sdk.Msg)(nil), messages...)
+
+	registry.RegisterImplementations(
+		(*govtypesv1beta1.Content)(nil),
+		&AddMarkerProposal{},
+		&SupplyIncreaseProposal{},
+		&SupplyDecreaseProposal{},
+		&SetAdministratorProposal{},
+		&RemoveAdministratorProposal{},
+		&ChangeStatusProposal{},
+		&WithdrawEscrowProposal{},
+		&SetDenomMetadataProposal{},
+	)
 
 	registry.RegisterImplementations(
 		(*authz.Authorization)(nil),
