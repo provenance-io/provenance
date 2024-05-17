08bf5994628484d9816126d09c92820a829146fd
Daniel Wedul <github@wedul.com>
2024-05-17 13:42:19 -0600

    [1760]: Remove the rosetta stuff.

--- client/rosetta/configuration/send_funds.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -e
-addr=$(provenanced -t keys show node0 -a --keyring-backend=test --home=/testrosetta/node0)
-echo "12345678" | provenanced -t tx bank send "$addr" "$1" 100000000000nhash --chain-id="chain-local" --node tcp://provenance:26657 --yes --keyring-backend=test  --home=/testrosetta/node0 --gas-prices="1905nhash" --gas=auto --gas-adjustment=1.5
