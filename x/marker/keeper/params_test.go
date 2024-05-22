257f19857da11ceefd1a1edfa1ea9db54b982b78
Carlton N Hanna <nullpointer0x00@gmail.com>
2024-05-22 14:54:39 -0600

    remove testing of deprecated value

--- x/marker/keeper/params_test.go
+++ x/marker/keeper/params_test.go
@@ -38,13 +38,11 @@ func (s *ParamTestSuite) TestGetSetParams() {
 	s.Require().Equal(types.DefaultUnrestrictedDenomRegex, defaultParams.UnrestrictedDenomRegex, "Default UnrestrictedDenomRegex should match")
 	s.Require().Equal(types.StringToBigInt(types.DefaultMaxSupply), defaultParams.MaxSupply, "Default MaxSupply should match")
 
-	newMaxTotalSupply := uint64(2000000)
 	newEnableGovernance := false
 	newUnrestrictedDenomRegex := "xyz.*"
 	newMaxSupply := "3000000"
 
 	newParams := types.Params{
-		MaxTotalSupply:         newMaxTotalSupply,
 		EnableGovernance:       newEnableGovernance,
 		UnrestrictedDenomRegex: newUnrestrictedDenomRegex,
 		MaxSupply:              types.StringToBigInt(newMaxSupply),
@@ -53,7 +51,6 @@ func (s *ParamTestSuite) TestGetSetParams() {
 	s.app.MarkerKeeper.SetParams(s.ctx, newParams)
 
 	updatedParams := s.app.MarkerKeeper.GetParams(s.ctx)
-	s.Require().Equal(newMaxTotalSupply, updatedParams.MaxTotalSupply, "Updated MaxTotalSupply should match")
 	s.Require().Equal(newEnableGovernance, updatedParams.EnableGovernance, "Updated EnableGovernance should match")
 	s.Require().Equal(newUnrestrictedDenomRegex, updatedParams.UnrestrictedDenomRegex, "Updated UnrestrictedDenomRegex should match")
 	s.Require().Equal(types.StringToBigInt(newMaxSupply), updatedParams.MaxSupply, "Updated MaxSupply should match")
