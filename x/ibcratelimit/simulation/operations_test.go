038f9222f97a735c0eb3105e4299288e1c596f42
Daniel Wedul <github@wedul.com>
2024-05-22 18:31:10 -0600

    [1760]: Switch the ibcratelimit sim tests to the new helpers.

--- x/ibcratelimit/simulation/operations_test.go
+++ x/ibcratelimit/simulation/operations_test.go
@@ -1,10 +1,7 @@
 package simulation_test
 
 import (
-	"bytes"
-	"fmt"
 	"math/rand"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -12,9 +9,10 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
-	"github.com/cosmos/cosmos-sdk/x/bank/testutil"
+
 	"github.com/provenance-io/provenance/app"
 	simappparams "github.com/provenance-io/provenance/app/params"
+	"github.com/provenance-io/provenance/testutil"
 	"github.com/provenance-io/provenance/x/ibcratelimit"
 
 	. "github.com/provenance-io/provenance/x/ibcratelimit/simulation"
@@ -36,25 +34,6 @@ func (s *SimTestSuite) SetupTest() {
 	s.ctx = s.app.BaseApp.NewContext(false)
 }
 
-// LogOperationMsg logs all fields of the provided operationMsg.
-func (s *SimTestSuite) LogOperationMsg(operationMsg simtypes.OperationMsg, msg string, args ...interface{}) {
-	msgFmt := "%s"
-	if len(bytes.TrimSpace(operationMsg.Msg)) == 0 {
-		msgFmt = "    %q"
-	}
-	fmtLines := []string{
-		fmt.Sprintf(msg, args...),
-		"operationMsg.Route:   %q",
-		"operationMsg.Name:    %q",
-		"operationMsg.Comment: %q",
-		"operationMsg.OK:      %t",
-		"operationMsg.Msg: " + msgFmt,
-	}
-	s.T().Logf(strings.Join(fmtLines, "\n"),
-		operationMsg.Route, operationMsg.Name, operationMsg.Comment, operationMsg.OK, string(operationMsg.Msg),
-	)
-}
-
 // MakeTestSimState creates a new module.SimulationState struct with the fields needed by the functions being tested.
 func (s *SimTestSuite) MakeTestSimState() module.SimulationState {
 	return module.SimulationState{
@@ -64,54 +43,28 @@ func (s *SimTestSuite) MakeTestSimState() module.SimulationState {
 	}
 }
 
+func (s *SimTestSuite) NewSimTestHelper(accCount int) *testutil.SimTestHelper {
+	return testutil.NewSimTestHelper(s.T(), s.ctx, rand.New(rand.NewSource(1)), s.app).
+		WithTestingAccounts(accCount)
+}
+
 func (s *SimTestSuite) TestProposalMsgs() {
-	expected := []struct {
-		key     string
-		weight  int
-		msgType sdk.Msg
-	}{
+	expected := []testutil.ExpectedProposalMsg{
 		{
-			key:     OpWeightMsgUpdateParams,
-			weight:  simappparams.DefaultWeightIBCRLUpdateParams,
-			msgType: &ibcratelimit.MsgUpdateParamsRequest{},
+			Key:     OpWeightMsgUpdateParams,
+			Weight:  simappparams.DefaultWeightIBCRLUpdateParams,
+			MsgType: &ibcratelimit.MsgUpdateParamsRequest{},
 		},
 	}
 
 	simState := s.MakeTestSimState()
-	var propMsgs []simtypes.WeightedProposalMsg
-	testGetPropMsgs := func() {
-		propMsgs = ProposalMsgs(simState, s.app.RateLimitingKeeper)
+	proposalMsgsFn := func() []simtypes.WeightedProposalMsg {
+		return ProposalMsgs(simState, s.app.RateLimitingKeeper)
 	}
-	s.Require().NotPanics(testGetPropMsgs, "ProposalMsgs")
-	s.Require().Len(propMsgs, len(expected), "ProposalMsgs")
-
-	r := rand.New(rand.NewSource(1))
-	accs := s.getTestingAccounts(r, 10)
-
-	for i, propMsg := range propMsgs {
-		exp := expected[i]
-		s.Run(exp.key, func() {
-			expMsgType := fmt.Sprintf("%T", exp.msgType)
-
-			s.Assert().Equal(exp.key, propMsg.AppParamsKey(), "AppParamsKey()")
-			s.Assert().Equal(exp.weight, propMsg.DefaultWeight(), "DefaultWeight()")
-			s.Require().NotNil(propMsg.MsgSimulatorFn(), "MsgSimulatorFn()")
-
-			var msg sdk.Msg
-			testPropMsg := func() {
-				msg = propMsg.MsgSimulatorFn()(r, s.ctx, accs)
-			}
-			s.Require().NotPanics(testPropMsg, "calling the propMsg.MsgSimulatorFn()")
-			s.Require().NotNil(msg, "msg result")
-			actMsgType := fmt.Sprintf("%T", msg)
-			s.Assert().Equal(expMsgType, actMsgType, "msg result")
-			s.Assert().IsType(exp.msgType, msg, "msg")
-		})
-	}
+	s.NewSimTestHelper(10).AssertProposalMsgs(expected, proposalMsgsFn)
 }
 
 func (s *SimTestSuite) TestSimulatePropMsgUpdateOracle() {
-	r := rand.New(rand.NewSource(1))
 	expMsg := &ibcratelimit.MsgUpdateParamsRequest{
 		Authority: s.app.OracleKeeper.GetAuthority(),
 		Params: ibcratelimit.Params{
@@ -119,25 +72,16 @@ func (s *SimTestSuite) TestSimulatePropMsgUpdateOracle() {
 		},
 	}
 
-	var msgSimFn simtypes.MsgSimulatorFn
-	testFnMaker := func() {
-		msgSimFn = SimulatePropMsgUpdateParams(s.app.RateLimitingKeeper)
+	simFnMaker := func() simtypes.MsgSimulatorFn {
+		return SimulatePropMsgUpdateParams(s.app.RateLimitingKeeper)
 	}
-	s.Require().NotPanics(testFnMaker, "SimulatePropMsgUpdateParams")
-	s.Require().NotNil(msgSimFn, "SimulatePropMsgUpdateParams resulting MsgSimulationFn")
-
-	var actMsg sdk.Msg
-	testSimFn := func() {
-		actMsg = msgSimFn(r, s.ctx, nil)
-	}
-	s.Require().NotPanics(testSimFn, "executing the SimulatePropMsgUpdateParams resulting MsgSimulationFn")
-	s.Assert().Equal(expMsg, actMsg)
+	s.NewSimTestHelper(0).AssertMsgSimulatorFn(expMsg, simFnMaker)
 }
 
 func (s *SimTestSuite) TestRandomAccs() {
 	source := rand.NewSource(1)
 	r := rand.New(source)
-	accounts := s.getTestingAccounts(r, 3)
+	accounts := testutil.GenerateTestingAccounts(s.T(), s.ctx, s.app, r, 3)
 
 	tests := []struct {
 		name     string
@@ -190,21 +134,4 @@ func (s *SimTestSuite) TestRandomAccs() {
 			}
 		})
 	}
-}
-
-func (s *SimTestSuite) getTestingAccounts(r *rand.Rand, n int) []simtypes.Account {
-	accounts := simtypes.RandomAccounts(r, n)
-
-	initAmt := sdk.TokensFromConsensusPower(1000000, sdk.DefaultPowerReduction)
-	initCoins := sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, initAmt))
-
-	// add coins to the accounts
-	for _, account := range accounts {
-		acc := s.app.AccountKeeper.NewAccountWithAddress(s.ctx, account.Address)
-		s.app.AccountKeeper.SetAccount(s.ctx, acc)
-		err := testutil.FundAccount(s.ctx, s.app.BankKeeper, account.Address, initCoins)
-		s.Require().NoError(err)
-	}
-
-	return accounts
 }
