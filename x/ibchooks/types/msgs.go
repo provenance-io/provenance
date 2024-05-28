0a0ed47f504734d11fef116579021e741e6b3702
Carlton N Hanna <nullpointer0x00@gmail.com>
2024-05-28 10:37:39 -0600

    add msg validate basic, add tests

--- x/ibchooks/types/msgs.go
+++ x/ibchooks/types/msgs.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	fmt "fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -12,4 +14,29 @@ var AllRequestMsgs = []sdk.Msg{
 func (m MsgEmitIBCAck) ValidateBasic() error {
 	_, err := sdk.AccAddressFromBech32(m.Sender)
 	return err
+}
+
+// NewMsgUpdateParamsRequest creates a new MsgUpdateParamsRequest instance
+func NewMsgUpdateParamsRequest(allowedAsyncAckContracts []string, authority string) *MsgUpdateParamsRequest {
+	return &MsgUpdateParamsRequest{
+		Params: Params{
+			AllowedAsyncAckContracts: allowedAsyncAckContracts,
+		},
+		Authority: authority,
+	}
+}
+
+// ValidateBasic implements the sdk.Msg interface.
+func (msg MsgUpdateParamsRequest) ValidateBasic() error {
+	for _, contract := range msg.Params.AllowedAsyncAckContracts {
+		if _, err := sdk.AccAddressFromBech32(contract); err != nil {
+			return fmt.Errorf("invalid contract address: %s", contract)
+		}
+	}
+
+	if _, err := sdk.AccAddressFromBech32(msg.Authority); err != nil {
+		return fmt.Errorf("invalid authority address: %s", msg.Authority)
+	}
+
+	return nil
 }
--- x/ibchooks/types/msgs_test.go
+++ x/ibchooks/types/msgs_test.go
@@ -5,7 +5,9 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/provenance-io/provenance/testutil"
+	"github.com/stretchr/testify/require"
 
+	"github.com/provenance-io/provenance/x/ibchooks/types"
 	. "github.com/provenance-io/provenance/x/ibchooks/types"
 )
 
@@ -15,4 +17,47 @@ func TestAllMsgsGetSigners(t *testing.T) {
 	}
 
 	testutil.RunGetSignersTests(t, AllRequestMsgs, msgMakers, nil)
+}
+
+func TestNewMsgUpdateParamsRequest(t *testing.T) {
+	authority := "cosmos1vh3htvc46rshps02w0p5hchdkrjvc4d8nxkw5t"
+	validContract := "cosmos1vh3htvc46rshps02w0p5hchdkrjvc4d8nxkw5t"
+	invalidContract := "invalid_contract"
+
+	tests := []struct {
+		name      string
+		contracts []string
+		authority string
+		expErr    string
+	}{
+		{
+			name:      "valid request",
+			contracts: []string{validContract, validContract},
+			authority: authority,
+		},
+		{
+			name:      "invalid contract address",
+			contracts: []string{validContract, invalidContract},
+			authority: authority,
+			expErr:    "invalid contract address: invalid_contract",
+		},
+		{
+			name:      "invalid authority address",
+			contracts: []string{validContract, validContract},
+			authority: "invalid_authority",
+			expErr:    "invalid authority address: invalid_authority",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			msg := types.NewMsgUpdateParamsRequest(tc.contracts, tc.authority)
+			err := msg.ValidateBasic()
+			if tc.expErr != "" {
+				require.EqualError(t, err, tc.expErr, "MsgUpdateParamsRequest.ValidateBasic expected error message: %s, but got: %s", tc.expErr, err)
+			} else {
+				require.NoError(t, err, "MsgUpdateParamsRequest.ValidateBasic expected no error, but got: %s", err)
+			}
+		})
+	}
 }
