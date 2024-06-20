56503dc8660dc2da55f32890deab402eb28d3069
Carlton N Hanna <nullpointer0x00@gmail.com>
2024-06-20 17:18:36 -0600

    fix tests to match cvs

--- app/scope_navs_updater_test.go
+++ app/scope_navs_updater_test.go
@@ -18,7 +18,7 @@ func TestReadNetAssetValues(t *testing.T) {
 	expectedFirst := NetAssetValueWithHeight{
 		ScopeUUID:     "2f389a9f-873d-4920-85a6-7734f27e1738",
 		NetAssetValue: metadatatypes.NewNetAssetValue(sdk.NewInt64Coin(metadatatypes.UsdDenom, 398820670)),
-		Height:        406,
+		Height:        23056719,
 	}
 	assert.Equal(t, expectedFirst.ScopeUUID, assets[0].ScopeUUID, "The first ScopeUUID should match")
 	assert.True(t, assets[0].NetAssetValue.Price.Equal(expectedFirst.NetAssetValue.Price), "The first NetAssetValue should match")
@@ -27,7 +27,7 @@ func TestReadNetAssetValues(t *testing.T) {
 	expectedLast := NetAssetValueWithHeight{
 		ScopeUUID:     "65939db0-6d7a-42ef-9443-378304d33225",
 		NetAssetValue: metadatatypes.NewNetAssetValue(sdk.NewInt64Coin(metadatatypes.UsdDenom, 93661920)),
-		Height:        406,
+		Height:        23056719,
 	}
 	assert.Equal(t, expectedLast.ScopeUUID, assets[len(assets)-1].ScopeUUID, "The last ScopeUUID should match")
 	assert.True(t, assets[len(assets)-1].NetAssetValue.Price.Equal(expectedLast.NetAssetValue.Price), "The last NetAssetValue should match")
