9f62c9ac38f5b825f5cf3038ea9e1773c5ac3fb3
kwt <4344285+kwtalley@users.noreply.github.com>
2024-06-20 16:36:52 -0500

    change provwasm stargate query plugin to use protocodec (#2034)
    * change provwasm stargate query plugin to use protocodec with WasmAny type
    
    * fix lint issue
    
    * fix imports lint issue
    
    * update cosmwasm capabilities to 1_4
    
    * add lint ignore to function
    
    * fix lint newline
    
    * add previous cosmwasm release capabilities
    
    ---------
    
    Co-authored-by: Ira Miller <72319+iramiller@users.noreply.github.com>

---
+++ x/wasm/types/interface_registry.go
@@ -0,0 +1,23 @@
+package types
+
+import (
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/gogoproto/proto"
+)
+
+var _ codectypes.InterfaceRegistry = &WasmInterfaceRegistry{}
+
+// NewWasmInterfaceRegistry returns a new WasmInterfaceRegistry instance
+func NewWasmInterfaceRegistry(registry codectypes.InterfaceRegistry) WasmInterfaceRegistry {
+	return WasmInterfaceRegistry{registry}
+}
+
+// WasmInterfaceRegistry represents an interface registry with a custom any resolver
+type WasmInterfaceRegistry struct {
+	codectypes.InterfaceRegistry
+}
+
+// Resolve implements codectypes.InterfaceRegistry
+func (WasmInterfaceRegistry) Resolve(_ string) (proto.Message, error) {
+	return new(WasmAny), nil
+}
