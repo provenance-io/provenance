2a7fb0f76102ef920bd76c95fffefdb3b2b90276
Daniel Wedul <github@wedul.com>
2024-06-27 16:31:42 -0600

    Add stuff that identifies all the needed proto files and copies/downloads them into a temp dir.

--- scripts/update-spec-links.sh
+++ scripts/update-spec-links.sh
@@ -67,6 +67,9 @@ while [[ "$#" -gt '0' ]]; do
     --not-verbose)
       verbose=''
       ;;
+    --no-clean|--no-cleanup)
+      no_clean='YES'
+      ;;
     *)
       if [[ -z "$ref" ]]; then
         ref="$1"
@@ -107,15 +110,22 @@ if [[ -n "$verbose" ]]; then
   fi
 fi
 
+########################################################################################################################
+########################################  Identify files that will be updated.  ########################################
+########################################################################################################################
+
+link_rx='^\+\+\+ https://github\.com/provenance-io/provenance'
 declare files=()
 
 if [[ "${#files_in[@]}" -eq '0' && "${#dirs_in[@]}" -eq '0' ]]; then
   dirs_in+=( '.' )
 fi
 
+i=0
 for file in "${files_in[@]}"; do
-  [[ -n "$verbose" ]] && printf 'Checking file: %s ... ' "$file"
-  if grep -E --no-messages --binary-file=without-match --silent '^\+\+\+ ' "$file" > /dev/null 2>&1; then
+  i=$(( i + 1 ))
+  [[ -n "$verbose" ]] && printf '[%d/%d]: Checking file: %s ... ' "$i" "${#files_in[@]}" "$file"
+  if grep -E --no-messages --binary-file=without-match --silent "$link_rx" "$file" > /dev/null 2>&1; then
     files+=( "$file" )
     [[ -n "$verbose" ]] && printf 'File has links.\n'
   else
@@ -127,16 +137,20 @@ for file in "${files_in[@]}"; do
   fi
 done
 
+i=0
 for folder in "${dirs_in[@]}"; do
-  [[ -n "$verbose" ]] && printf 'Checking directory for files with links: %s ... ' "$folder"
-  set -o noglob
+  i=$(( i + 1 ))
+  [[ -n "$verbose" ]] && printf '[%d/%d]: Checking directory for files with links: %s ... ' "$i" "${#dirs_in[@]}" "$folder"
   declare new_files=()
-  IFS=$'\n' new_files+=( $( grep -E --files-with-matches --recursive --no-messages --binary-file=without-match '^\+\+\+ ' "$folder" | sort ) )
+  set -o noglob
+  IFS=$'\n' new_files+=( $( grep -E --files-with-matches --recursive --no-messages --binary-file=without-match "$link_rx" "$folder" | sort ) )
   set +o noglob
   if [[ "${#new_files[@]}" -ne '0' ]]; then
     [[ -n "$verbose" ]] && printf 'Found %d file(s) with links:\n' "${#new_files[@]}"
+    j=0
     for file in "${new_files[@]}"; do
-      [[ -n "$verbose" ]] && printf '  %s' "$file"
+      j=$(( j + 1 ))
+      [[ -n "$verbose" ]] && printf '[%d/%d|%d/%d]: %s' "$i" "${#dirs_in[@]}" "$j" "${#new_files[@]}" "$file"
       if [[ "$file" =~ \.md$ ]]; then
         files+=( "$file" )
         [[ -n "$verbose" ]] && printf '\n'
@@ -153,11 +167,106 @@ for folder in "${dirs_in[@]}"; do
   fi
 done
 
-file_count="${#files[@]}"
-
-if [[ "$file_count" -eq '0' ]]; then
+if [[ "${#files[@]}" -eq '0' ]]; then
   printf 'No files found to update.\n'
   exit 0
 fi
 
-[[ -z "$quiet" ]] && printf 'Updating links in %d file(s).\n' "$file_count"
+[[ -z "$quiet" ]] && printf 'Updating links in %d file(s).\n' "${#files[@]}"
+[[ -n "$verbose" ]] && printf '  %s\n' "${files[@]}"
+
+########################################################################################################################
+###########################################  Identify proto files involved.  ###########################################
+########################################################################################################################
+
+[[ -n "$verbose" ]] && printf 'Identifying proto files linked to from %d files.\n' "${#files[@]}"
+
+declare protos_linked=()
+i=0
+for file in "${files[@]}"; do
+  i=$(( i + 1 ))
+  [[ -n "$verbose" ]] && printf '[%d/%d]: Checking %s ... ' "$i" "${#files[@]}" "$file"
+  declare new_proto_files=()
+  set -o noglob
+  IFS=$'\n' new_proto_files+=( $( grep -E "$link_rx" "$file" | sed 's|^.*/proto/|proto/|; s/#.*$//;' | sort -u ) )
+  set +o noglob
+  if [[ "${#new_proto_files[@]}" -ne '0' ]]; then
+    [[ -n "$verbose" ]] && printf 'Found %d:\n' "${#new_proto_files[@]}" && printf '[%d/%d]:   %s\n' "$i" "${#files[@]}" "${new_proto_files[@]}"
+    protos_linked+=( "${new_proto_files[@]}" )
+  else
+    [[ -n "$verbose" ]] && printf 'None found.\n'
+  fi
+done
+
+[[ -n "$verbose" ]] && printf 'All linked protos (with dups) (%d):\n' "${#protos_linked[@]}"
+[[ -n "$verbose" ]] && printf '  %s\n' "${protos_linked[@]}"
+
+# Deduplicate entries linked from multiple files.
+declare protos=()
+set -o noglob
+IFS=$'\n' protos+=( $( printf '%s\n' "${protos_linked[@]}" | sort -u ) )
+set +o noglob
+
+[[ -n "$verbose" ]] && printf 'All linked protos (no dups) (%d):\n' "${#protos[@]}"
+[[ -n "$verbose" ]] && printf '  %s\n' "${protos[@]}"
+
+########################################################################################################################
+#####################################  Put all needed proto files in a temp dir.  ######################################
+########################################################################################################################
+
+temp_dir="$( mktemp -d -t link-updates )"
+[[ -n "$verbose" ]] && printf 'Created temp dir for protos: %s\n' "$temp_dir"
+
+# Usage: safe_exit [code]
+safe_exit () {
+  local ec
+  ec="${1:-0}"
+
+  if [[ -n "$temp_dir" && -d "$temp_dir" ]]; then
+    if [[ -n "$no_clean" ]]; then
+      [[ -z "$quiet" ]] && printf 'NOT deleting temporary directory: %s\n' "$temp_dir"
+    else
+      [[ -n "$verbose" ]] && printf 'Deleting temporary directory: %s\n' "$temp_dir"
+      rm -rf "$temp_dir"
+      temp_dir=''
+    fi
+  fi
+
+  exit "$ec"
+}
+
+[[ -n "$verbose" ]] && printf 'Getting %d proto files.\n' "${#protos[@]}"
+
+repo_root="$( git rev-parse --show-toplevel )"
+
+i=0
+for file in "${protos[@]}"; do
+  i=$(( i + 1 ))
+  [[ -n "$verbose" ]] && printf '[%d/%d] Getting: %s ... ' "$i" "${#protos[@]}" "$file"
+  file_dir="$( dirname "$file" )"
+  dest_dir="${temp_dir}/${file_dir}"
+  mkdir -p "$dest_dir"
+
+  if [[ -n "$source_ref" ]]; then
+    dest_file="${temp_dir}/${file}"
+    header_file="$dest_file.header"
+    url="https://raw.githubusercontent.com/provenance-io/provenance/${source_ref}/$file"
+    [[ -n "$verbose" ]] && printf 'From: %s\n' "$url"
+    curl --silent -o "$dest_file" --dump-header "$header_file" "$url"
+    if ! head -n 1 "$header_file" | grep -q '200[[:space:]]*$' > /dev/null 2>&1; then
+      printf 'Source file not found: %s\n' "$url"
+      stop_early='YES'
+    fi
+  else
+    [[ -n "$verbose" ]] && printf 'From: %s\n' "$source_file"
+    source_file="${repo_root}/${file}"
+    cp "$source_file" "$dest_dir" || stop_early='YES'
+  fi
+done
+
+if [[ -n "$stop_early" ]]; then
+  safe_exit 1
+fi
+
+
+safe_exit 0
