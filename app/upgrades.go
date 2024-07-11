6f20237b5d3051eafffd34218bd1407da9c17321
Daniel Wedul <github@wedul.com>
2024-07-11 17:05:15 -0600

    In the umber upgrade, set the scope navs using the mainnet data.

--- app/upgrades.go
+++ app/upgrades.go
@@ -145,6 +145,11 @@ var upgrades = map[string]appUpgrade{
 
 			updateIBCClients(ctx, app)
 
+			err = addScopeNAVs(ctx, app, umberMainnetScopeNAVsFN)
+			if err != nil {
+				return nil, err
+			}
+
 			removeInactiveValidatorDelegations(ctx, app)
 
 			return vm, nil
