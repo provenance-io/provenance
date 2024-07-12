06d8db2ce3c02474b520b82ccc3c1915db05d369
Daniel Wedul <github@wedul.com>
2024-07-12 17:10:02 -0600

    Inject the umber upgrade.

--- app/app.go
+++ app/app.go
@@ -1049,6 +1049,8 @@ func (app *App) registerUpgradeHandlers(appOpts servertypes.AppOptions) {
 	// Add the upgrade handlers for each release.
 	InstallCustomUpgradeHandlers(app)
 
+	app.injectUpgrade("umber")
+
 	// Use the dump of $home/data/upgrade-info.json:{"name":"$plan","height":321654} to determine
 	// if we load a store upgrade from the handlers. No file == no error from read func.
 	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
