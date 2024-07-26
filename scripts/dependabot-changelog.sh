14b874b3791236c3e93ae7ae4714ca42f4fd4e74
Daniel Wedul <github@wedul.com>
2024-07-26 13:45:50 -0600

    Add a script that the github action will call.

---
+++ scripts/dependabot-changelog.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+# This script will create the changelog entry for a dependabot PR.
+# It's designed to be called by a github action kicked off because of a dependabot PR.
+
+while [[ "$#" -gt '0' ]]; do
+    case "$1" in
+        --help)
+          printf 'Usage: ./dependabot-changelog.sh --pr <num> --title <title> --branch <branch>\n'
+          exit 0
+          ;;
+        -p|--pull-request|--pr)
+            if [[ -z "$2" ]]; then
+                printf 'No argument provided after %s\n' "$1"
+                exit 1
+            fi
+            if [[ "$2" =~ [^[:digit:]] ]]; then
+                printf 'Invalid %s value: [%s]. Only digits are allowed.\n' "$1" "$2"
+                exit 1
+            fi
+            pr="$2"
+            shift
+            ;;
+        -t|--title)
+            if [[ -z "$2" ]]; then
+                printf 'No argument provided after %s\n' "$1"
+                exit 1
+            fi
+            title="$2"
+            shift
+            ;;
+        -b|--branch)
+            if [[ -z "$2" ]]; then
+                printf 'No argument provided after %s\n' "$1"
+                exit 1
+            fi
+            branch="$2"
+            shift
+            ;;
+        -v|--verbose)
+            verbose="$1"
+            ;;
+        *)
+            printf 'Unknown argument: [%s].\n' "$1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+if [[ -z "$branch" ]]; then
+    branch="$( git branch --show-current )"
+fi
+
+if [[ -z "$branch" || "$branch" == 'HEAD' ]]; then
+    printf 'Could not determine the branch and no --branch <branch> provided.\n'
+    exit 1
+fi
+[[ -n "$verbose" ]] && printf 'Branch: "%s"\n' "$branch"
+
+if [[ -z "$pr" ]]; then
+    printf 'No --pr <num> provided.\n'
+    exit 1
+fi
+[[ -n "$verbose" ]] && printf '   PR: "%s"\n' "$pr"
+
+if [[ -z "$title" ]]; then
+    printf 'No --title <title> provided.\n'
+    exit 1
+fi
+[[ -n "$verbose" ]] && printf 'Title: "%s"\n' "$title"
+
+# Dependabot branch names look like this: "dependabot/github_actions/bufbuild/buf-setup-action-1.34.0"
+# The "github_actions" can also be "go_modules" (anb probably other things too).
+# For the filename, we'll omit the "dependabot/<lib type>/" part and use just what's left.
+branch_fn="$( sed -E 's|^[^/]+/[^/]+/||; s|/|-|g;' <<< "$branch" )"
+[[ -n "$verbose" ]] && printf 'Branch Filename: "%s"\n' "$branch_fn"
+
+# This script requires another script that must be in the same directory.
+# To consistently find them, we'll need to know the absolute path to the dir with this script.
+where_i_am="$( cd "$( dirname "${BASH_SOURCE:-$0}" )"; pwd -P )"
+[[ -n "$verbose" ]] && printf '     Where I Am: "%s"\n' "$where_i_am"
+
+# Run the script to create the entry from the changes in go.mod.
+# The $verbose variable is purposely not quoted so that it doesn't count as an arg if it's empty.
+"$where_i_am/get-dep-changes.sh" --pr "$pr" --name "$branch_fn" $verbose --force
+ec=$?
+# That script exits with 0 when there are go.mod changes and the new file was created.
+# If there were go.mod changes, we're all done here.
+# I don't think I've ever seen a dependabot PR that bumps both a go module and something else.
+if [[ "$ec" -eq '0' ]]; then
+    exit 0
+fi
+
+# That script exits with 10 to indicate there were no go.mod changes.
+# All other exit codes are an error that requires attention.
+if [[ "$ec" -ne '10' ]]; then
+    printf 'An error was encountered.\n'
+    exit "$ec"
+fi
+
+# Okay. There weren't any go.mod changes. It's a bump to something else (e.g. a
+# github action helper). Create an entry ourselves, based on the title, which
+# should look something like this: "Bump <library> from <old version> to <new version>".
+# First, though, standardize the spacing so the rest of the regex stuff is cleaner.
+title="$( sed -E 's/[[:space:]]+/ /; s/^ //; s/ $//;' <<< "$title" )"
+[[ -n "$verbose" ]] && printf 'Clean Title: "%s"\n' "$title"
+if ! grep -Eqi '^Bump [^ ]+ from [^ ]+ to [^ ]+$' <<< "$title"; then
+    printf 'Unknown title format: %s\n' "$title"
+    exit 1
+fi
+
+lib="$( sed -E 's/^Bump //; s/ from.*$//;' <<< "$title" )"
+[[ -n "$verbose" ]] && printf 'Library: "%s"\n' "$lib"
+if [[ -z "$lib" || "$lib" == "$title" || "$lib" =~ ' ' ]]; then
+    printf 'Could not extract library from title: %s\n' "$title"
+    exit 1
+fi
+
+old_ver="$( sed -E 's/^.*from //; s/ to.*$//' <<< "$title" )"
+[[ -n "$verbose" ]] && printf 'Old Ver: "%s"\n' "$old_ver"
+if [[ -z "$old_ver" || "$old_ver" == "$title" || "$old_ver" =~ ' ' ]]; then
+    printf 'Could not extract old version from title: %s\n' "$title"
+    exit 1
+fi
+
+new_ver="$( sed -E 's/^.*to //' <<< "$title" )"
+[[ -n "$verbose" ]] && printf 'New Ver: "%s"\n' "$new_ver"
+if [[ -z "$new_ver" || "$new_ver" == "$title" || "$new_ver" =~ ' ' ]]; then
+    printf 'Could not extract new version from title: %s\n' "$title"
+    exit 1
+fi
+
+link="[PR ${pr}](https://github.com/provenance-io/provenance/pull/${pr})"
+[[ -n "$verbose" ]] && printf 'Link: "%s"\n' "$link"
+repo_root="$( git rev-parse --show-toplevel > /dev/null 2>&1 )"
+if [[ -z "$repo_root" ]]; then
+    if [[ ! -d './.changelog' ]]; then
+        printf 'Could not identify target directory.\n'
+        exit 1
+    fi
+    repo_root='.'
+fi
+[[ -n "$verbose" ]] && printf 'Repo Root: "%s"\n' "$repo_root"
+
+out_dir="${repo_root}/.changelog/unreleased/dependencies"
+[[ -n "$verbose" ]] && printf 'Output  Dir: "%s"\n' "$out_dir"
+if ! mkdir -p "$out_dir"; then
+    printf 'Could not create directory: %s\n' "$out_dir"
+    exit 1
+fi
+
+name="$( sed -E 's/[^[:alnum:]]+/-/g; s/^-//; s/-$//;' <<< "$branch_fn" | tr '[:upper:]' '[:lower:]' )"
+[[ -n "$verbose" ]] && printf 'Name: "%s"\n' "$name"
+out_fn="${out_dir}/${pr}-${name}.md"
+[[ -n "$verbose" ]] && printf 'Output File: "%s"\n' "$out_fn"
+
+printf '* `%s` bumped to %s (from %s) %s.\n' "$lib" "$new_ver" "$old_ver" "$link" > "$out_fn" || exit 1
+
+printf 'Dependabot changelog entry created: %s\n' "$out_fn"
+exit 0
