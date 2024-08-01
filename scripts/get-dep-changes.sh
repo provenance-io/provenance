64d16f1d4615bb30ad545b777ab0fc7a4f4f923c
Daniel Wedul <github@wedul.com>
2024-08-01 15:26:18 -0600

    Change dependency entries for replace lines to list the version first, then the replaced library.

--- scripts/get-dep-changes.sh
+++ scripts/get-dep-changes.sh
@@ -217,9 +217,10 @@ get_replace_str () {
     if [[ -n "$repl" ]]; then
         if [[ "$repl" =~ ' ' ]]; then
             # $repl has the format "<other library> <other version>"
-            # It's provided without quotes so that it gets split on that space and provided
-            # to this printf as two separate args to put tics around the <other library>.
-            printf '`%s` %s' $repl
+            # We'll want it as "<other version> of `<other library>`".
+            # It's provided without quotes to printf so that it gets split on that
+            # space and provided as two separate args to put tics around the <other library>.
+            printf '%s of `%s`' $( sed -E 's/^([^ ]+) +(.*)$/\2 \1/' <<< "$repl" )
         else
             # $repl is a <location>, put tics around it.
             printf '`%s`' "$repl"
