a16b8b1449e033085b748ebb498da7cc4d7396d3
Daniel Wedul <github@wedul.com>
2024-09-27 13:04:46 -0600

    [2137]: Fix import ordering in app/upgrades.go.

--- app/upgrades.go
+++ app/upgrades.go
@@ -13,6 +13,7 @@ import (
 	storetypes "cosmossdk.io/store/types"
 	circuittypes "cosmossdk.io/x/circuit/types"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
+
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
