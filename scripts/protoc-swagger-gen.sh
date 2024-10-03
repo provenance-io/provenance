4e8964761b167bfd4d10dc3ad3ebf99120d41849
Daniel Wedul <github@wedul.com>
2024-10-03 15:40:10 -0600

    [2135]: Update the protoc-swagger-gen.sh and protocgen.sh scripts. Add warnings from protoc-swagger.gen about stuff not in the config.

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -1,31 +1,27 @@
 #!/usr/bin/env bash
 
-verbose=''
-if [[ "$1" == '-v' || "$1" == '--verbose' ]]; then
-  verbose='--verbose'
+if [ "$1" == '-v' ] || [ "$1" == '--verbose' ]; then
+  VERBOSE=1
 fi
 
 set -eo pipefail
 
 mkdir -p ./tmp-swagger-gen
-proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-for dir in $proto_dirs; do
+proto_files=$( find ./proto -type f -name '*.proto' -print0 | xargs -0 grep -El '^service +[^ ]+ +\{' )
+for file in $proto_files; do
+  [ -n "$VERBOSE" ] && printf 'Generating swagger file for [%s].\n' "$file"
+  buf generate --template proto/buf.gen.swagger.yaml "$file"
+done
 
-  # generate swagger files for the queries.
-  query_file=$(find "${dir}" -maxdepth 1 -name 'query.proto')
-  if [[ -n "$query_file" ]]; then
-    [[ -n "$verbose" ]] && printf 'Generating swagger file for [%s].\n' "$query_file"
-    buf generate --template proto/buf.gen.swagger.yaml "$query_file"
-  fi
-  # generate swagger files for the transactions.
-  tx_file=$(find "${dir}" -maxdepth 1 -name 'tx.proto')
-  if [[ -n "$tx_file" ]]; then
-    [[ -n "$verbose" ]] && printf 'Generating swagger file for [%s].\n' "$tx_file"
-    buf generate --template proto/buf.gen.swagger.yaml "$tx_file"
+[ -n "$VERBOSE" ] && printf 'Combining swagger files.\n'
+
+swagger_files=$( find ./tmp-swagger-gen -type f -name '*.swagger.json' )
+for file in $swagger_files; do
+  if ! grep -Fq "\"url\": \"$file\"" ./client/docs/config.json; then
+      printf '\033[93mWARNING\033[0m: "%s" not referenced in ./client/docs/config.json\n' "$file"
   fi
 done
 
-[[ -n "$verbose" ]] && printf 'Combining swagger files.\n'
 # combine swagger files
 # uses nodejs package `swagger-combine`.
 # all the individual swagger files need to be configured in `config.json` for merging
@@ -37,5 +33,5 @@ swagger-combine ./client/docs/config.json -o ./tmp-swagger-gen/swagger-new.yaml 
 
 grep -v '        - Query' "./tmp-swagger-gen/swagger-new.yaml" | grep -v '        - Service' > "./client/docs/swagger-ui/swagger.yaml"
 # clean swagger files
-[[ -n "$verbose" ]] && printf 'Deleting ./tmp-swagger-gen\n'
+[ -n "$VERBOSE" ] && printf 'Deleting ./tmp-swagger-gen\n'
 rm -rf ./tmp-swagger-gen
--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -6,26 +6,15 @@
 
 set -eo pipefail
 
-protoc_gen_gocosmos() {
-  if ! grep "github.com/cosmos/gogoproto" go.mod &>/dev/null ; then
-    echo -e "\tPlease run this command from somewhere inside the cosmos-sdk folder."
-    return 1
-  fi
+if [ "$1" == '-v' ] || [ "$1" == '--verbose' ]; then
+    VERBOSE=1
+fi
 
-  go get github.com/cosmos/gogoproto/protoc-gen-gocosmos@latest
-}
-
-protoc_gen_gocosmos
-
-proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-for dir in $proto_dirs; do
-  proto_files=$(find "${dir}" -maxdepth 1 -name '*.proto')
-  for file in $proto_files; do
-    # Check if the go_package in the file is pointing to provenance
-    if grep -q "option go_package.*provenance" "$file"; then
-      buf generate --template proto/buf.gen.gogo.yaml "$file"
-    fi
-  done
+# Find all of our proto files that have our go_package name.
+proto_files=$( find ./proto -type f -name '*.proto' -print0 | xargs -0 grep -l 'option go_package.*provenance' )
+for file in $proto_files; do
+  [ "$VERBOSE" ] && printf 'Generating proto code for %s\n' "$file"
+  bug generate --template proto/buf.gen.gogo.yaml "$file"
 done
 
 # move proto files to the right places
