8ccd14fd24b735a6b1760a31ef1cad1f679f044d
Daniel Wedul <github@wedul.com>
2024-10-03 17:33:45 -0600

    [2135]: run make proto-gen

--- x/attribute/types/query.pb.go
+++ x/attribute/types/query.pb.go
@@ -1000,6 +1000,7 @@ func _Query_AccountData_Handler(srv interface{}, ctx context.Context, dec func(i
 	return interceptor(ctx, in, info, handler)
 }
 
+var Query_serviceDesc = _Query_serviceDesc
 var _Query_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "provenance.attribute.v1.Query",
 	HandlerType: (*QueryServer)(nil),
--- x/attribute/types/tx.pb.go
+++ x/attribute/types/tx.pb.go
@@ -1166,6 +1166,7 @@ func _Msg_UpdateParams_Handler(srv interface{}, ctx context.Context, dec func(in
 	return interceptor(ctx, in, info, handler)
 }
 
+var Msg_serviceDesc = _Msg_serviceDesc
 var _Msg_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "provenance.attribute.v1.Msg",
 	HandlerType: (*MsgServer)(nil),
