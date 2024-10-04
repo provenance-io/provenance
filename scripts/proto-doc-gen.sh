e6e53332dae690fa8c4a16110c0ab769a2e5bdc5
Daniel Wedul <github@wedul.com>
2024-10-04 16:12:55 -0600

    [376]: Create the proto-doc-gen.sh script that will recreate docs/proto-docs.md and create a proto-doc-gen make target. Add proto-doc-gen to proto-all and proto-regen.

---
+++ scripts/proto-doc-gen.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+# Prerequisites:
+# - protoc: https://grpc.io/docs/protoc-installation/
+# - protoc-gen-doc: https://github.com/pseudomuto/protoc-gen-doc
+
+ec=0
+
+if ! command -v protoc > /dev/null 2>&1; then
+  printf 'Command not found: protoc\n' >&2
+  printf 'See: https://grpc.io/docs/protoc-installation/\n' >&2
+  ec=1
+fi
+
+if ! command -v protoc-gen-doc > /dev/null 2>&1; then
+  printf 'Plugin not found: protoc-gen-doc\n' >&2
+  printf 'See: https://github.com/pseudomuto/protoc-gen-doc\n' >&2
+  ec=1
+fi
+
+if [ "$ec" != '0' ]; then
+  exit "$ec"
+fi
+
+# It's assumed that this script is in <repo root>/<some folder>/proto-doc-gen.sh.
+# We need to be in <repo root> for this to work, so start where this script is and go up one.
+where_i_am="$( cd "$( dirname "${BASH_SOURCE:-$0}" )" || exit $?; pwd -P )" || ec=$?
+if [ -z "$where_i_am" ] || [ "$ec" != '0' ]; then
+  printf 'Could not identify where this script is located: %s\n' "$0" >&2
+  ec=1
+fi
+
+cd "$where_i_am/.." || ec=$?
+if [ "$ec" != '0' ]; then
+  printf 'Could not cd %s\n' "$where_i_am/.." >&2
+  exit 1
+fi
+
+if ! [ -d ./proto ]; then
+  printf 'Directory not found: ./proto\n' >&2
+  ec=1
+fi
+if ! [ -d ./third_party ]; then
+  printf 'Directory not found: ./third_party\n' >&2
+  ec=1
+fi
+if ! [ -f ./docs/protodoc-markdown.tmpl ]; then
+  printf 'File not found: ./docs/protodoc-markdown.tmpl\n' >&2
+  ec=1
+fi
+
+if [ "$ec" != '0' ]; then
+  printf 'Base directory . = %s\n' "`pwd`" >&2
+  exit "$ec"
+fi
+
+# Generate the docs/proto-docs.md file.
+protoc \
+  -I "proto" \
+  -I "third_party/proto" \
+  --doc_out=./docs \
+  --doc_opt=./docs/protodoc-markdown.tmpl,proto-docs.md \
+  $( find "proto" -name '*.proto' )
