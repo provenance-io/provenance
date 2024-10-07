363eb32f9efe519a742c7903654f70cbf5ee65b7
Daniel Wedul <github@wedul.com>
2024-10-07 11:39:51 -0600

    Fix our proto generation stuff. (#2167)
    * [2135]: Update the proto stuff in the makefile. Bump proto image ver to 0.15.1 (from 0.14.0). Clean up the proto-related targets to match the SDK's.
    
    * [2135]: Update the protoc-swagger-gen.sh and protocgen.sh scripts. Add warnings from protoc-swagger.gen about stuff not in the config.
    
    * [2135]: Update proto/buf.gen.swagger.yaml to use openapiv2 instead of swagger.
    
    * [2135]: Further tweaks to protoc-swagger-gen to make it easier to alter for third-party purposes.
    
    * [2135]: Fix typo in protocgen.sh.
    
    * [2135]: run make proto-gen
    
    * [2135]: Add missing ibchooks entry from swagger config.
    
    * [2135]: run make proto-swagger-gen
    
    * [2135]: Add changelog entry.
    
    * [2135]: Switch to = for the tests in [ ] for verbosity.

--- x/name/types/query.pb.go
+++ x/name/types/query.pb.go
@@ -475,6 +475,7 @@ func _Query_ReverseLookup_Handler(srv interface{}, ctx context.Context, dec func
 	return interceptor(ctx, in, info, handler)
 }
 
+var Query_serviceDesc = _Query_serviceDesc
 var _Query_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "provenance.name.v1.Query",
 	HandlerType: (*QueryServer)(nil),
--- x/trigger/types/query.pb.go
+++ x/trigger/types/query.pb.go
@@ -367,6 +367,7 @@ func _Query_Triggers_Handler(srv interface{}, ctx context.Context, dec func(inte
 	return interceptor(ctx, in, info, handler)
 }
 
+var Query_serviceDesc = _Query_serviceDesc
 var _Query_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "provenance.trigger.v1.Query",
 	HandlerType: (*QueryServer)(nil),
