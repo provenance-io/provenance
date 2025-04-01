aae4c3b66e0f351edd9dcbcb8ae1228f908d0fca
Matt Conroy <mconroy@figuremarkets.com>
2025-04-01 10:49:47 -0400

    - Init ledger module

---
+++ x/ledger/module/module.go
@@ -0,0 +1,88 @@
+package mymodule
+
+import (
+	"encoding/json"
+
+	abci "github.com/cometbft/cometbft/abci/types"
+	client "github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/provenance-io/provenance/x/ledger"
+	"github.com/provenance-io/provenance/x/ledger/keeper"
+)
+
+// AppModuleBasic defines the basic application module used by mymodule.
+type AppModuleBasic struct{}
+
+// Name returns the module name.
+func (AppModuleBasic) Name() string {
+	return ledger.ModuleName
+}
+
+// RegisterCodec registers the module's types. (In newer SDK versions, use RegisterLegacyAminoCodec.)
+func (AppModuleBasic) RegisterCodec(cdc *codec.LegacyAmino) {
+	// Register any concrete types if needed.
+}
+
+// DefaultGenesis returns default genesis state as raw bytes.
+// func (AppModuleBasic) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
+// 	// Return a default (empty) genesis state.
+// 	return cdc.MustMarshalJSON()
+// }
+
+// ValidateGenesis validates the genesis state.
+func (AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, config client.TxEncodingConfig, bz json.RawMessage) error {
+	return nil
+}
+
+// AppModule implements an application module for mymodule.
+type AppModule struct {
+	AppModuleBasic
+	keeper keeper.Keeper
+}
+
+// NewAppModule creates a new AppModule instance.
+func NewAppModule(k keeper.Keeper) AppModule {
+	return AppModule{
+		keeper: k,
+	}
+}
+
+// RegisterInvariants registers the invariants of the module.
+func (am AppModule) RegisterInvariants(ir sdk.InvariantRegistry) {}
+
+// Route returns the message routing key for the module.
+// func (am AppModule) Route() types.RouterKey {
+// 	return ledger.RouterKey
+// }
+
+// NewHandler returns an sdk.Handler for the module.
+// func (am AppModule) NewHandler() sdk.Handler {
+// 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
+// 		// Handle messages. This is a placeholder.
+// 		return nil, nil
+// 	}
+// }
+
+// QuerierRoute returns the module's querier route name.
+func (am AppModule) QuerierRoute() string {
+	return ledger.ModuleName
+}
+
+// LegacyQuerierHandler returns the module sdk.Querier.
+// func (am AppModule) LegacyQuerierHandler(legacyQuerierCdc *codec.LegacyAmino) sdk.Querier {
+// 	// Implement query handlers if needed.
+// 	return nil
+// }
+
+// InitGenesis initializes the genesis state.
+func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, gs json.RawMessage) []abci.ValidatorUpdate {
+	// Initialize genesis state if needed.
+	return []abci.ValidatorUpdate{}
+}
+
+// ExportGenesis exports the module's genesis state.
+// func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.RawMessage {
+// 	// Export current state as genesis state.
+// 	return cdc.MustMarshalJSON(struct{}{})
+// }
