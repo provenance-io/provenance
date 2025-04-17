626ecf8a974cd5603a8c85149d40fa8be1c14399
kwt <4344285+kwtalley@users.noreply.github.com>
2025-04-17 10:02:23 -0500

    add asset module

---
+++ x/asset/simulation/genesis.go
@@ -0,0 +1,9 @@
+package simulation
+
+import "github.com/cosmos/cosmos-sdk/types/module"
+
+// RandomizedGenState generates a random GenesisState for the asset module.
+func RandomizedGenState(simState *module.SimulationState) {
+	_ = simState
+	// TODO randomize genesis state
+}
