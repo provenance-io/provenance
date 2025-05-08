7e41d8f743a6674becf2f5ca5839654fbbbe6306
Matt Conroy <matt@figure.com>
2025-05-08 16:39:38 -0400

    - Implement the grant/revoke

--- x/registry/keeper/msg_server.go
+++ x/registry/keeper/msg_server.go
@@ -33,10 +33,52 @@ func (k msgServer) RegisterNFT(ctx context.Context, msg *registry.MsgRegisterNFT
 }
 
 func (k msgServer) GrantRole(ctx context.Context, msg *registry.MsgGrantRole) (*registry.MsgGrantRoleResponse, error) {
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+	authority, err := sdk.AccAddressFromBech32(msg.Authority)
+	if err != nil {
+		return nil, err
+	}
+
+	addresses := make([]*sdk.AccAddress, len(msg.Addresses))
+	for i, address := range msg.Addresses {
+		addr, err := sdk.AccAddressFromBech32(address)
+		if err != nil {
+			return nil, err
+		}
+
+		addresses[i] = &addr
+	}
+
+	err = k.keeper.GrantRole(sdkCtx, authority, msg.Key, msg.Role.String(), addresses)
+	if err != nil {
+		return nil, err
+	}
+
 	return nil, nil
 }
 
 func (k msgServer) RevokeRole(ctx context.Context, msg *registry.MsgRevokeRole) (*registry.MsgRevokeRoleResponse, error) {
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+	authority, err := sdk.AccAddressFromBech32(msg.Authority)
+	if err != nil {
+		return nil, err
+	}
+
+	addresses := make([]*sdk.AccAddress, len(msg.Addresses))
+	for i, address := range msg.Addresses {
+		addr, err := sdk.AccAddressFromBech32(address)
+		if err != nil {
+			return nil, err
+		}
+
+		addresses[i] = &addr
+	}
+
+	err = k.keeper.RevokeRole(sdkCtx, authority, msg.Key, msg.Role.String(), addresses)
+	if err != nil {
+		return nil, err
+	}
+
 	return nil, nil
 }
 
