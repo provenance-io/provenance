4e3ae9fc2cda06b36d06da9b8e56ce6821d0bd5d
Daniel Wedul <github@wedul.com>
2025-07-02 14:15:45 -0600

    Get rid of the unused error types.

--- x/flatfees/types/errors.go
+++ x/flatfees/types/errors.go
@@ -4,12 +4,8 @@ import (
 	cerrs "cosmossdk.io/errors"
 )
 
-// x/flatfees module sentinel errors
+// The x/flatfees module sentinel errors.
+
 var (
-	ErrEmptyMsgType        = cerrs.Register(ModuleName, 2, "msg type is empty")
-	ErrInvalidFee          = cerrs.Register(ModuleName, 3, "invalid fee amount")
-	ErrMsgFeeAlreadyExists = cerrs.Register(ModuleName, 4, "fee for type already exists")
-	ErrMsgFeeDoesNotExist  = cerrs.Register(ModuleName, 5, "fee for type does not exist")
-	ErrInvalidFeeProposal  = cerrs.Register(ModuleName, 6, "invalid fee proposal")
-	ErrInvalidBipsValue    = cerrs.Register(ModuleName, 7, "invalid bips amount")
+	ErrMsgFeeDoesNotExist = cerrs.Register(ModuleName, 5, "fee for type does not exist")
 )
