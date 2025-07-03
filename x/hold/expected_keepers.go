0a77d984be1e6cde5939c871af9f05d0d72f61e7
Daniel Wedul <github@wedul.com>
2025-07-03 13:24:10 -0600

    Remove HasAccount and NewAccount from the AccountKeeper expected interface since those aren't used in here.

--- x/hold/expected_keepers.go
+++ x/hold/expected_keepers.go
@@ -14,6 +14,4 @@ type BankKeeper interface {
 type AccountKeeper interface {
 	GetAccount(ctx context.Context, addr sdk.AccAddress) sdk.AccountI
 	SetAccount(ctx context.Context, acc sdk.AccountI)
-	HasAccount(ctx context.Context, addr sdk.AccAddress) bool
-	NewAccount(ctx context.Context, acc sdk.AccountI) sdk.AccountI
 }
