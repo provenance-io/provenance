15baaac80d544ab69876af1799584f6c95e05e6a
nagarajdivine <nmanjunath@figuremarkets.com>
2025-07-07 20:58:41 +0800

    Added governance-only tx endpoint to the hold module for unlocking vesting accounts back to base accounts. (#2385)
    * feat(unlock-vesting-account): implemented unlocking vesting account to base account
    
    * feat(unlock-vesting-account): added event handler and updated test cases
    
    * feat(unlock-vesting-account): added changelog and fixed lint-fix
    
    * feat(unlock-vesting-account): fixed proto file, updated hold module msg,codec and lint fix
    
    * feat(unlock-vesting-account): added event and cli test cases and fixed proto & resolved linting issue
    
    * feat(unlock-vesting-account): resolved linting issue
    
    * feat(unlock-vesting-account): resolved nolint:gosec
    
    * Move the hold tx section of swagger config up to be with they hold query section and remove the unneeded rename.
    
    * Proto work: Add the EventVestingAccountUnlocked event and remove EventUnlockVestingAccounts. Remove the fields from MsgUnlockVestingAccountsResponse and remove the UnlockFailure message.
    
    * Switch to our standard codec RegisterInterfaces process.
    
    * Remove HasAccount and NewAccount from the AccountKeeper expected interface since those aren't used in here.
    
    * Constructor for the new EventVestingAccountUnlocked, and remove the old one.
    
    * Clean up the msg server and move the actual working methods to the keeper.
    
    * Add some tests on some of the simple keeper stuff.
    
    * Unit tests on the new event constructor and remove the tests on the old one.
    
    * Tweak the MsgUnlockVestingAccountsRequest ValidateBasic to fix up the errors that had incorrect formatting and wasn't using the best typed errors. Unit tests on the msgs stuff.
    
    * Remove a couple unneeded keys strings: RouterKey and QuerierRoute.
    
    * In GenerateOrBroadcastTxCLIAsGovProp, run validate basic on each msg.
    
    * Overhaul the new command: Skip manual error checking since ValidateBasic will do everything we need. Allow addresses to be provided by both flag and file. Read the file as a raw text file, one address per line. Fix examples and usage and give some aliases.
    
    * Fix up the unit tests on the new tx CLI command.
    
    * Allow unlocking permanent locked accounts too.
    
    * Overhaul the msg server tests.
    
    * Fix two CLI tests (in marker and oracle) that broke because I made GenerateOrBroadcastTxCLIAsGovProp run basic validation.
    
    * feat(unlock-vesting-account): resolved lint issue
    
    * Finish unit tests on the UnlockVestingAccount (and plural) keeper methods.
    
    ---------
    
    Co-authored-by: Daniel Wedul <github@wedul.com>

---
+++ .changelog/unreleased/features/2347-unlock-vesting-accounts-base.md
@@ -0,0 +1,1 @@
+* Added governance-only tx endpoint to the hold module for unlocking vesting accounts back to base accounts [#2347](https://github.com/provenance-io/provenance/issues/2347).
--- x/oracle/client/cli/cli_test.go
+++ x/oracle/client/cli/cli_test.go
@@ -163,7 +163,7 @@ func (s *IntegrationTestSuite) TestOracleUpdate() {
 		{
 			name:         "failure - unable to pass validate basic with bad address",
 			address:      "badaddress",
-			expectErrMsg: "invalid address for oracle: decoding bech32 failed: invalid separator index -1: invalid proposal message",
+			expectErrMsg: "invalid address for oracle: decoding bech32 failed: invalid separator index -1",
 			expectedCode: 12,
 			signer:       s.accountAddresses[0].String(),
 		},
@@ -183,7 +183,7 @@ func (s *IntegrationTestSuite) TestOracleUpdate() {
 			}
 			testcli.NewTxExecutor(cmd, args).
 				WithExpCode(tc.expectedCode).
-				WithExpInRawLog([]string{tc.expectErrMsg}).
+				WithExpErrMsg(tc.expectErrMsg).
 				Execute(s.T(), s.network)
 		})
 	}
