d7fb577891b6d107c3528c41b4ffd464b6d3b23a
nagarajdivine <nmanjunath@figuremarkets.com>
2025-07-29 01:25:29 +0800

    feature(collections-name-module): added mogations method and confgiured app.go for migration

--- app/upgrades.go
+++ app/upgrades.go
@@ -8,7 +8,6 @@ import (
 
 	storetypes "cosmossdk.io/store/types"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
-
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
@@ -104,6 +103,20 @@ var upgrades = map[string]appUpgrade{
 			setFees(ctx, app)
 
 			return vm, nil
+		},
+	},
+	"name_module_collections_migration_v2_to_v3": {
+		Handler: func(ctx sdk.Context, app *App, vm module.VersionMap) (module.VersionMap, error) {
+			// Create the Migrator using the app's NameKeeper
+			migrator := namekeeper.NewMigrator(app.NameKeeper)
+
+			// Call the method on the migrator
+			if err := migrator.MigrateKVToCollections2to3(ctx); err != nil {
+				return nil, err
+			}
+
+			// Continue with regular module migrations
+			return app.mm.RunMigrations(ctx, app.configurator, vm)
 		},
 	},
 }
