0d59834f9793f58b0b2aa91e1428552e8d041d08
nagarajdivine <nmanjunath@figuremarkets.com>
2025-08-01 02:16:55 +0800

    feature(collections-name-module): rebased to main and resolved all suggestions

--- app/upgrades_test.go
+++ app/upgrades_test.go
@@ -1057,17 +1057,6 @@ func (s *UpgradeTestSuite) TestGeranium() {
 	}
 	s.AssertUpgradeHandlerLogs("geranium", expInLog, nil)
 }
-func (s *UpgradeTestSuite) TestCollectionsmigration() {
-	expInLog := []string{
-		"INF Migrating name module from KV store to collections (v2 to v3)...",
-		"INF Migrated name module parameters to collections store.",
-		"INF Migrating name records...",
-		"INF Migrated 1 name records.",
-		"INF Migrated 1 address index records.",
-		"INF Name module migration to collections (v2 to v3) completed successfully.",
-	}
-	s.AssertUpgradeHandlerLogs("collectionsmigration", expInLog, nil)
-}
 
 func (s *UpgradeTestSuite) TestAddFlatFeesOracleAddress() {
 	addr := sdk.AccAddress(secp256k1.GenPrivKey().PubKey().Address()).String()
