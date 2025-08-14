89161a16c618feb772ae0fd37f2aa6e58ed8c7c2
kwt <4344285+kwtalley@users.noreply.github.com>
2025-08-14 16:45:10 -0500

    add wrapperspb.StringValue to interface registry for asset/nft queries

--- x/asset/types/codec.go
+++ x/asset/types/codec.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	wrapperspb "google.golang.org/protobuf/types/known/wrapperspb"
+
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
@@ -12,5 +14,6 @@ func RegisterInterfaces(registry types.InterfaceRegistry) {
 	messages := make([]proto.Message, len(AllRequestMsgs))
 	copy(messages, AllRequestMsgs)
 	registry.RegisterImplementations((*sdk.Msg)(nil), messages...)
+	registry.RegisterImplementations((*proto.Message)(nil), &wrapperspb.StringValue{})
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
