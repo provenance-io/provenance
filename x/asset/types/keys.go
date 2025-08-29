9e92844c0a0027ca5c36dcf43c4705e69b96e4dd
kwt <4344285+kwtalley@users.noreply.github.com>
2025-08-29 16:43:08 -0500

    remove unneeded keys from asset

--- x/asset/types/keys.go
+++ x/asset/types/keys.go
@@ -3,13 +3,4 @@ package types
 const (
 	// ModuleName defines the module name
 	ModuleName = "asset"
-
-	// StoreKey defines the primary module store key
-	StoreKey = ModuleName
-
-	// RouterKey is the message route for assets
-	RouterKey = ModuleName
-
-	// QuerierRoute defines the module's query routing key
-	QuerierRoute = ModuleName
 )
