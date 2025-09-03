1af4f92d8c244959955b65a6c7e227a202b843cc
Daniel Wedul <github@wedul.com>
2025-09-03 15:03:46 -0600

    Registry: Add the TestAllMsgsGetSigners test to the registry module.

---
+++ x/registry/types/msgs_test.go
@@ -0,0 +1,21 @@
+package types_test
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/provenance-io/provenance/testutil"
+
+	. "github.com/provenance-io/provenance/x/registry/types"
+)
+
+func TestAllMsgsGetSigners(t *testing.T) {
+	msgMakers := []testutil.MsgMaker{
+		func(signer string) sdk.Msg { return &MsgRegisterNFT{Authority: signer} },
+		func(signer string) sdk.Msg { return &MsgGrantRole{Authority: signer} },
+		func(signer string) sdk.Msg { return &MsgRevokeRole{Authority: signer} },
+		func(signer string) sdk.Msg { return &MsgUnregisterNFT{Authority: signer} },
+	}
+
+	testutil.RunGetSignersTests(t, AllRequestMsgs, msgMakers, nil)
+}
