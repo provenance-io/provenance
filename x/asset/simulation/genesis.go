df0ca4299934aafb2a6a041d724a88bee02d6be2
Daniel Wedul <github@wedul.com>
2025-09-04 17:14:19 -0600

    Delete the asset simulation stuff since it's not used.

--- x/asset/simulation/genesis.go
+++
@@ -1,9 +0,0 @@
-package simulation
-
-import "github.com/cosmos/cosmos-sdk/types/module"
-
-// RandomizedGenState generates a random GenesisState for the asset module.
-func RandomizedGenState(simState *module.SimulationState) {
-	_ = simState
-	// TODO randomize genesis state
-}
