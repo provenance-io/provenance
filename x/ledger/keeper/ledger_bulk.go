cef8cfae73b722e28c2a5af4a4fc29ca2519316e
Daniel Wedul <github@wedul.com>
2025-09-04 17:25:49 -0600

    Use a cache context when bulk loading.

--- x/ledger/keeper/ledger_bulk.go
+++ x/ledger/keeper/ledger_bulk.go
@@ -8,9 +8,12 @@ import (
 
 // BulkCreate creates ledgers and their entries. This function assumes that ledger classes, status types, entry types,
 // and bucket types are already created before calling this function.
-func (k Keeper) BulkCreate(ctx sdk.Context, ledgers []*types.LedgerToEntries) error {
+func (k Keeper) BulkCreate(ctxOrig sdk.Context, ledgers []*types.LedgerToEntries) error {
 	// Import ledgers and their entries
 	for _, ledgerToEntries := range ledgers {
+		// Use a cache context so that an entry is all or nothing.
+		ctx, writeCache := ctxOrig.CacheContext()
+
 		// If we have a ledger object, add it.
 		// All errors are handled in the AddLedger function (dupe, ledger class, etc).
 		if ledgerToEntries.Ledger != nil {
@@ -24,6 +27,9 @@ func (k Keeper) BulkCreate(ctx sdk.Context, ledgers []*types.LedgerToEntries) er
 		if err := k.AppendEntries(ctx, ledgerToEntries.LedgerKey, ledgerToEntries.Entries); err != nil {
 			return err
 		}
+
+		// Done with this entry, write it!
+		writeCache()
 	}
 
 	return nil
