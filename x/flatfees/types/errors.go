5258be2d69dfaacaaf823e0aee31194b187f4f34
nagarajdivine <nmanjunath@figuremarkets.com>
2025-09-09 00:58:58 +0800

    chore(upgrade-golanglint): rebased to main,fix govet, nolintlint and revive lint warnings

--- x/flatfees/types/errors.go
+++ x/flatfees/types/errors.go
@@ -7,5 +7,6 @@ import (
 // The x/flatfees module sentinel errors.
 
 var (
+	// ErrMsgFeeDoesNotExist is returned when a message fee entry does not exist for a given message type.
 	ErrMsgFeeDoesNotExist = cerrs.Register(ModuleName, 5, "fee for type does not exist")
 )
