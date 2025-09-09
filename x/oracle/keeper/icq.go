e86f40752a13cdd9b7abef8a9f762433cabbc77e
nagarajdivine <nmanjunath@figuremarkets.com>
2025-09-10 01:14:17 +0800

    chore(upgrade-golanglint): fix gosec warning from lint

--- x/oracle/keeper/icq.go
+++ x/oracle/keeper/icq.go
@@ -36,7 +36,7 @@ func (k Keeper) QueryOracle(ctx sdk.Context, query wasmtypes.RawContractMessage,
 	}
 
 	timeoutTimestamp := ctx.BlockTime().Add(time.Minute).UnixNano()
-	seq, err := k.SendQuery(ctx, types.PortID, channel, chanCap, reqs, clienttypes.ZeroHeight(), uint64(timeoutTimestamp))
+	seq, err := k.SendQuery(ctx, types.PortID, channel, chanCap, reqs, clienttypes.ZeroHeight(), uint64(timeoutTimestamp)) //nolint:gosec // G115
 	if err != nil {
 		return 0, err
 	}
