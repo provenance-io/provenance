29ec471010c731f7a741b11fd1ee58be4f6d5473
nagarajdivine <nmanjunath@figuremarkets.com>
2025-11-05 22:58:28 +0800

    test: proto-format failure

--- proto/provenance/trigger/v1/test_trigger.go
+++ proto/provenance/trigger/v1/test_trigger.go
@@ -1,6 +1,6 @@
 syntax = "proto3";
-
-package provenance.trigger.v1;
-
-// Intentional formatting issue below: this line is too long and should fail the proto-format check
-message TestTriggerMessage { string field_one = 1; string field_two = 2; string field_three = 3; string field_four = 4; string field_five = 5; }
+package trigger.v1;
+message TestTriggerMessage{
+string field_one = 1;
+string field_two = 2;
+}
