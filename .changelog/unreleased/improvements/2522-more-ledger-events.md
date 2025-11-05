5f6c88bc45ca67bd12521cdcc8d4908206b43bae
Daniel Wedul <github@wedul.com>
2025-11-05 16:06:07 -0700

    Ledger: New events related to ledger class creation. (#2522)
    * Create new events for creating a ledger class and its types and one for updating a ledger entry. Emit them at the appropriate times.
    
    * In the new EventLedgerClassTypeCreated type, change the field with the enum to type_created (from update_type).
    
    * Shorten the names of the variables created for the UpdateType enum.
    
    * Add changelog entry.
    
    * Fix linter errors that my stupid IDE used to auto-fix for me all the time, but now only does it some of the time.

---
+++ .changelog/unreleased/improvements/2522-more-ledger-events.md
@@ -0,0 +1,1 @@
+* Emit events when a ledger class or class type is created, and when a ledger entry is updated [PR 2522](https://github.com/provenance-io/provenance/pull/2522).
--- x/ledger/types/events.go
+++ x/ledger/types/events.go
@@ -1,5 +1,7 @@
 package types
 
+import "strconv"
+
 func NewEventLedgerCreated(key *LedgerKey) *EventLedgerCreated {
 	return &EventLedgerCreated{
 		AssetClassId: key.AssetClassId,
@@ -23,6 +25,14 @@ func NewEventLedgerEntryAdded(key *LedgerKey, correlationID string) *EventLedger
 	}
 }
 
+func NewEventLedgerEntryUpdated(key *LedgerKey, correlationID string) *EventLedgerEntryUpdated {
+	return &EventLedgerEntryUpdated{
+		AssetClassId:  key.AssetClassId,
+		NftId:         key.NftId,
+		CorrelationId: correlationID,
+	}
+}
+
 func NewEventFundTransferWithSettlement(key *LedgerKey, correlationID string) *EventFundTransferWithSettlement {
 	return &EventFundTransferWithSettlement{
 		AssetClassId:  key.AssetClassId,
@@ -35,5 +45,40 @@ func NewEventLedgerDestroyed(key *LedgerKey) *EventLedgerDestroyed {
 	return &EventLedgerDestroyed{
 		AssetClassId: key.AssetClassId,
 		NftId:        key.NftId,
+	}
+}
+
+func NewEventLedgerClassCreated(lc *LedgerClass) *EventLedgerClassCreated {
+	return &EventLedgerClassCreated{
+		LedgerClassId: lc.LedgerClassId,
+		AssetClassId:  lc.AssetClassId,
+		Denom:         lc.Denom,
+	}
+}
+
+func NewEventLedgerClassStatusTypeCreated(lcID string, t *LedgerClassStatusType) *EventLedgerClassTypeCreated {
+	return &EventLedgerClassTypeCreated{
+		LedgerClassId: lcID,
+		TypeCreated:   CLASS_TYPE_CREATED_STATUS,
+		Id:            strconv.Itoa(int(t.Id)),
+		Code:          t.Code,
+	}
+}
+
+func NewEventLedgerClassEntryTypeCreated(lcID string, t *LedgerClassEntryType) *EventLedgerClassTypeCreated {
+	return &EventLedgerClassTypeCreated{
+		LedgerClassId: lcID,
+		TypeCreated:   CLASS_TYPE_CREATED_ENTRY,
+		Id:            strconv.Itoa(int(t.Id)),
+		Code:          t.Code,
+	}
+}
+
+func NewEventLedgerClassBucketTypeCreated(lcID string, t *LedgerClassBucketType) *EventLedgerClassTypeCreated {
+	return &EventLedgerClassTypeCreated{
+		LedgerClassId: lcID,
+		TypeCreated:   CLASS_TYPE_CREATED_BUCKET,
+		Id:            strconv.Itoa(int(t.Id)),
+		Code:          t.Code,
 	}
 }
