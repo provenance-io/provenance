0eb715d7a6b41ad882c32ce4c972e54c26c9fd80
Carlton N Hanna <nullpointer0x00@gmail.com>
2025-12-10 15:55:25 -0700

    fix lint

--- app/upgrades.go
+++ app/upgrades.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"fmt"
 
+	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
+	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
+
 	storetypes "cosmossdk.io/store/types"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 
@@ -12,9 +15,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module"
 	vesting "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
 	ibctmmigrations "github.com/cosmos/ibc-go/v8/modules/light-clients/07-tendermint/migrations"
-
-	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
-	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 )
 
 // appUpgrade is an internal structure for defining all things for an upgrade.
