9e3969a1c5beeb4c7a5b49e3bea9efe61bca376b
nagarajdivine <nmanjunath@figuremarkets.com>
2025-12-18 23:39:28 +0800

    fix ErrInvalidOracleAddr code

--- x/flatfees/types/errors.go
+++ x/flatfees/types/errors.go
@@ -10,5 +10,5 @@ var (
 	ErrMsgFeeDoesNotExist  = cerrs.Register(ModuleName, 5, "fee for type does not exist")
 	ErrOracleAlreadyExists = cerrs.Register(ModuleName, 1100, "oracle address already exists")
 	ErrOracleNotFound      = cerrs.Register(ModuleName, 1101, "oracle address not found")
-	ErrInvalidOracleAddr   = cerrs.Register(ModuleName, 1103, "invalid oracle address")
+	ErrInvalidOracleAddr   = cerrs.Register(ModuleName, 1102, "invalid oracle address")
 )
