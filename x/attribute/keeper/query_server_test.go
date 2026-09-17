de8b72899c5515b59ce03eff280ba6baf2db6f77
nagarajdivine <nmanjunath@figuremarkets.com>
2026-09-17 22:51:49 +0800

    return the correct error type

--- x/attribute/keeper/query_server_test.go
+++ x/attribute/keeper/query_server_test.go
@@ -45,7 +45,6 @@ func (s *QueryServerTestSuite) SetupTest() {
 	s.ctx = s.app.BaseApp.NewContext(true)
 	s.app.AccountKeeper.Params.Set(s.ctx, authtypes.DefaultParams())
 	s.app.BankKeeper.SetParams(s.ctx, banktypes.DefaultParams())
-	s.app.NameKeeper.SetParams(s.ctx, nametypes.DefaultParams())
 	s.Require().NoError(s.app.NameKeeper.SetParams(s.ctx, nametypes.DefaultParams()), "NameKeeper.SetParams")
 	s.cfg = testutil.DefaultTestNetworkConfig()
 	queryHelper := baseapp.NewQueryServerTestHelper(s.ctx, s.app.InterfaceRegistry())
