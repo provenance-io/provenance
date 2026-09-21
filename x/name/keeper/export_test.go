e3afa9a4a1d36fa4a045aad511e82f78ad1be55d
nagarajdivine <nmanjunath@figuremarkets.com>
2026-09-22 04:56:32 +0800

    Migrate name module from KV store to collections. (#2411)
    * feature(collections-name-module): updated name module kvstore to collections
    
    * feature(collections-name-module): updated collection in app.go and in keeper with all test cases
    
    * feature(collections-name-module): added keycodec to address index and name prefix
    
    * feature(collections-name-module): added migration method
    
    * feature(collections-name-module): added mogations method and confgiured app.go for migration
    
    * feature(collections-name-module): added entry for collections in upgrade with test cases and fixed migration test
    
    * feature(collections-name-module): fixed lint issue
    
    * feature(collections-name-module): rebased to main and resolved all suggestions
    
    * feature(collections-name-module): fix lint issue
    
    * feature(collections-name-module): added changelog with changes made in add record
    
    * feature(collections-name-module): update name keeper to use IndexMap, fix migrations and test cases
    
    * feature(collections-name-module): name module updated from collections map to index map with lint-fix
    
    * feature(collections-name-module): fixed test cases,updated reverse lookup pagination
    
    * feature(collections-name-module): rebased main and fixed the lint issue
    
    * feature(collections-name-module): fixed lint issue
    
    * feature(collections-name-module): added comment G115 for safe conversion
    
    * feature(collections-name-module): removed log from migration
    
    * feature(collections-name-module): rebased main and fix all suggestions on name module
    
    * feature(collections-name-module): fixed query and getrecordbyaddress
    
    * feature(collections-name-module): fix test cases
    
    * feature(collections-name-module): fix ReverseLookup query by address index
    
    * feature(collections-name-module): rebase to main
    
    * rebase onto main and fix lint
    
    * fix test case
    
    * Use hashed name for key instead of just the segments
    
    * Use hashed name for key instead of just the segments
    
    * update migration function
    
    * replace legacykeys migration to keys.
    
    * fix the migration and update normalize name.
    
    * move LegacyComputeNameHash and LegacyGetNameKeyBytes to migration.
    
    * resolve all suggestions
    
    * return the correct error type
    
    * update the normalize name func with test case.
    
    * fix SetParams test

--- x/name/keeper/export_test.go
+++ x/name/keeper/export_test.go
@@ -1,11 +1,12 @@
 package keeper
 
-import sdk "github.com/cosmos/cosmos-sdk/types"
-
 // This file is available only to unit tests and exposes private things
 // so that they can be used in unit tests.
-
-// AddRecord is a TEST ONLY exposure of addRecord.
-func (k Keeper) AddRecord(ctx sdk.Context, name string, addr sdk.AccAddress, restrict, isModifiable bool) error {
-	return k.addRecord(ctx, name, addr, restrict, isModifiable)
-}
+var (
+	// LegacyNameKeyPrefix is a TEST ONLY exposure of legacyNameKeyPrefix.
+	LegacyNameKeyPrefix = legacyNameKeyPrefix
+	// LegacyAddressKeyPrefix is a TEST ONLY exposure of legacyAddressKeyPrefix.
+	LegacyAddressKeyPrefix = legacyAddressKeyPrefix
+	// LegacyNameParamStoreKey is a TEST ONLY exposure of legacyNameParamStoreKey.
+	LegacyNameParamStoreKey = legacyNameParamStoreKey
+)
